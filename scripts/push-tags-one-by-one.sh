4409ef46c189e33fb925c7c9b013914d87fed89e
StarpTech <deusdustin@gmail.com>
2023-08-24 12:30:10 +0200

    chore: test release pipeline

--- scripts/push-tags-one-by-one.sh
+++ scripts/push-tags-one-by-one.sh
@@ -4,8 +4,5 @@
 TAGS=$(git tag --points-at HEAD | cat)
 
 # only push tags one by one if there are more than 3
-if (($(echo "$TAGS"| wc -l) > 3))
-then
-  echo "Pushing tags one by one to avoid GitHub webhook limit of 3"
-  echo "$TAGS" | while read line ; do git push origin $line; done
-fi
+echo "Pushing tags one by one to avoid GitHub webhook limit of 3"
+echo "$TAGS" | while read line ; do git push origin $line; done
