5173740cf31928530daca776cf8e4fede4a4377c
Alberto García Hierro <alberto@garciahierro.com>
2023-08-29 09:45:31 +0200

    fix: otel support in demo (#15)

---
+++ demo/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ex
+
+if test -z $OTEL_AUTH_TOKEN; then
+    echo missing OTEL_AUTH_TOKEN
+    exit 1
+fi
+
+npx concurrently --kill-others \
+    "cd employees && go run main.go" \
+    "cd family && go run main.go" \
+    "cd hobbies && go run main.go" \
+    "cd products && go run main.go"
