16aef7994eaaadea4555b17661064b087374c983
StarpTech <deusdustin@gmail.com>
2023-09-05 15:49:14 +0200

    chore: improve

--- router/cmd/custom/main.go
+++ router/cmd/custom/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	routercmd "github.com/wundergraph/cosmo/router/cmd"
 	// Import your modules here
-	_ "github.com/wundergraph/cosmo/router/cmd/custom/middleware"
+	_ "github.com/wundergraph/cosmo/router/cmd/custom/module"
 )
 
 func main() {
--- router/cmd/custom/module/module.go
+++ router/cmd/custom/module/module.go
@@ -1,4 +1,4 @@
-package middleware
+package module
 
 import (
 	"context"
