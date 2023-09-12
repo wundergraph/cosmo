8e14d1fe3aadd1a5b51da4f8a354e6757105f562
spetrunin <neyasut@gmail.com>
2023-09-12 11:52:16 +0300

    add run subgraphs sh to demo

--- demo/run.sh
+++ demo/run.sh
@@ -2,13 +2,9 @@
 
 set -ex
 
-#if test -z $OTEL_AUTH_TOKEN; then
-#    echo missing OTEL_AUTH_TOKEN
-#    exit 1
-#fi
+if test -z $OTEL_AUTH_TOKEN; then
+    echo missing OTEL_AUTH_TOKEN
+    exit 1
+fi
 
-npx concurrently --kill-others \
-    "cd employees && go run main.go" \
-    "cd family && go run main.go" \
-    "cd hobbies && go run main.go" \
-    "cd products && go run main.go"
+./run_subgraphs.sh
