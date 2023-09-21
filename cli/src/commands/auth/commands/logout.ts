47a4b973eacecfb957fdd42c3d5559b41b3dd097
JivusAyrus <suvijsurya76@gmail.com>
2023-09-22 00:53:21 +0530

    fix: pr suggestion

--- cli/src/commands/auth/commands/logout.ts
+++ cli/src/commands/auth/commands/logout.ts
@@ -3,19 +3,16 @@ import { Command } from 'commander';
 import pc from 'picocolors';
 import { configDir } from '../../../core/config.js';
 import { BaseCommandOptions } from '../../../core/types/types.js';
-import program from '../../index.js';
 
 export default (opts: BaseCommandOptions) => {
   const logoutCommand = new Command('logout');
-  logoutCommand.description('Logout a user.');
+  logoutCommand.description('Logout a user from the Cosmo platform.');
 
   logoutCommand.action(() => {
     try {
       rmSync(configDir, { recursive: true });
-      console.log(pc.green('Logged out Successfully!'));
-    } catch {
-      program.error(pc.red(`No access token found. Please login with 'wgc auth login'`));
-    }
+    } catch {}
+    console.log(pc.green('Logged out Successfully!'));
   });
 
   return logoutCommand;
