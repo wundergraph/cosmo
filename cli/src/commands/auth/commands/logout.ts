8a90337e92ac8683be6ac872348a39aa545692cd
JivusAyrus <suvijsurya76@gmail.com>
2023-09-22 00:45:37 +0530

    fix: pr suggestion

--- cli/src/commands/auth/commands/logout.ts
+++ cli/src/commands/auth/commands/logout.ts
@@ -1,20 +1,21 @@
-import { existsSync, rmSync } from 'node:fs';
+import { rmSync } from 'node:fs';
 import { Command } from 'commander';
 import pc from 'picocolors';
-import { configDir, configFile } from '../../../core/config.js';
-import program from '../../index.js';
+import { configDir } from '../../../core/config.js';
 import { BaseCommandOptions } from '../../../core/types/types.js';
+import program from '../../index.js';
 
 export default (opts: BaseCommandOptions) => {
   const logoutCommand = new Command('logout');
   logoutCommand.description('Logout a user.');
 
   logoutCommand.action(() => {
-    if (!existsSync(configFile)) {
+    try {
+      rmSync(configDir, { recursive: true });
+      console.log(pc.green('Logged out Successfully!'));
+    } catch {
       program.error(pc.red(`No access token found. Please login with 'wgc auth login'`));
     }
-    rmSync(configDir, { recursive: true });
-    console.log(pc.green('Logged out Successfully!'));
   });
 
   return logoutCommand;
