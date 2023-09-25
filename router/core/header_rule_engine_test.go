5a8692023b7f7a51d705ac9fb3e7f779b087d648
Clarence Delmacio Manuel <71768661+cs-clarence@users.noreply.github.com>
2023-09-26 01:07:46 +0800

    feat: support per subgraph header rules (#110)
    Co-authored-by: Dustin Deus <deusdustin@gmail.com>

--- router/core/header_rule_engine_test.go
+++ router/core/header_rule_engine_test.go
@@ -2,12 +2,14 @@ package core
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/wundergraph/cosmo/router/internal/config"
 	"go.uber.org/zap"
-	"net/http"
-	"net/http/httptest"
-	"testing"
 )
 
 func TestNamedPropagateHeaderRule(t *testing.T) {
@@ -166,4 +168,70 @@ func TestInvalidRegex(t *testing.T) {
 		},
 	})
 	assert.Error(t, err)
+}
+
+func TestSubgraphNamedHeaderRule(t *testing.T) {
+	ht, err := NewHeaderTransformer(config.HeaderRules{
+		Subgraphs: map[string]config.GlobalHeaderRule{
+			"subgraph-1": {
+				Request: []config.RequestHeaderRule{
+					{
+						Operation: "propagate",
+						Named:     "X-Test-Subgraph-1",
+						Default:   "Test-Value-1",
+					},
+				},
+			},
+			"subgraph-2": {
+				Request: []config.RequestHeaderRule{
+					{
+						Operation: "propagate",
+						Named:     "X-Test-Subgraph-2",
+					},
+				},
+			},
+		},
+	})
+	assert.Nil(t, err)
+
+	rr := httptest.NewRecorder()
+
+	clientReq, err := http.NewRequest("POST", "http://localhost", nil)
+	clientReq.Header.Set("X-Test-Subgraph-2", "Test-Value-2")
+
+	sg1Url, _ := url.Parse("http://subgraph-1.local")
+	sg2Url, _ := url.Parse("http://subgraph-2.local")
+
+	ctx := &requestContext{
+		logger:         zap.NewNop(),
+		responseWriter: rr,
+		request:        clientReq,
+		operation:      &operationContext{},
+		subgraphs: []Subgraph{
+			{
+				Name: "subgraph-1",
+				Id:   "subgraph-1",
+				Url:  sg1Url,
+			},
+			{
+				Name: "subgraph-2",
+				Id:   "subgraph-2",
+				Url:  sg2Url,
+			},
+		},
+	}
+
+	originReq1, err := http.NewRequest("POST", "http://subgraph-1.local", nil)
+	assert.Nil(t, err)
+	updatedClientReq1, _ := ht.OnOriginRequest(originReq1, ctx)
+
+	assert.Equal(t, "Test-Value-1", updatedClientReq1.Header.Get("X-Test-Subgraph-1"))
+	assert.Empty(t, updatedClientReq1.Header.Get("X-Test-Subgraph-2"))
+
+	originReq2, err := http.NewRequest("POST", "http://subgraph-2.local", nil)
+	assert.Nil(t, err)
+	updatedClientReq2, _ := ht.OnOriginRequest(originReq2, ctx)
+
+	assert.Empty(t, updatedClientReq2.Header.Get("X-Test-Subgraph-1"))
+	assert.Equal(t, "Test-Value-2", updatedClientReq2.Header.Get("X-Test-Subgraph-2"))
 }
