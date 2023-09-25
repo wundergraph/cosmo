4cdb18c5cabc4abca4ea6154613937ed9d02fb0e
thisisnithin <nithinkumar5353@gmail.com>
2023-09-26 00:10:42 +0530

    address feedback

--- cli/src/commands/federated-graph/commands/changelog.ts
+++ cli/src/commands/federated-graph/commands/changelog.ts
@@ -1,10 +1,23 @@
 import * as fs from 'node:fs';
 import { EnumStatusCode } from '@wundergraph/cosmo-connect/dist/common/common_pb';
-import { Command } from 'commander';
+import { Command, program } from 'commander';
 import { endOfDay, formatISO, startOfDay, subDays } from 'date-fns';
 import pc from 'picocolors';
 import { baseHeaders } from '../../../core/config.js';
 import { BaseCommandOptions } from '../../../core/types/types.js';
+
+// modifying this type is a breaking change
+type SchemaChanges = {
+  createdAt: string;
+  schemaVersionId: string;
+  changelogs: {
+    id: string;
+    path: string;
+    changeType: string;
+    changeMessage: string;
+    createdAt: string;
+  }[];
+}[];
 
 export default (opts: BaseCommandOptions) => {
   const command = new Command('changelog');
@@ -12,9 +25,9 @@ export default (opts: BaseCommandOptions) => {
   command.argument('<name>', 'The name of the federated graph to update.');
   command.option('-l, --limit [number]', 'Limit of entries. Defaults to 10', '10');
   command.option('-f, --offset [number]', 'Offset of entries. Defaults to 0', '0');
-  command.option('-s, --start', 'Start date. Defaults to 3 days back');
-  command.option('-e, --end', 'End date. Defaults to today');
-  command.option('-o, --out', 'Destination file for changelog. Defaults to changelog.json');
+  command.option('-s, --start [date]', 'Start date. Defaults to 3 days back');
+  command.option('-e, --end [date]', 'End date. Defaults to today');
+  command.option('-o, --out', 'Destination file for changelog. Defaults to changelog.json', 'changelog.json');
   command.action(async (name, options) => {
     let startDate = subDays(new Date(), 3);
     let endDate = new Date();
@@ -44,13 +57,25 @@ export default (opts: BaseCommandOptions) => {
     );
 
     if (resp.response?.code === EnumStatusCode.OK) {
-      fs.writeFileSync(options.out || './changelog.json', JSON.stringify(resp.federatedGraphChangelogOutput));
+      const output: SchemaChanges = resp.federatedGraphChangelogOutput.map((op) => ({
+        createdAt: op.createdAt,
+        schemaVersionId: op.schemaVersionId,
+        changelogs: op.changelogs.map((cl) => ({
+          id: cl.id,
+          path: cl.path,
+          changeType: cl.changeType,
+          changeMessage: cl.changeMessage,
+          createdAt: cl.createdAt,
+        })),
+      }));
+
+      fs.writeFileSync(options.out, JSON.stringify(output));
     } else {
-      console.log(`Failed to fetch changelog for ${pc.bold(name)}.`);
+      let message = `Failed to fetch changelog for ${pc.bold(name)}.`;
       if (resp.response?.details) {
-        console.log(pc.red(pc.bold(resp.response?.details)));
+        message += pc.red(pc.bold(resp.response?.details));
       }
-      process.exit(1);
+      program.error(message);
     }
   });
 
