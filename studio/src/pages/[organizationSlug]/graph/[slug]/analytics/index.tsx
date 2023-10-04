0c58e82bbd5c05dba9b82cbb4a3b5044463cb1bd
Pagebakers <eelco@appulse.nl>
2023-10-04 18:21:43 +0200

    fix: adjust margins

--- studio/src/pages/[organizationSlug]/graph/[slug]/analytics/index.tsx
+++ studio/src/pages/[organizationSlug]/graph/[slug]/analytics/index.tsx
@@ -660,7 +660,7 @@ const ErrorRateOverTimeCard = () => {
       <ResponsiveContainer width="100%" height="100%">
         <AreaChart
           data={data}
-          margin={{ top: 10, right: 8, bottom: 8, left: 0 }}
+          margin={{ top: 8, right: 8, bottom: 8, left: 0 }}
         >
           <defs>
             <linearGradient id={`${id}-gradient`} x1="0" y1="0" x2="0" y2="1">
