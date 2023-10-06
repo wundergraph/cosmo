df002f42159c029f9083789d8685e35cbd38274c
fiam <alberto@garciahierro.com>
2023-10-06 15:04:36 +0100

    test: add helper functions for integration test

--- router-tests/integration_test.go
+++ router-tests/integration_test.go
@@ -3,8 +3,13 @@ package module
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
 	"net/http/httptest"
 	"path/filepath"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,10 +17,60 @@ import (
 	"github.com/wundergraph/cosmo/demo/pkg/subgraphs"
 	"github.com/wundergraph/cosmo/router/config"
 	"github.com/wundergraph/cosmo/router/core"
+	"go.uber.org/zap"
 )
 
-func TestIntegration(t *testing.T) {
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+func randString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+type testQuery struct {
+	Name      string
+	Body      string
+	Variables map[string]interface{}
+}
+
+func (t *testQuery) Data() []byte {
+	name := t.Name
+	if name == "" {
+		name = randString(10)
+	}
+	values := map[string]interface{}{
+		"query":         fmt.Sprintf("query %s %s", name, t.Body),
+		"operationName": name,
+	}
+	if len(t.Variables) > 0 {
+		values["variables"] = t.Variables
+	}
+	data, err := json.Marshal(values)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
+
+func sendQuery(tb testing.TB, server *core.Server, query *testQuery) string {
+	return sendData(tb, server, query.Data())
+}
+
+func sendData(tb testing.TB, server *core.Server, data []byte) string {
+	rr := httptest.NewRecorder()
+
+	req := httptest.NewRequest("POST", "/graphql", bytes.NewBuffer(data))
+	server.Server.Handler.ServeHTTP(rr, req)
+
+	fmt.Println("RES", rr.Body.String())
+	//assert.Equal(tb, 200, rr.Code)
+	return rr.Body.String()
+}
+
+func setupServer(tb testing.TB) *core.Server {
 	ctx := context.Background()
 	cfg := config.Config{
 		Graph: config.Graph{
@@ -24,7 +79,7 @@ func TestIntegration(t *testing.T) {
 	}
 
 	routerConfig, err := core.SerializeConfigFromFile(filepath.Join("testdata", "config.json"))
-	require.Nil(t, err)
+	require.Nil(tb, err)
 
 	sg, err := subgraphs.New(&subgraphs.Config{
 		Ports: subgraphs.Ports{
@@ -34,40 +89,63 @@ func TestIntegration(t *testing.T) {
 			Products:  4004,
 		},
 	})
-	require.Nil(t, err)
+	require.Nil(tb, err)
 
 	go func() {
-		require.Nil(t, sg.ListenAndServe(ctx))
+		err := sg.ListenAndServe(ctx)
+		if err != http.ErrServerClosed {
+			require.Nil(tb, err)
+		}
 	}()
-	t.Cleanup(func() {
-		assert.Nil(t, sg.Shutdown(ctx))
+	tb.Cleanup(func() {
+		assert.Nil(tb, sg.Shutdown(ctx))
 	})
+
+	logger := zap.New()
 
 	rs, err := core.NewRouter(
 		core.WithFederatedGraphName(cfg.Graph.Name),
 		core.WithStaticRouterConfig(routerConfig),
 		core.WithModulesConfig(cfg.Modules),
+		core.WithLogger(logger),
 		core.WithListenerAddr("http://localhost:3002"),
 	)
-	require.Nil(t, err)
+	require.Nil(tb, err)
 
-	t.Cleanup(func() {
-		assert.Nil(t, rs.Shutdown(ctx))
+	tb.Cleanup(func() {
+		assert.Nil(tb, rs.Shutdown(ctx))
 	})
 
 	server, err := rs.NewTestServer(ctx)
-	require.Nil(t, err)
+	require.Nil(tb, err)
+	return server
+}
 
-	rr := httptest.NewRecorder()
+func TestIntegration(t *testing.T) {
+	server := setupServer(t)
+	result := sendQuery(t, server, &testQuery{
+		Body: "{ employees { id } }",
+	})
+	assert.JSONEq(t, result, `{"data":{"employees":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":7},{"id":8},{"id":9},{"id":10},{"id":11},{"id":12}]}}`)
+}
 
-	var jsonData = []byte(`{
-		"query": "query MyQuery { employees { id } }",
-		"operationName": "MyQuery"
-	}`)
-	req := httptest.NewRequest("POST", "/graphql", bytes.NewBuffer(jsonData))
-	server.Server.Handler.ServeHTTP(rr, req)
-
-	assert.Equal(t, 200, rr.Code)
-
-	assert.JSONEq(t, rr.Body.String(), `{"data":{"employees":[{"id":1},{"id":2},{"id":3},{"id":4},{"id":5},{"id":7},{"id":8},{"id":9},{"id":10},{"id":11},{"id":12}]}}`)
+func BenchmarkIntegration(b *testing.B) {
+	server := setupServer(b)
+	q := &testQuery{
+		Body:      "($n:Int!) { employee(id:$n) { id } }",
+		Variables: map[string]interface{}{"n": 4},
+	}
+	data := q.Data()
+	//res := sendData(b, server, data)
+	b.ReportAllocs()
+	b.ResetTimer()
+	var wg sync.WaitGroup
+	for ii := 0; ii < b.N; ii++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_ = sendData(b, server, data)
+			//assert.Equal(b, res, result)
+		}()
+	}
 }
