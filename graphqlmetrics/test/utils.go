050a2c37fedc28c09fa130d9d7725cdccb88d144
StarpTech <deusdustin@gmail.com>
2023-10-12 17:42:22 +0200

    chore: improve

--- graphqlmetrics/test/utils.go
+++ graphqlmetrics/test/utils.go
@@ -9,6 +9,11 @@ func GetTestDatabase() *sql.DB {
 	db := clickhouse.OpenDB(&clickhouse.Options{
 		Addr:     []string{"localhost:8123"},
 		Protocol: clickhouse.HTTP,
+		Settings: map[string]any{
+			"insert_quorum":                 "1",
+			"insert_quorum_parallel":        "0",
+			"select_sequential_consistency": "1",
+		},
 		Auth: clickhouse.Auth{
 			Database: "cosmo",
 			Username: "default",
