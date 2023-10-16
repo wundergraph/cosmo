b4804417f2c145e145e8c27f0ff840d235a0e14c
fiam <alberto@garciahierro.com>
2023-10-16 15:09:34 +0100

    Revert "fix: export enum types from /shared"
    This reverts commit 28bdd93227004aac450d40305b5d811330c499b9.

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,3 +1,3 @@
-export * from './utils/util';
-export * from './router-config/builder';
-export * from './openai-graphql/index';
+export * from './utils/util.js';
+export * from './router-config/builder.js';
+export * from './openai-graphql/index.js';
