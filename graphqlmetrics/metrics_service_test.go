4013524b4996d602218896ee4fe90dc5c09787b4
StarpTech <deusdustin@gmail.com>
2023-10-25 16:34:24 +0200

    chore: improve

--- graphqlmetrics/metrics_service_test.go
+++ graphqlmetrics/metrics_service_test.go
@@ -50,6 +50,10 @@ func TestPublishGraphQLMetrics(t *testing.T) {
 					Name:    "wundergraph",
 					Version: "1.0.0",
 				},
+				RequestInfo: &graphqlmetricsv1.RequestInfo{
+					StatusCode: 200,
+					Error:      true,
+				},
 				Attributes: map[string]string{
 					"test": "test123",
 				},
@@ -73,6 +77,8 @@ func TestPublishGraphQLMetrics(t *testing.T) {
 	)
 	require.NoError(t, err)
 
+	// Validate insert
+
 	var opCount int
 	require.NoError(t, db.QueryRow(`
 		SELECT COUNT(*) FROM gql_metrics_operations
@@ -85,19 +91,43 @@ func TestPublishGraphQLMetrics(t *testing.T) {
 
 	assert.Greater(t, opCount, 0)
 
+	// Validate insert
+
 	var fieldUsageCount int
 	require.NoError(t, db.QueryRow(`
 		SELECT COUNT(*) FROM gql_metrics_schema_usage
 		WHERE OperationHash = 'hash123' AND
+		OrganizationID = 'org123' AND
+		FederatedGraphID = 'fed123' AND
 		RouterConfigVersion = 'v1' AND
 		Attributes['test'] = 'test123' AND
+		HttpStatusCode = '200' AND
+		HasError = true AND
 		ClientName = 'wundergraph' AND
 		ClientVersion = '1.0.0' AND
 		hasAny(TypeNames, ['Query']) AND
 		startsWith(Path, ['hello'])
 	`).Scan(&fieldUsageCount))
 
-	assert.Greater(t, fieldUsageCount, 0)
+	// Validate materialized view
+
+	var fieldUsageCountMv int
+	require.NoError(t, db.QueryRow(`
+		SELECT COUNT(*) FROM gql_metrics_schema_usage_5m_90d_mv
+		WHERE OperationHash = 'hash123' AND
+		OrganizationID = 'org123' AND
+		FederatedGraphID = 'fed123' AND
+		RouterConfigVersion = 'v1' AND
+		TotalErrors = 1 AND
+		TotalUsages = 1 AND
+		TotalClientErrors = 0 AND
+		ClientName = 'wundergraph' AND
+		ClientVersion = '1.0.0' AND
+		hasAny(TypeNames, ['Query']) AND
+		startsWith(Path, ['hello'])
+	`).Scan(&fieldUsageCountMv))
+
+	assert.Greater(t, fieldUsageCountMv, 0)
 }
 
 func TestAuthentication(t *testing.T) {
