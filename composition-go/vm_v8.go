1f68219de654190dc04df05a332d09e253f0d38f
Alberto García Hierro <alberto@garciahierro.com>
2023-11-02 10:05:26 +0000

    feat: add support for router config generation to composition-go (#218)
    Extend composition-go with a new BuildRouterConfiguration() function. This allows us to use composition-go
    during integration tests instead of calling out to wgc via the command line. Update README to include instructions
    on how to keep the generated files within the library up to date.

--- composition-go/vm_v8.go
+++ composition-go/vm_v8.go
@@ -5,6 +5,7 @@ package composition
 import (
 	"fmt"
 	"os"
+	"reflect"
 
 	v8 "rogchap.com/v8go"
 )
@@ -31,18 +32,55 @@ func deserializeArray[T any](arrayObject *v8.Object, elementDecoder func(*v8.Obj
 	return result, nil
 }
 
+// serializes an object for v8 using reflect and the goja
+// tags for field names
+func serializeGojaObject(ctx *v8.Context, v any) (*v8.Value, error) {
+	rv := reflect.ValueOf(v)
+	for rv.Kind() == reflect.Ptr {
+		rv = rv.Elem()
+	}
+	rt := rv.Type()
+	switch rt.Kind() {
+	case reflect.Struct:
+		tmpl := v8.NewObjectTemplate(ctx.Isolate())
+		obj, err := tmpl.NewInstance(ctx)
+		if err != nil {
+			return nil, err
+		}
+		for ii := 0; ii < rt.NumField(); ii++ {
+			field := rt.Field(ii)
+			tag := field.Tag.Get("goja")
+			if tag == "" {
+				return nil, fmt.Errorf("no goja field tag in field %s", field.Name)
+			}
+			value := rv.Field(ii).Interface()
+			valueObject, err := serializeGojaObject(ctx, value)
+			if err != nil {
+				return nil, err
+			}
+			obj.Set(tag, valueObject)
+		}
+		return obj.Value, nil
+	case reflect.String:
+		return v8.NewValue(ctx.Isolate(), rv.String())
+	default:
+		return nil, fmt.Errorf("cannot serialize type kind %s", rt.Kind())
+	}
+}
+
 type v8Vm struct {
-	isolate           *v8.Isolate
-	ctx               *v8.Context
-	null              v8.Valuer
-	federateSubgraphs *v8.Function
+	isolate                  *v8.Isolate
+	ctx                      *v8.Context
+	null                     v8.Valuer
+	federateSubgraphs        *v8.Function
+	buildRouterConfiguration *v8.Function
 }
 
 func (m *v8Vm) Dispose() {
 	m.isolate.Dispose()
 }
 
-func (m *v8Vm) FederateSubgraphs(subgraphs []*Subgraph) (*FederatedGraph, error) {
+func (m *v8Vm) subgraphsToJS(subgraphs []*Subgraph) (*v8.Object, error) {
 	tmpl := v8.NewObjectTemplate(m.ctx.Isolate())
 	arrayValue, err := m.ctx.RunScript("[]", "array")
 	if err != nil {
@@ -60,7 +98,14 @@ func (m *v8Vm) FederateSubgraphs(subgraphs []*Subgraph) (*FederatedGraph, error)
 		obj.Set("schema", subgraphs[ii].Schema)
 		arrayObject.SetIdx(uint32(ii), obj)
 	}
+	return arrayObject, nil
+}
 
+func (m *v8Vm) FederateSubgraphs(subgraphs []*Subgraph) (*FederatedGraph, error) {
+	arrayObject, err := m.subgraphsToJS(subgraphs)
+	if err != nil {
+		return nil, err
+	}
 	result, err := m.federateSubgraphs.Call(m.null, arrayObject)
 	if err != nil {
 		return nil, err
@@ -109,6 +154,18 @@ func (m *v8Vm) FederateSubgraphs(subgraphs []*Subgraph) (*FederatedGraph, error)
 	}, nil
 }
 
+func (m *v8Vm) BuildRouterConfiguration(subgraphs []*Subgraph) (string, error) {
+	arrayObject, err := m.subgraphsToJS(subgraphs)
+	if err != nil {
+		return "", err
+	}
+	result, err := m.buildRouterConfiguration.Call(m.null, arrayObject)
+	if err != nil {
+		return "", err
+	}
+	return result.String(), nil
+}
+
 func debugErr(err error) error {
 	if os.Getenv("DEBUG_V8_ERRORS") != "" {
 		if e, ok := err.(*v8.JSError); ok {
@@ -120,9 +177,64 @@ func debugErr(err error) error {
 	return err
 }
 
+func v8Exception(isolate *v8.Isolate, err error) {
+	exc, err := v8.NewValue(isolate, err.Error())
+	if err != nil {
+		panic(err)
+	}
+	isolate.ThrowException(exc)
+}
+
+func stringHashV8(info *v8.FunctionCallbackInfo) *v8.Value {
+	isolate := info.Context().Isolate()
+	input := info.Args()[0].String()
+	result, err := stringHash(input)
+	if err != nil {
+		v8Exception(isolate, err)
+		return nil
+	}
+	resultValue, err := v8.NewValue(isolate, result)
+	if err != nil {
+		panic(err)
+	}
+	return resultValue
+}
+
+func urlParseV8(info *v8.FunctionCallbackInfo) *v8.Value {
+	isolate := info.Context().Isolate()
+	args := info.Args()
+	url := args[0].String()
+	base := args[1].String()
+
+	result, err := urlParse(url, base)
+	if err != nil {
+		v8Exception(isolate, err)
+		return nil
+	}
+	value, err := serializeGojaObject(info.Context(), result)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
 func newVM() (*v8Vm, error) {
 	isolate := v8.NewIsolate()
-	ctx := v8.NewContext(isolate)
+
+	global := v8.NewObjectTemplate(isolate)
+
+	stringHash := v8.NewFunctionTemplate(isolate, stringHashV8)
+	if err := global.Set("stringHash", stringHash, v8.ReadOnly); err != nil {
+		return nil, err
+	}
+
+	urlParse := v8.NewFunctionTemplate(isolate, urlParseV8)
+	if err := global.Set("urlParse", urlParse, v8.ReadOnly); err != nil {
+		return nil, err
+	}
+
+	ctx := v8.NewContext(isolate, global)
+
 	if _, err := ctx.RunScript(jsPrelude, "prelude.js"); err != nil {
 		return nil, fmt.Errorf("error running prelude: %w", debugErr(err))
 	}
@@ -148,12 +260,17 @@ func newVM() (*v8Vm, error) {
 	if err != nil {
 		return nil, err
 	}
+	buildRouterConfiguration, err := shimFunc("buildRouterConfiguration")
+	if err != nil {
+		return nil, err
+	}
 
 	return &v8Vm{
-		isolate:           isolate,
-		ctx:               ctx,
-		null:              v8.Null(ctx.Isolate()),
-		federateSubgraphs: federateSubgraphs,
+		isolate:                  isolate,
+		ctx:                      ctx,
+		null:                     v8.Null(ctx.Isolate()),
+		federateSubgraphs:        federateSubgraphs,
+		buildRouterConfiguration: buildRouterConfiguration,
 	}, nil
 }
 
