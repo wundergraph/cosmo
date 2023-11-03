378f1a37e012440dc7bda2b97d8fe921f4eeb6c3
thisisnithin <nithinkumar5353@gmail.com>
2023-11-03 10:10:33 +0530

    cleanup migrations

--- controlplane/migrations/0049_skinny_sir_ram.sql
+++ controlplane/migrations/0049_skinny_sir_ram.sql
@@ -15,6 +15,12 @@ CREATE TABLE IF NOT EXISTS "schema_check_change_operation_usage" (
 	"last_seen_at" timestamp with time zone NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "schema_check_federated_graphs" (
+	"check_id" uuid NOT NULL,
+	"federated_graph_id" uuid NOT NULL,
+	"traffic_check_days" integer NOT NULL
+);
+--> statement-breakpoint
 ALTER TABLE "schema_checks" ADD COLUMN "has_client_traffic" boolean DEFAULT false;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "federated_graph_configs" ADD CONSTRAINT "federated_graph_configs_federated_graph_id_federated_graphs_id_fk" FOREIGN KEY ("federated_graph_id") REFERENCES "federated_graphs"("id") ON DELETE cascade ON UPDATE no action;
@@ -24,6 +30,18 @@ END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "schema_check_change_operation_usage" ADD CONSTRAINT "schema_check_change_operation_usage_schema_check_change_action_id_schema_check_change_action_id_fk" FOREIGN KEY ("schema_check_change_action_id") REFERENCES "schema_check_change_action"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "schema_check_federated_graphs" ADD CONSTRAINT "schema_check_federated_graphs_check_id_schema_checks_id_fk" FOREIGN KEY ("check_id") REFERENCES "schema_checks"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "schema_check_federated_graphs" ADD CONSTRAINT "schema_check_federated_graphs_federated_graph_id_federated_graphs_id_fk" FOREIGN KEY ("federated_graph_id") REFERENCES "federated_graphs"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- controlplane/migrations/0051_stiff_nightshade.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "schema_check_federated_graphs" ADD COLUMN "traffic_check_days" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "schema_checks" DROP COLUMN IF EXISTS "traffic_check_days";
