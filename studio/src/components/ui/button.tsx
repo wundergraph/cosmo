c4ee77bf7f50e124ada9a895931ffae6ef727da7
Pagebakers <eelco@appulse.nl>
2023-11-09 15:31:36 +0100

    fix: add sso link to main screen if sso is set

--- studio/src/components/ui/button.tsx
+++ studio/src/components/ui/button.tsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input shadow-sm hover:bg-accent/80 hover:text-accent-foreground",
+          "border border-input bg-transparent shadow-sm hover:bg-accent/80 hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80 border border-input",
         ghost:
--- studio/src/pages/login.tsx
+++ studio/src/pages/login.tsx
@@ -10,7 +10,7 @@ import { FaGoogle } from "react-icons/fa";
 
 import { BsBuildingLock } from "react-icons/bs";
 import { useCookies } from "react-cookie";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { z } from "zod";
 
 const loginUrl = `${process.env.NEXT_PUBLIC_COSMO_CP_URL}/v1/auth/login`;
@@ -48,99 +48,11 @@ const LoginPage: NextPageWithLayout = () => {
   const [cookies] = useCookies(["cosmo_idp_hint"]);
 
   useEffect(() => {
-    if (!router || sso) return;
+    if (!router.isReady || sso) return;
     if (cookies && cookies.cosmo_idp_hint) {
       router.replace(`/login?sso=${cookies.cosmo_idp_hint}`);
     }
   }, [cookies, sso, router]);
-
-  let content;
-  if (sso) {
-    content = (
-      <div className="w-full">
-        <p className="text-muted-foreground">
-          Don&apos;t use SSO?{" "}
-          <Link
-            href={redirectURL ? `/login?redirectURL=${redirectURL}` : "/login"}
-            className="underline hover:text-foreground"
-          >
-            Go back
-          </Link>
-        </p>
-
-        <div className="mt-12 w-full space-y-4">
-          <Button
-            variant="outline"
-            size="lg"
-            className="text-md w-full px-12 py-6"
-            asChild
-          >
-            <Link
-              href={constructLoginURL({ redirectURL, sso })}
-              className="flex gap-x-2"
-            >
-              <BsBuildingLock className="h-5 w-5" />
-              Sign in with SSO
-            </Link>
-          </Button>
-        </div>
-      </div>
-    );
-  } else {
-    content = (
-      <div className="w-full">
-        <p className="text-muted-foreground">
-          Don&apos;t have an account yet?{" "}
-          <Link
-            href={
-              redirectURL ? `/signup?redirectURL=${redirectURL}` : "/signup"
-            }
-            className="underline hover:text-foreground"
-          >
-            Sign up
-          </Link>
-        </p>
-        <div className="mt-12 space-y-4">
-          <Button
-            variant="outline"
-            size="lg"
-            className="text-md w-full px-12 py-6"
-            asChild
-          >
-            <Link href={constructLoginURL({ redirectURL, provider: "github" })}>
-              <GitHubLogoIcon className="me-2" />
-              Sign in with GitHub
-            </Link>
-          </Button>
-          <Button
-            variant="outline"
-            size="lg"
-            className="text-md w-full px-12 py-6"
-            asChild
-          >
-            <Link href={constructLoginURL({ redirectURL, provider: "google" })}>
-              <FaGoogle className="me-2" />
-              Sign in with Google
-            </Link>
-          </Button>
-
-          <Divider />
-
-          <Button
-            variant="outline"
-            size="lg"
-            className="text-md group w-full px-12 py-6"
-            asChild
-          >
-            <Link href={constructLoginURL({ redirectURL })}>
-              Continue with Email{" "}
-              <ArrowRightIcon className="ms-2 transition-all group-hover:translate-x-1" />
-            </Link>
-          </Button>
-        </div>
-      </div>
-    );
-  }
 
   return (
     <div className="flex min-h-screen items-center justify-center xl:items-start xl:justify-start">
@@ -151,7 +63,77 @@ const LoginPage: NextPageWithLayout = () => {
         </div>
         <div className="flex flex-col items-start pt-16">
           <h2 className="mb-1 text-2xl font-medium">Sign in</h2>
-          {content}
+          <div className="w-full">
+            <p className="text-muted-foreground">
+              Don&apos;t have an account yet?{" "}
+              <Link
+                href={
+                  redirectURL ? `/signup?redirectURL=${redirectURL}` : "/signup"
+                }
+                className="underline hover:text-foreground"
+              >
+                Sign up
+              </Link>
+            </p>
+            <div className="mt-12 space-y-4">
+              <Button
+                variant="outline"
+                size="lg"
+                className="text-md w-full px-12 py-6"
+                asChild
+              >
+                <Link
+                  href={constructLoginURL({ redirectURL, provider: "github" })}
+                >
+                  <GitHubLogoIcon className="me-2" />
+                  Sign in with GitHub
+                </Link>
+              </Button>
+              <Button
+                variant="outline"
+                size="lg"
+                className="text-md w-full px-12 py-6"
+                asChild
+              >
+                <Link
+                  href={constructLoginURL({ redirectURL, provider: "google" })}
+                >
+                  <FaGoogle className="me-2" />
+                  Sign in with Google
+                </Link>
+              </Button>
+              {sso ? (
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="text-md w-full px-12 py-6"
+                  asChild
+                >
+                  <Link
+                    href={constructLoginURL({ redirectURL, sso })}
+                    className="flex gap-x-2"
+                  >
+                    <BsBuildingLock className="h-5 w-5" />
+                    Sign in with SSO
+                  </Link>
+                </Button>
+              ) : null}
+
+              <Divider />
+
+              <Button
+                variant="outline"
+                size="lg"
+                className="text-md group w-full px-12 py-6"
+                asChild
+              >
+                <Link href={constructLoginURL({ redirectURL })}>
+                  Continue with Email{" "}
+                  <ArrowRightIcon className="ms-2 transition-all group-hover:translate-x-1" />
+                </Link>
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
       <div className="relative hidden flex-1 flex-col items-center gap-y-4 pt-52 text-center text-white xl:flex">
