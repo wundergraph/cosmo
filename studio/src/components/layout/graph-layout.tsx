729f0b3887ef315641e5981a9a950168a48f109e
JivusAyrus <suvijsurya76@gmail.com>
2023-11-15 15:18:28 +0530

    fix: remove free trial banner

--- studio/src/components/layout/graph-layout.tsx
+++ studio/src/components/layout/graph-layout.tsx
@@ -138,22 +138,6 @@ const GraphLayout = ({ children }: LayoutProps) => {
   return (
     <div className="2xl:flex 2xl:flex-1 2xl:flex-col 2xl:items-center">
       <div className="min-h-screen w-full max-w-screen-4xl bg-background font-sans antialiased">
-        {user?.currentOrganization.isFreeTrial && (
-          <Link
-            className="sticky top-0 z-50 flex cursor-pointer justify-center rounded bg-primary px-2 py-1 text-sm text-secondary-foreground"
-            href={calURL}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span>
-              Limited trial version.{" "}
-              <span className="underline underline-offset-2">
-                Talk to sales
-              </span>{" "}
-              for Production use.
-            </span>
-          </Link>
-        )}
         <Nav links={links}>{render}</Nav>
       </div>
     </div>
