996f7272d469123e095e020a5a7c787f432d215d
StarpTech <deusdustin@gmail.com>
2023-11-20 12:01:19 +0100

    fix: move to bitnami charts and exit 1 on migration issues

--- controlplane/src/bin/ch-migrate.ts
+++ controlplane/src/bin/ch-migrate.ts
@@ -10,9 +10,13 @@ if (!CLICKHOUSE_DSN) {
 
 console.log(`Migrating ClickHouse database`);
 
-const child = execSync(
-  `dbmate --wait --wait-timeout 30s -u '${CLICKHOUSE_DSN}' --no-dump-schema -d clickhouse/migrations up`,
-);
-console.log(child.toString());
-
-console.log(`ClickHouse database migrated`);
+try {
+  const child = execSync(
+    `dbmate --wait --wait-timeout 30s -u '${CLICKHOUSE_DSN}' --no-dump-schema -d clickhouse/migrations up`,
+  );
+  console.log(child.toString());
+  console.log(`ClickHouse database migrated`);
+} catch (error: any) {
+  console.error(`ClickHouse database migration failed with exit code ${error.status}\n\tDetails: ${error.message}`);
+  process.exit(1);
+}
