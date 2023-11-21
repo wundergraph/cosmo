5fa0833f8e840ff5bfaf39005a30e25914b2126c
Pagebakers <eelco@appulse.nl>
2023-11-21 13:41:58 +0100

    fix: update keycloak theme

--- keycloak/theme/tailwind.config.js
+++ keycloak/theme/tailwind.config.js
@@ -1,25 +1,33 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx,ftl,scss,properties}"],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,ftl,scss,properties}'],
   theme: {
     extend: {
       colors: {
         gray: {
-          50: "#F9F9FB",
-          100: "#F3F3F6",
-          200: "#E5E5EB",
-          300: "#D2D1DB",
-          400: "#9D9CB0",
-          500: "#6B6B80",
-          600: "#4B4B63",
-          700: "#393852",
-          800: "#201F38",
-          850: "#1A1A38",
-          900: "#171632",
-          950: "#101023",
+          50: 'hsl(258, 16%, 94%)',
+          100: 'hsl(258, 14%, 91%)',
+          200: 'hsl(258, 12%, 83%)',
+          300: 'hsl(258, 10%, 71%)',
+          400: 'hsl(258, 10%, 57%)',
+          500: 'hsl(258, 10%, 45%)',
+          600: 'hsl(258, 10%, 36%)',
+          700: 'hsl(258, 12%, 29%)',
+          800: 'hsl(258, 14%, 24%)',
+          850: 'hsl(258, 14%, 17%)',
+          900: 'hsl(258, 21%, 9%)',
+          950: 'hsl(273, 52%, 4%)',
+        },
+        primary: {
+          DEFAULT: 'hsl(258, 100%, 50%)',
+          foreground: 'hsl(258, 100%, 100%)',
+        },
+        link: {
+          DEFAULT: 'hsl(258, 12%, 83%)',
+          hover: '#ffffff',
         },
       },
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require('@tailwindcss/forms')],
 };
