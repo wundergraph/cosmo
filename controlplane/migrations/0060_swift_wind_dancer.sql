4c4efbf7169d74efd1e299db9159ca80a72bff1a
fiam <alberto@garciahierro.com>
2023-11-27 15:01:08 +0000

    chore: add comment about edited migration

--- controlplane/migrations/0060_swift_wind_dancer.sql
+++ controlplane/migrations/0060_swift_wind_dancer.sql
@@ -1,5 +1,7 @@
 ALTER TABLE "federated_graph_persisted_operations" DROP CONSTRAINT "federated_graph_operation_hash";--> statement-breakpoint
 ALTER TABLE "federated_graph_persisted_operations" DROP CONSTRAINT "federated_graph_operation_file_hash";--> statement-breakpoint
+-- In order to migrate the existing data, create the column first allowing NULL values,
+-- update it and finally disable NULL entries.
 ALTER TABLE "federated_graph_persisted_operations" ADD COLUMN "operation_id" text;--> statement-breakpoint
 UPDATE "federated_graph_persisted_operations" SET "operation_id" = "hash";
 ALTER TABLE "federated_graph_persisted_operations" ALTER COLUMN "operation_id" SET NOT NULL;--> statement-breakpoint
