4b34f092259bfe08d1d4a876b97d800f4504a2b2
fiam <alberto@garciahierro.com>
2023-11-28 16:14:35 +0000

    feat: add helm chart for CDN

--- cdn-server/src/index.ts
+++ cdn-server/src/index.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
-import { cdn } from '../cdn/src/index';
+import { cdn } from '@wundergraph/cosmo-cdn';
 import { createS3BlobStorage } from './s3';
 
 dotenv.config();
@@ -22,5 +22,22 @@ cdn(app, {
   blobStorage,
 });
 
+let exiting = false;
+app.get('/health', (c) => {
+  if (exiting) {
+    c.status(503);
+    return c.json({ status: 'exiting' });
+  }
+  return c.json({ status: 'ok' })
+});
+
 const port = process.env.PORT ? Number.parseInt(process.env.PORT, 10) : 8787;
-serve({ fetch: app.fetch, port });
+const server = serve({ fetch: app.fetch, port });
+const exit = () => {
+  exiting = true;
+  server.close();
+  // eslint-disable-next-line unicorn/no-process-exit
+  process.exit(0);
+};
+process.on('SIGTERM', exit);
+process.on('SIGINT', exit);
