24d25d9b80970efe6ffcb399f82ca513d6b15e1b
StarpTech <deusdustin@gmail.com>
2023-11-29 19:16:25 +0100

    chore: improve

--- router/core/router.go
+++ router/core/router.go
@@ -741,8 +741,8 @@ func (r *Router) newServer(ctx context.Context, routerConfig *nodev1.RouterConfi
 		Headers:   r.headerRules,
 	}
 
-	if r.developmentMode {
-		r.logger.Debug("Request tracing is enabled but requires controlplane token to function in production. For more information see https://wundergraph.com/docs/advanced/request-tracing")
+	if r.developmentMode && r.engineExecutionConfiguration.EnableRequestTracing {
+		r.logger.Debug("Request tracing is enabled in development mode but requires a graph token to work securely in production. For more information see https://wundergraph.com/docs/advanced/request-tracing")
 	}
 
 	executor, err := ecb.Build(ctx, routerConfig, routerEngineConfig)
