12ed7820417532fdf91ee7434a6ec9e576627f6e
Aenimus <daviditstutt@gmail.com>
2023-12-12 22:40:16 +0000

    chore: add tests

--- composition/tests/objects.test.ts
+++ composition/tests/objects.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
 import { noFieldDefinitionsError, normalizeSubgraphFromString } from '../src';
+import { normalizeString, versionOneBaseSchema } from './utils/utils';
 
 describe('Objects tests', () => {
   describe('Normalization tests', () => {
@@ -11,7 +12,7 @@ describe('Objects tests', () => {
       expect(errors![0]).toStrictEqual(noFieldDefinitionsError('object', 'Object'));
     });
 
-    test('that an error is returned if an extended interface does not have any fields', () => {
+    test('that an error is returned if an extended object does not have any fields', () => {
       const { errors } = normalizeSubgraphFromString(`
         type Object
         
@@ -19,6 +20,34 @@ describe('Objects tests', () => {
       `);
       expect(errors).toBeDefined();
       expect(errors![0]).toStrictEqual(noFieldDefinitionsError('object', 'Object'));
+    });
+
+    test('that a Query root type that defines no fields is valid', () => {
+      const { errors, normalizationResult } = normalizeSubgraphFromString(`
+        type Query
+      `);
+      expect(errors).toBeUndefined();
+      expect(normalizeString(normalizationResult!.subgraphString)).toBe(normalizeString(versionOneBaseSchema + `
+        type Query
+      `));
+    });
+
+    test('that a renamed Query root type that defines no fields is valid', () => {
+      const { errors, normalizationResult } = normalizeSubgraphFromString(`
+        schema {
+          query: Queries
+        }
+        
+        type Queries
+      `);
+      expect(errors).toBeUndefined();
+      expect(normalizeString(normalizationResult!.subgraphString)).toBe(normalizeString(versionOneBaseSchema + `
+        schema {
+          query: Queries
+        }
+        
+        type Queries
+      `));
     });
   });
 });
