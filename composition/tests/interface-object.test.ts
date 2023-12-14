19cabde5063a89024f99e926d534b08e784025ea
fiam <alberto@garciahierro.com>
2023-12-14 15:50:57 +0000

    chore: enable code formatting with prettier for composition library

--- composition/tests/interface-object.test.ts
+++ composition/tests/interface-object.test.ts
@@ -13,7 +13,10 @@ describe('@interfaceObject Tests', () => {
     const { errors, federationResult } = federateSubgraphs([subgraphC, subgraphD]);
     expect(errors).toBeUndefined();
     const federatedGraph = federationResult!.federatedGraphAST;
-    expect(documentNodeToNormalizedString(federatedGraph)).toBe(normalizeString(versionTwoPersistedBaseSchema + `
+    expect(documentNodeToNormalizedString(federatedGraph)).toBe(
+      normalizeString(
+        versionTwoPersistedBaseSchema +
+          `
       interface Interface {
         id: ID!
         name: String!
@@ -29,16 +32,18 @@ describe('@interfaceObject Tests', () => {
         name: String!
         age: Int!
       }
-    `));
+    `,
+      ),
+    );
   });
 
   test('that fields contributed by an interface object are added to each concrete type', () => {
-    const { errors, federationResult } = federateSubgraphs(
-      [subgraphA, subgraphB],
-    );
+    const { errors, federationResult } = federateSubgraphs([subgraphA, subgraphB]);
     expect(errors).toBeUndefined();
-    expect(documentNodeToNormalizedString(federationResult!.federatedGraphAST))
-      .toBe(normalizeString(versionTwoPersistedBaseSchema + `
+    expect(documentNodeToNormalizedString(federationResult!.federatedGraphAST)).toBe(
+      normalizeString(
+        versionTwoPersistedBaseSchema +
+          `
       interface Interface {
         id: ID!
         name: String!
@@ -54,24 +59,18 @@ describe('@interfaceObject Tests', () => {
         name: String!
         age: Int!
       }
-    `));
+    `,
+      ),
+    );
   });
 
-  test.skip('that an error is returned if a type declared with @interfaceObject is not an interface in other subgraphs', () => {
-  });
+  test.skip('that an error is returned if a type declared with @interfaceObject is not an interface in other subgraphs', () => {});
 
+  test.skip('that an error is returned if a type declared with @interfaceObject is not an entity', () => {});
 
-  test.skip('that an error is returned if a type declared with @interfaceObject is not an entity' ,() => {
+  test.skip('that an error is returned if an interface object does not include the same primary keys as its interface definition', () => {});
 
-  });
-
-  test.skip('that an error is returned if an interface object does not include the same primary keys as its interface definition' ,() => {
-
-  });
-
-  test.skip('that an error is returned if the concerete types that implement the entity interface are present in the same graph as the interface object', () => {
-
-  });
+  test.skip('that an error is returned if the concerete types that implement the entity interface are present in the same graph as the interface object', () => {});
 });
 
 const subgraphA: Subgraph = {
