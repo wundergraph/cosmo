69433d487c31fe0397bba05c7e6794275d6e7233
Pagebakers <eelco@appulse.nl>
2023-12-27 14:13:28 +0100

    chore: improved billing seed script

--- controlplane/src/bin/seed-billing.ts
+++ controlplane/src/bin/seed-billing.ts
@@ -1,17 +1,39 @@
+#!/usr/bin/env node
+
 import 'dotenv/config';
 
+import { readFileSync } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from '../db/schema.js';
-import billing from './billing.json' assert { type: 'json' };
 
 const databaseConnectionUrl = process.env.DB_URL || 'postgresql://postgres:changeme@localhost:5432/controlplane';
 
-const seedBilling = () => {
+const seedBilling = async () => {
   const queryConnection = postgres(databaseConnectionUrl);
   const db = drizzle(queryConnection, { schema: { ...schema } });
 
-  const entries = Object.entries(billing.plans);
+  const configPath = process.argv[2]
+    ? path.resolve(process.cwd(), process.argv[2])
+    : path.join(path.dirname(fileURLToPath(import.meta.url)), 'billing.json');
+
+  const data = readFileSync(configPath, 'utf8');
+  const json = JSON.parse(data);
+
+  const entries = Object.entries<{
+    name: string;
+    price: number;
+    active: boolean;
+    weight: number;
+    stripePriceId?: string;
+    features: {
+      id: string;
+      description: string;
+      limit?: number;
+    }[];
+  }>(json.plans);
 
   for (const [id, plan] of entries) {
     const values = {
@@ -23,16 +45,22 @@ const seedBilling = () => {
       stripePriceId: 'stripePriceId' in plan ? plan.stripePriceId : undefined,
       features: plan.features,
     };
-    db.insert(schema.billingPlans)
+
+    await db
+      .insert(schema.billingPlans)
       .values(values)
       .onConflictDoUpdate({
         target: schema.billingPlans.id,
         set: values,
       })
       .execute();
+
+    console.log('Synced billing plan', id);
   }
 
-  return true;
+  console.log('Seed completed');
+
+  process.exit(0);
 };
 
 await seedBilling();
