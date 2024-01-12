55099382ae6acea6df85e03b42f4265f67f56deb
thisisnithin <nithinkumar5353@gmail.com>
2024-01-12 17:12:28 +0530

    add test

---
+++ studio/src/__tests__/schema-helpers.test.ts
@@ -0,0 +1,36 @@
+import { getDeprecatedTypes, parseSchema } from "../lib/schema-helpers";
+import { expect, test } from "vitest";
+
+const schema = `
+type Query {
+  employees: [Employee!]!
+  teammates(team: Department! @deprecated): [Employee!]!
+}
+
+enum Department {
+  ENGINEERING
+  MARKETING
+  OPERATIONS
+}
+
+type Employee {
+  id: Int!
+  firstName: String!
+  lastName: String!
+  fullName: String! @deprecated(reason: "Please use first and last name instead")
+}
+`;
+
+test("return the correct types with deprecated fields or args", async () => {
+  const ast = await parseSchema(schema);
+
+  expect(ast).not.toBeNull();
+
+  const deprecated = getDeprecatedTypes(ast!);
+
+  expect(deprecated.length).toEqual(2);
+  expect(deprecated[0].fields?.length).toEqual(1);
+  expect(deprecated[0].fields?.[0]?.name).toEqual("teammates");
+  expect(deprecated[1].fields?.length).toEqual(1);
+  expect(deprecated[1].fields?.[0]?.name).toEqual("fullName");
+});
