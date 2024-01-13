5a0a25e2dd46761459bfa080aa6495a1a82e8549
Dustin Deus <deusdustin@gmail.com>
2024-01-13 18:22:53 +0100

    feat: support custom trace propagation (#420)

--- router/internal/trace/propagation.go
+++ router/internal/trace/propagation.go
@@ -1,11 +1,27 @@
 package trace
 
 import (
-	"go.opentelemetry.io/otel"
+	"fmt"
+	"go.opentelemetry.io/contrib/propagators/b3"
+	"go.opentelemetry.io/contrib/propagators/jaeger"
 	"go.opentelemetry.io/otel/propagation"
 )
 
-func init() {
-	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
-		propagation.TraceContext{}, propagation.Baggage{}))
+func NewCompositePropagator(propagators ...Propagator) (propagation.TextMapPropagator, error) {
+	var allPropagators []propagation.TextMapPropagator
+	for _, p := range propagators {
+		switch p {
+		case PropagatorTraceContext:
+			allPropagators = append(allPropagators, propagation.TraceContext{})
+		case PropagatorB3:
+			allPropagators = append(allPropagators, b3.New(b3.WithInjectEncoding(b3.B3MultipleHeader|b3.B3SingleHeader)))
+		case PropagatorJaeger:
+			allPropagators = append(allPropagators, jaeger.Jaeger{})
+		case PropagatorBaggage:
+			allPropagators = append(allPropagators, propagation.Baggage{})
+		default:
+			return nil, fmt.Errorf("unknown trace propagator: %s", p)
+		}
+	}
+	return propagation.NewCompositeTextMapPropagator(allPropagators...), nil
 }
