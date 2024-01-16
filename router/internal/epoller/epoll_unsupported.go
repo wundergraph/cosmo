7c0da0fd6d74e1d18039ad7b9e3a65c4e5c52355
StarpTech <deusdustin@gmail.com>
2024-01-16 19:55:45 +0100

    fix: poller interface for Windows

--- router/internal/epoller/epoll_unsupported.go
+++ router/internal/epoller/epoll_unsupported.go
@@ -3,9 +3,12 @@
 
 package epoller
 
-import "errors"
+import (
+	"errors"
+	"time"
+)
 
 // NewPoller creates a new epoll poller.
-func NewPoller(connBufferSize int) (Poller, error) {
+func NewPoller(connBufferSize int, _ time.Duration) (Poller, error) {
 	return nil, errors.New("epoll is not supported on windows")
 }
