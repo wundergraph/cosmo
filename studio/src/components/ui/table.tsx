e014c0896dd017cf4db6a2c5f2c2d83b1fc86017
Suvij Surya <suvijsurya76@gmail.com>
2024-01-23 20:49:32 +0530

    feat: implement pagination and date filter for audit logs (#444)

--- studio/src/components/ui/table.tsx
+++ studio/src/components/ui/table.tsx
@@ -33,7 +33,11 @@ const TableHeader = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <thead ref={ref} className={cn("[&_tr]:border-b", className)} {...props} />
+  <thead
+    ref={ref}
+    className={cn("sticky top-0 bg-background [&_tr]:border-b", className)}
+    {...props}
+  />
 ));
 TableHeader.displayName = "TableHeader";
 
