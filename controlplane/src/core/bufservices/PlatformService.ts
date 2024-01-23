ac7e99c31dff2037e60ce3de938b8eb84a0f7af1
Suvij Surya <suvijsurya76@gmail.com>
2024-01-23 20:49:32 +0530

    feat: implement pagination and date filter for audit logs (#444)

--- controlplane/src/core/bufservices/PlatformService.ts
+++ controlplane/src/core/bufservices/PlatformService.ts
@@ -5766,6 +5766,7 @@ export default function (opts: RouterOptions): Partial<ServiceImpl<typeof Platfo
 
       return handleError<PlainMessage<GetAuditLogsResponse>>(logger, async () => {
         const authContext = await opts.authenticator.authenticate(ctx.requestHeader);
+        const orgRepo = new OrganizationRepository(opts.db);
         const auditLogRepo = new AuditLogRepository(opts.db);
 
         if (!authContext.isAdmin) {
@@ -5775,6 +5776,43 @@ export default function (opts: RouterOptions): Partial<ServiceImpl<typeof Platfo
               details: `The user doesnt have the permissions to perform this operation`,
             },
             logs: [],
+            count: 0,
+          };
+        }
+
+        const analyticsRetention = await orgRepo.getFeature({
+          organizationId: authContext.organizationId,
+          featureId: 'analytics-retention',
+        });
+
+        const { dateRange } = validateDateRanges({
+          limit: analyticsRetention?.limit ?? 7,
+          dateRange: {
+            start: req.startDate,
+            end: req.endDate,
+          },
+        });
+
+        if (!dateRange) {
+          return {
+            response: {
+              code: EnumStatusCode.ERR,
+              details: 'Invalid date range',
+            },
+            logs: [],
+            count: 0,
+          };
+        }
+
+        // check that the limit is less than the max option provided in the ui
+        if (req.limit > 50) {
+          return {
+            response: {
+              code: EnumStatusCode.ERR,
+              details: 'Invalid limit',
+            },
+            logs: [],
+            count: 0,
           };
         }
 
@@ -5782,6 +5820,13 @@ export default function (opts: RouterOptions): Partial<ServiceImpl<typeof Platfo
           organizationId: authContext.organizationId,
           limit: req.limit,
           offset: req.offset,
+          startDate: dateRange.start,
+          endDate: dateRange.end,
+        });
+        const auditLogsCount = await auditLogRepo.getAuditLogsCount({
+          organizationId: authContext.organizationId,
+          startDate: dateRange.start,
+          endDate: dateRange.end,
         });
 
         const logs: PlainMessage<AuditLog>[] = auditLogs.map((log) => ({
@@ -5801,6 +5846,7 @@ export default function (opts: RouterOptions): Partial<ServiceImpl<typeof Platfo
             code: EnumStatusCode.OK,
           },
           logs,
+          count: auditLogsCount,
         };
       });
     },
--- studio/src/pages/[organizationSlug]/audit-log.tsx
+++ studio/src/pages/[organizationSlug]/audit-log.tsx
@@ -1,20 +1,85 @@
-import { getDashboardLayout } from "@/components/layout/dashboard-layout";
-import { Loader } from "@/components/ui/loader";
-import { NextPageWithLayout } from "@/lib/page";
-import { useQuery } from "@tanstack/react-query";
-import { getAuditLogs } from "@wundergraph/cosmo-connect/dist/platform/v1/platform-PlatformService_connectquery";
 import { AuditLogTable, Empty } from "@/components/audit-log-table";
+import {
+  DatePickerWithRange,
+  DateRangePickerChangeHandler,
+} from "@/components/date-picker-with-range";
+import { EmptyState } from "@/components/empty-state";
+import { getDashboardLayout } from "@/components/layout/dashboard-layout";
+import { Button } from "@/components/ui/button";
+import { Loader } from "@/components/ui/loader";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Toolbar } from "@/components/ui/toolbar";
+import { useFeatureLimit } from "@/hooks/use-feature-limit";
+import { NextPageWithLayout } from "@/lib/page";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+  ExclamationTriangleIcon,
+} from "@radix-ui/react-icons";
+import { useQuery } from "@tanstack/react-query";
 import { EnumStatusCode } from "@wundergraph/cosmo-connect/dist/common/common_pb";
-import { useUser } from "@/hooks/use-user";
+import { getAuditLogs } from "@wundergraph/cosmo-connect/dist/platform/v1/platform-PlatformService_connectquery";
+import { endOfDay, formatISO, startOfDay, subDays } from "date-fns";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
+const useDateRange = () => {
+  const router = useRouter();
+
+  const dateRange = router.query.dateRange
+    ? JSON.parse(router.query.dateRange as string)
+    : {
+        start: subDays(new Date(), 7),
+        end: new Date(),
+      };
+  const startDate = new Date(dateRange.start);
+  const endDate = new Date(dateRange.end);
+
+  return {
+    startDate,
+    endDate,
+  };
+};
 
 const AuditLogPage: NextPageWithLayout = () => {
-  const user = useUser();
-  const { data, isLoading, error } = useQuery({
+  const router = useRouter();
+  const pageNumber = router.query.page
+    ? parseInt(router.query.page as string)
+    : 1;
+
+  const limit = Number.parseInt((router.query.pageSize as string) || "10");
+
+  const { startDate, endDate } = useDateRange();
+
+  const { data, isLoading, error, refetch } = useQuery({
     ...getAuditLogs.useQuery({
-      limit: 100,
+      limit: limit > 50 ? 50 : limit,
+      offset: (pageNumber - 1) * limit,
+      startDate: formatISO(startOfDay(startDate)),
+      endDate: formatISO(endOfDay(endDate)),
     }),
-    queryKey: [user?.currentOrganization.slug || "", "GetAuditLogs", {}],
+    queryKey: [router.asPath, "GetAuditLogs", {}],
   });
+
+  const applyNewParams = useCallback(
+    (newParams: Record<string, string>) => {
+      router.push({
+        query: {
+          ...router.query,
+          ...newParams,
+        },
+      });
+    },
+    [router],
+  );
 
   if (isLoading) return <Loader fullscreen />;
 
@@ -22,12 +87,129 @@ const AuditLogPage: NextPageWithLayout = () => {
     return <Empty unauthorized={true} />;
   }
 
+  if (error || data?.response?.code !== EnumStatusCode.OK)
+    return (
+      <EmptyState
+        icon={<ExclamationTriangleIcon className="w-10 h-10" />}
+        title="Could not retrieve the members of this organization."
+        description={
+          data?.response?.details || error?.message || "Please try again"
+        }
+        actions={<Button onClick={() => refetch()}>Retry</Button>}
+      />
+    );
+
   if (!data?.logs.length) return <Empty unauthorized={false} />;
 
+  const noOfPages = Math.ceil(data.count / limit);
+
   return (
-    <div className="flex flex-col gap-y-4 pt-2">
+    <div className="flex h-full flex-col gap-y-4">
       <AuditLogTable logs={data?.logs} />
+      <div className="mr-2 flex justify-end">
+        <div className="flex items-center space-x-2">
+          <p className="text-sm font-medium">Rows per page</p>
+          <Select
+            value={`${limit}`}
+            onValueChange={(value) => {
+              applyNewParams({ pageSize: value });
+            }}
+          >
+            <SelectTrigger className="h-8 w-[70px]">
+              <SelectValue placeholder={`${limit}`} />
+            </SelectTrigger>
+            <SelectContent side="top">
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <SelectItem key={pageSize} value={`${pageSize}`}>
+                  {pageSize}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+          Page {noOfPages === 0 ? "0" : pageNumber} of {noOfPages}
+        </div>
+        <div className="flex items-center space-x-2">
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => {
+              applyNewParams({ page: "1" });
+            }}
+            disabled={pageNumber === 1}
+          >
+            <span className="sr-only">Go to first page</span>
+            <DoubleArrowLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => {
+              applyNewParams({ page: (pageNumber - 1).toString() });
+            }}
+            disabled={pageNumber === 1}
+          >
+            <span className="sr-only">Go to previous page</span>
+            <ChevronLeftIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="h-8 w-8 p-0"
+            onClick={() => {
+              applyNewParams({ page: (pageNumber + 1).toString() });
+            }}
+            disabled={pageNumber === noOfPages || noOfPages === 0}
+          >
+            <span className="sr-only">Go to next page</span>
+            <ChevronRightIcon className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="outline"
+            className="hidden h-8 w-8 p-0 lg:flex"
+            onClick={() => {
+              applyNewParams({ page: noOfPages.toString() });
+            }}
+            disabled={pageNumber === noOfPages || noOfPages === 0}
+          >
+            <span className="sr-only">Go to last page</span>
+            <DoubleArrowRightIcon className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
     </div>
+  );
+};
+
+const AuditLogToolbar = () => {
+  const router = useRouter();
+
+  const { startDate, endDate } = useDateRange();
+
+  const onDateRangeChange: DateRangePickerChangeHandler = ({ dateRange }) => {
+    const stringifiedDateRange = JSON.stringify({
+      start: dateRange?.start as Date,
+      end: (dateRange?.end as Date) ?? (dateRange?.end as Date),
+    });
+
+    router.push({
+      query: {
+        ...router.query,
+        dateRange: stringifiedDateRange,
+      },
+    });
+  };
+
+  const auditLogRetention = useFeatureLimit("analytics-retention", 7);
+
+  return (
+    <Toolbar>
+      <DatePickerWithRange
+        dateRange={{ start: startDate, end: endDate }}
+        onChange={onDateRangeChange}
+        calendarDaysLimit={auditLogRetention}
+      />
+    </Toolbar>
   );
 };
 
@@ -36,6 +218,8 @@ AuditLogPage.getLayout = (page) => {
     page,
     "Audit log",
     "Audit log of your organization",
+    undefined,
+    <AuditLogToolbar />,
   );
 };
 
