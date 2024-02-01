40beab0e073e730754e302d765139ab4268bc960
Dustin Deus <deusdustin@gmail.com>
2024-02-01 17:29:50 +0100

    fix: support tls when migrate (#479)

--- controlplane/src/core/migrate.ts
+++ controlplane/src/core/migrate.ts
@@ -1,11 +1,14 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import { buildDatabaseConnectionConfig, DatabaseConnectionConfig } from './plugins/database.js';
 
-export async function RunMigration(databaseConnectionUrl: string) {
+export async function RunMigration(databaseConnectionUrl: string, opts?: DatabaseConnectionConfig) {
+  const connectionConfig = await buildDatabaseConnectionConfig(opts);
   const migrationConnection = postgres(databaseConnectionUrl, {
+    ...connectionConfig,
+    onnotice: (_) => {},
     max: 1,
-    onnotice(notice) {},
   });
 
   await migrate(drizzle(migrationConnection), {
