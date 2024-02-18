de8163e6c8ab1f4ffb8af751fbde323ee0efec44
Dustin Deus <deusdustin@gmail.com>
2024-02-18 14:24:40 +0100

    feat(metrics): collect OS type and enrich traces with resource instan… (#546)

--- router/pkg/metric/meter.go
+++ router/pkg/metric/meter.go
@@ -219,6 +219,7 @@ func getResource(ctx context.Context, serviceName, serviceVersion string, servic
 		resource.WithAttributes(semconv.ServiceVersionKey.String(serviceVersion)),
 		resource.WithAttributes(semconv.ServiceInstanceID(serviceInstanceID)),
 		resource.WithProcessPID(),
+		resource.WithOSType(),
 		resource.WithTelemetrySDK(),
 		resource.WithHost(),
 	)
