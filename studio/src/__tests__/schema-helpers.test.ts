0de20bee0077393c7d123335c61e08c58fa1e88b
Nithin Kumar B <nithinkumar5353@gmail.com>
2024-02-19 23:32:40 +0530

    feat: generate variables when we copy a persisted operation (#552)

--- studio/src/__tests__/schema-helpers.test.ts
+++ studio/src/__tests__/schema-helpers.test.ts
@@ -1,7 +1,10 @@
+import { parse, OperationDefinitionNode, Kind } from "graphql";
 import {
   getDeprecatedTypes,
   getTypeCounts,
   formatAndParseSchema,
+  extractVariablesFromGraphQL,
+  parseSchema,
 } from "../lib/schema-helpers";
 import { expect, test } from "vitest";
 
@@ -9,6 +12,17 @@ const schema = `
 type Query {
   employees: [Employee!]!
   teammates(team: Department! @deprecated): [Employee!]!
+  findID(criteria: Criteria!): Int!
+}
+
+input Criteria {
+  age: Int!
+  nested: Nested
+  hasPets: Boolean
+}
+
+input Nested {
+  department: Department!
 }
 
 enum Department {
@@ -39,14 +53,102 @@ test("return the correct types with deprecated fields or args", async () => {
   expect(deprecated[1].fields?.[0]?.name).toEqual("fullName");
 });
 
-test("returns correct type counts", async () => {
-  const ast = await formatAndParseSchema(schema);
+test("returns correct type counts", () => {
+  const ast = parseSchema(schema);
 
   expect(ast).not.toBeNull();
 
   const counts = getTypeCounts(ast!);
 
-  expect(counts["query"]).toEqual(2);
+  expect(counts["query"]).toEqual(3);
   expect(counts["objects"]).toEqual(1);
   expect(counts["enums"]).toEqual(1);
+});
+
+test("returns empty if no variables are present", () => {
+  const ast = parseSchema(schema);
+  expect(ast).not.toBeNull();
+
+  const query = `
+    query {
+      employees {
+        id
+      }
+    }
+  `;
+
+  const variables = extractVariablesFromGraphQL(query, ast);
+  expect(variables).toMatchObject({});
+});
+
+test("returns multiple variables", () => {
+  const ast = parseSchema(schema);
+  expect(ast).not.toBeNull();
+
+  const query = `
+    query ($a: Int, $criteria: Criteria!) {
+      employees {
+       id
+      }
+    }
+  `;
+
+  const variables = extractVariablesFromGraphQL(query, ast);
+  expect(variables).toMatchObject({
+    a: 0,
+    criteria: {
+      age: 0,
+      nested: {
+        department: "ENGINEERING",
+      },
+    },
+  });
+});
+
+test("returns multiple variables with defaults", () => {
+  const ast = parseSchema(schema);
+  expect(ast).not.toBeNull();
+
+  const query = `
+    query ($a: Int = 10, $criteria: Criteria = { age: 12, hasPets: true, nested: { department: "ENGINEERING" }}, $b: [Int] = [1,2,3]) {
+      employees {
+       id
+      }
+    }
+  `;
+
+  const variables = extractVariablesFromGraphQL(query, ast);
+
+  expect(variables).toMatchObject({
+    a: 10,
+    criteria: {
+      age: 12,
+      nested: {
+        department: "ENGINEERING",
+      },
+      hasPets: true,
+    },
+    b: [1, 2, 3],
+  });
+});
+
+test("returns nested variables", () => {
+  const ast = parseSchema(schema);
+  expect(ast).not.toBeNull();
+
+  const query = `
+    query ($criteria: Criteria) {
+      findID(criteria: $criteria)
+    }
+  `;
+
+  const variables = extractVariablesFromGraphQL(query, ast);
+  expect(variables).toMatchObject({
+    criteria: {
+      age: 0,
+      nested: {
+        department: "ENGINEERING",
+      },
+    },
+  });
 });
