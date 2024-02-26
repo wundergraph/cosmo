5d3fa523bc0bdaa446ac36dba40fa450244f1529
StarpTech <deusdustin@gmail.com>
2024-02-26 16:45:02 +0100

    Revert "chore: improve"
    This reverts commit 1f330e9d27040926e869490ec15160850c039981.

--- router/cmd/custom/module/module.go
+++ router/cmd/custom/module/module.go
@@ -53,12 +53,8 @@ func (m *MyModule) OnOriginResponse(response *http.Response, ctx core.RequestCon
 	// Return a new response or nil if you want to pass it to the next handler
 	// If you want to modify the response, return a new response
 
-	// Access the custom value set in OnOriginRequest
-	value := ctx.GetString("myValue")
-
-	fmt.Println("SharedValue", value)
-
-	fmt.Println("OnOriginResponse", response.Request.URL, response.StatusCode)
+	// Set a header on the final client response
+	ctx.ResponseWriter().Header().Set("myHeader", ctx.GetString("myValue"))
 
 	return nil
 }
