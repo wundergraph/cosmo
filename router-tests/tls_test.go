80c6c3d8ef7fbe4a03158b28096cbfa4f548c302
StarpTech <deusdustin@gmail.com>
2024-03-05 11:44:06 +0100

    chore: improve

--- router-tests/tls_test.go
+++ router-tests/tls_test.go
@@ -2,6 +2,7 @@ package integration
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"github.com/stretchr/testify/require"
 	"github.com/wundergraph/cosmo/router-tests/testenv"
 	"github.com/wundergraph/cosmo/router/core"
@@ -9,6 +10,7 @@ import (
 	"net"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"testing"
 )
@@ -18,6 +20,30 @@ const employeesIDData = `{"data":{"employees":[{"id":1},{"id":2},{"id":3},{"id":
 func TestTLS(t *testing.T) {
 
 	t.Parallel()
+
+	t.Run("Ensure router URL is https when passing TLSConfig", func(t *testing.T) {
+		t.Parallel()
+
+		testenv.Run(t, &testenv.Config{
+			TLSConfig: &core.TlsConfig{
+				Enabled:  true,
+				CertFile: "testdata/tls/cert.pem",
+				KeyFile:  "testdata/tls/key.pem",
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			require.Contains(t, xEnv.RouterURL, "https://")
+		})
+	})
+
+	t.Run("Ensure router URL is not https when nil TLSConfig is passed", func(t *testing.T) {
+		t.Parallel()
+
+		testenv.Run(t, &testenv.Config{
+			TLSConfig: nil,
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			require.Contains(t, xEnv.RouterURL, "http://")
+		})
+	})
 
 	t.Run("TestPlayground", func(t *testing.T) {
 		t.Parallel()
@@ -57,6 +83,47 @@ func TestTLS(t *testing.T) {
 				Query: `query { employees { id } }`,
 			})
 			require.JSONEq(t, employeesIDData, res.Body)
+		})
+	})
+
+	t.Run("Can't connect to server securely because client certificate is not signed be the same authority", func(t *testing.T) {
+		t.Parallel()
+
+		testenv.Run(t, &testenv.Config{
+			TLSConfig: &core.TlsConfig{
+				Enabled:  true,
+				CertFile: "testdata/tls/cert.pem",
+				KeyFile:  "testdata/tls/key.pem",
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			req, err := http.NewRequestWithContext(xEnv.Context, http.MethodPost, xEnv.RouterURL, strings.NewReader(`query { employees { id } }`))
+			require.NoError(t, err)
+
+			cert, err := tls.LoadX509KeyPair("testdata/tls/cert-2.pem", "testdata/tls/key-2.pem")
+			require.NoError(t, err)
+
+			caCert, err := os.ReadFile("testdata/tls/cert-2.pem")
+			require.NoError(t, err)
+
+			caCertPool := x509.NewCertPool()
+			if ok := caCertPool.AppendCertsFromPEM(caCert); !ok {
+				t.Fatalf("could not append ca cert to pool")
+			}
+
+			client := &http.Client{
+				Transport: &http.Transport{
+					TLSClientConfig: &tls.Config{
+						RootCAs:      caCertPool,
+						Certificates: []tls.Certificate{cert},
+					},
+				},
+			}
+			_, err = client.Do(req)
+
+			var urlErr *url.Error
+			require.ErrorAs(t, err, &urlErr)
+
+			require.ErrorContains(t, urlErr, "tls: failed to verify certificate: x509: certificate signed by unknown authority")
 		})
 	})
 
@@ -213,6 +280,27 @@ func TestMTLS(t *testing.T) {
 			require.ErrorAs(t, err, &netOpErr)
 
 			require.Error(t, netOpErr, "remote error: tls: certificate required")
+		})
+	})
+
+	t.Run("Can skip client authentication also when cert is specified", func(t *testing.T) {
+		t.Parallel()
+
+		testenv.Run(t, &testenv.Config{
+			TLSConfig: &core.TlsConfig{
+				Enabled:  true,
+				CertFile: "testdata/tls/cert.pem",
+				KeyFile:  "testdata/tls/key.pem",
+				ClientAuth: &core.TlsClientAuthConfig{
+					Verify:   false,
+					CertFile: "testdata/tls/cert.pem",
+				},
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			res := xEnv.MakeGraphQLRequestOK(testenv.GraphQLRequest{
+				Query: `query { employees { id } }`,
+			})
+			require.JSONEq(t, employeesIDData, res.Body)
 		})
 	})
 }
