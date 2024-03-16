a1ec1902eb4aa7af461ee76b63fd8bda3691c0cf
StarpTech <deusdustin@gmail.com>
2024-03-16 21:24:51 +0100

    fix: csp header

--- studio/next.config.mjs
+++ studio/next.config.mjs
@@ -2,6 +2,7 @@ import withMarkdoc from "@markdoc/next.js";
 import pkg from "./package.json" assert { type: "json" };
 
 const isProd = process.env.NODE_ENV === "production";
+const isPreview = process.env.VERCEL_ENV === "preview";
 const debugCSP = true
 
 // Content Security Policy (CSP) is a security standard that helps prevent cross-site scripting (XSS),
@@ -11,18 +12,19 @@ const debugCSP = true
 // Known provider content security policies:
 // For Stripe see https://docs.stripe.com/security/guide?csp=csp-js#content-security-policy
 // For Koala see https://getkoala.com/docs/sdk/installation
-// 'unsafe-eval' is only used in development mode, when script is injected by Next.js
+// Vercel Preview Environment see https://vercel.com/docs/workflow-collaboration/comments/specialized-usage#using-a-content-security-policy
+// Important: 'unsafe-eval' is only used in development mode, when script is injected by Next.js
 
 const cspHeader = `
   default-src 'self' ${process.env.NEXT_PUBLIC_COSMO_STUDIO_URL} ${process.env.NEXT_PUBLIC_COSMO_CP_URL};
-  script-src 'report-sample' 'self' 'unsafe-inline' ${isProd ? '' : "'unsafe-eval'"} https://js.stripe.com https://maps.googleapis.com https://plausible.io https://wundergraph.com https://*.getkoala.com
+  script-src 'report-sample' 'self' 'unsafe-inline' ${isProd ? '' : "'unsafe-eval'"} https://js.stripe.com https://maps.googleapis.com https://plausible.io https://wundergraph.com https://*.getkoala.com ${isPreview ? 'https://vercel.live/ https://vercel.com' : ''};
   style-src 'report-sample' 'self' 'unsafe-inline' data:;;
   object-src 'none';
   base-uri 'self';
-  connect-src 'self' ${process.env.NEXT_PUBLIC_COSMO_STUDIO_URL} ${process.env.NEXT_PUBLIC_COSMO_CP_URL} https://api.getkoala.com wss://*.getkoala.com https://*.getkoala.com https://api.stripe.com https://maps.googleapis.com;
+  connect-src 'self' ${process.env.NEXT_PUBLIC_COSMO_STUDIO_URL} ${process.env.NEXT_PUBLIC_COSMO_CP_URL} https://api.getkoala.com wss://*.getkoala.com https://*.getkoala.com https://api.stripe.com https://maps.googleapis.com ${isPreview ? 'https://vercel.live/ https://vercel.com *.pusher.com *.pusherapp.com' : ''};
   font-src 'self' data:;;
-  frame-src 'self' https://js.stripe.com https://hooks.stripe.com;
-  img-src 'self';
+  frame-src 'self' https://js.stripe.com https://hooks.stripe.com ${isPreview ? 'https://vercel.live/ https://vercel.com' : ''};
+  img-src 'self' ${isPreview ? 'https://vercel.live/ https://vercel.com *.pusher.com/ data: blob:' : ''};
   manifest-src 'self';
   media-src 'self';
   worker-src 'none';
