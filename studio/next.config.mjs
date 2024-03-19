82a1781153cd07fe260bca4e81c72b1fc7662117
Dustin Deus <deusdustin@gmail.com>
2024-03-19 18:41:29 +0100

    fix: soften CSP on playground page (#653)

--- studio/next.config.mjs
+++ studio/next.config.mjs
@@ -3,9 +3,9 @@ import pkg from "./package.json" assert { type: "json" };
 
 const isPreview = process.env.VERCEL_ENV === "preview";
 // Allow it only for development once https://github.com/vercel/next.js/issues/23587 is fixed
-const allowUnsafeEval = true
+const allowUnsafeEval = true;
 // Report CSP violations to the console instead of blocking them
-const debugCSP = false
+const debugCSP = false;
 
 // Content Security Policy (CSP) is a security standard that helps prevent cross-site scripting (XSS),
 // clickjacking, and other code injection attacks resulting from execution of malicious content
@@ -16,46 +16,77 @@ const debugCSP = false
 // Vercel Preview Environment see https://vercel.com/docs/workflow-collaboration/comments/specialized-usage#using-a-content-security-policy
 // Important: 'unsafe-eval' is only used in development mode, when script is injected by Next.js
 
-const cspHeader = `
-  default-src 'self' ${process.env.NEXT_PUBLIC_COSMO_STUDIO_URL} ${process.env.NEXT_PUBLIC_COSMO_CP_URL};
-  script-src 'report-sample' 'self' 'unsafe-inline' ${allowUnsafeEval ? "'unsafe-eval'" : ''} https://*.wundergraph.com https://js.stripe.com https://maps.googleapis.com https://plausible.io https://wundergraph.com ${isPreview ? 'https://vercel.live https://vercel.com' : ''};
+const lightweightCspHeader = `
   style-src 'report-sample' 'self' 'unsafe-inline' data:;;
   object-src 'none';
   base-uri 'self';
-  connect-src 'self' ${process.env.NEXT_PUBLIC_COSMO_STUDIO_URL} ${process.env.NEXT_PUBLIC_COSMO_CP_URL} https://*.wundergraph.com wss://*.wundergraph.com https://plausible.io https://api.stripe.com https://maps.googleapis.com ${isPreview ? 'https://vercel.live https://vercel.com *.pusher.com *.pusherapp.com' : ''};
   font-src 'self' data:;;
-  frame-src 'self' https://js.stripe.com https://hooks.stripe.com ${isPreview ? 'https://vercel.live/ https://vercel.com' : ''};
-  img-src 'self' ${isPreview ? 'https://vercel.live/ https://vercel.com *.pusher.com/ data: blob:' : ''};
+  frame-src 'self' https://js.stripe.com https://hooks.stripe.com ${
+    isPreview ? "https://vercel.live/ https://vercel.com" : ""
+  };
+  img-src 'self' ${
+    isPreview
+      ? "https://vercel.live/ https://vercel.com *.pusher.com/ data: blob:"
+      : ""
+  };
+   script-src 'report-sample' 'self' 'unsafe-inline' ${
+    allowUnsafeEval ? "'unsafe-eval'" : ""
+  } https://*.wundergraph.com https://js.stripe.com https://maps.googleapis.com https://plausible.io https://wundergraph.com ${
+      isPreview ? "https://vercel.live https://vercel.com" : ""
+  };
   manifest-src 'self';
   media-src 'self';
   worker-src 'self';
-`
+`;
+
+const fullCspHeader = `
+  default-src 'self' ${process.env.NEXT_PUBLIC_COSMO_STUDIO_URL} ${
+    process.env.NEXT_PUBLIC_COSMO_CP_URL
+  };
+  connect-src 'self' ${process.env.NEXT_PUBLIC_COSMO_STUDIO_URL} ${
+    process.env.NEXT_PUBLIC_COSMO_CP_URL
+  } https://*.wundergraph.com wss://*.wundergraph.com https://plausible.io https://api.stripe.com https://maps.googleapis.com ${
+    isPreview
+      ? "https://vercel.live https://vercel.com *.pusher.com *.pusherapp.com"
+      : ""
+  };
+  ${lightweightCspHeader}
+`;
 
 /** @type {import("next").NextConfig} */
 const config = {
   output: "standalone",
   pageExtensions: ["md", "mdoc", "js", "jsx", "ts", "tsx"],
-  headers: () => {
-
-    // Replace newline characters and spaces
-    const contentSecurityPolicyHeaderValue = cspHeader
-        .replace(/\s{2,}/g, ' ')
-        .trim()
-
+  publicRuntimeConfig: {
+    version: pkg.version,
+  },
+  async headers() {
     return [
       {
         source: "/(.*)",
         headers: [
           {
-            key: debugCSP ? "Content-Security-Policy-Report-Only" : "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, ''),
+            key: debugCSP
+                ? "Content-Security-Policy-Report-Only"
+                : "Content-Security-Policy",
+            value: fullCspHeader.replace(/\n/g, ""),
           },
         ],
       },
+      {
+        // Allow the playground to make requests to the public router that can be hosted on a different domain
+        source: "/(.*)/playground",
+        headers: [
+          {
+            key: debugCSP
+              ? "Content-Security-Policy-Report-Only"
+              : "Content-Security-Policy",
+            value: lightweightCspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+
     ];
-  },
-  publicRuntimeConfig: {
-    version: pkg.version,
   },
 };
 
