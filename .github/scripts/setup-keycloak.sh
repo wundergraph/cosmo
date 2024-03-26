295d13bb4d6f6520779b1b051f62f53f604a2b4b
JivusAyrus <suvijsurya76@gmail.com>
2024-03-27 01:44:13 +0530

    fix: ci

--- .github/scripts/setup-keycloak.sh
+++ .github/scripts/setup-keycloak.sh
@@ -3,7 +3,7 @@
 export KC_VERSION=22.0.4
 curl -LO  https://github.com/keycloak/keycloak/releases/download/"${KC_VERSION}"/keycloak-"${KC_VERSION}".zip
 
-unzip keycloak-${KC_VERSION}.zip
+unzip -q keycloak-${KC_VERSION}.zip
 
 cd keycloak-${KC_VERSION}
 
