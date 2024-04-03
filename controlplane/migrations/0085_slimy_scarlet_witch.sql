29ac9dd33e0630f50b1850afa1ef201a5cf0faee
JivusAyrus <suvijsurya76@gmail.com>
2024-04-03 18:52:17 +0530

    fix: merge migrations

--- controlplane/migrations/0085_slimy_scarlet_witch.sql
+++ controlplane/migrations/0085_slimy_scarlet_witch.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS "api_key_permissions" (
 	"permission" text NOT NULL
 );
 --> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "active" boolean DEFAULT true NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "updated_at" timestamp with time zone;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "api_key_permissions" ADD CONSTRAINT "api_key_permissions_api_key_id_api_keys_id_fk" FOREIGN KEY ("api_key_id") REFERENCES "api_keys"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
--- controlplane/migrations/0085_typical_loners.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "active" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "updated_at" timestamp with time zone;
