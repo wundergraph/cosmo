490038b8f90982db836cd4603e706047a641d2f2
starptech <deusdustin@gmail.com>
2024-04-03 19:45:55 +0200

    feat: use offline token, refresh token implementation

--- cli/src/commands/auth/utils.ts
+++ cli/src/commands/auth/utils.ts
@@ -1,4 +1,7 @@
+import { program } from 'commander';
+import pc from 'picocolors';
 import { config } from '../../core/config.js';
+import { readConfigFile, updateConfigFile } from '../../utils.js';
 
 interface DeviceAuthResponse {
   deviceCode: string;
@@ -12,6 +15,13 @@ export interface KeycloakToken {
   refreshToken: string;
   expiresAt: Date;
   refreshExpiresAt: Date;
+}
+
+export interface KeycloakTokenResponse {
+  access_token: string;
+  refresh_token: string;
+  expires_in: number;
+  refresh_expires_in: number;
 }
 
 export interface GraphToken {
@@ -42,7 +52,7 @@ export const performDeviceAuth = async (): Promise<{
   headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
   const requestBody = new URLSearchParams();
-  requestBody.append('scope', 'openid');
+  requestBody.append('scope', 'openid offline_access');
   requestBody.append('client_id', config.kcClientId);
 
   const response = await fetch(`${config.kcApiURL}/realms/${config.kcRealm}/protocol/openid-connect/auth/device`, {
@@ -88,7 +98,8 @@ export const startPollingForAccessToken = async ({
   requestBody.append('client_id', config.kcClientId);
   requestBody.append('grant_type', 'urn:ietf:params:oauth:grant-type:device_code');
   requestBody.append('device_code', deviceCode);
-  requestBody.append('scope', 'openid');
+  // Request an offline token https://wjw465150.gitbooks.io/keycloak-documentation/content/server_admin/topics/sessions/offline.html
+  requestBody.append('scope', 'openid offline_access');
 
   while (true) {
     const response = await fetch(`${config.kcApiURL}/realms/${config.kcRealm}/protocol/openid-connect/token`, {
@@ -109,7 +120,10 @@ export const startPollingForAccessToken = async ({
         errorMessage: 'Could not fetch the access token.',
       };
     }
-    const body = await response.json();
+    const body: KeycloakTokenResponse = await response.json();
+
+    console.log(body);
+
     const present = new Date();
     return {
       success: true,
@@ -122,3 +136,80 @@ export const startPollingForAccessToken = async ({
     };
   }
 };
+
+// checks if either of access token or api key are present
+// if not, it will try to refresh the access token
+export async function checkAuth(silent = false) {
+  const userConfig = readConfigFile();
+
+  if (!userConfig.organizationSlug) {
+    program.error(pc.red('Organization slug is not set. Please run `wgc auth login` to set the organization slug.'));
+  }
+
+  if (config.apiKey && userConfig.accessToken) {
+    console.error(
+      `${pc.yellow('Warning')} ${pc.dim(
+        'Both COSMO_API_KEY and login credentials found. Environment variable has precedence.\n',
+      )}`,
+    );
+  }
+
+  // API Key is present and assumed to be valid
+  if (config.apiKey) {
+    return;
+  }
+
+  // Access token is present and does not expire in the next 60 seconds
+  if (
+    userConfig?.accessToken &&
+    userConfig?.expiresAt &&
+    new Date(userConfig.expiresAt) > new Date(Date.now() - 60 * 1000)
+  ) {
+    // Update the api key to the current valid access token
+    config.apiKey = userConfig.accessToken;
+    return;
+  }
+
+  // Check if refresh token is expired
+  if (userConfig?.refreshToken && userConfig?.refreshExpiresAt && new Date(userConfig.refreshExpiresAt) < new Date()) {
+    program.error(pc.red('Refresh token has expired. Please login again with `wgc auth login`'));
+  }
+
+  // Refresh tokens with the refresh token
+  const resp = await fetch(`${config.kcApiURL}/realms/${config.kcRealm}/protocol/openid-connect/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      client_id: config.kcClientId,
+      refresh_token: userConfig.refreshToken!,
+    }),
+  });
+
+  if (resp.status !== 200) {
+    throw new Error(
+      'Failed to refresh access token. If the issue persists, please contact support. StatusCode: ' + resp.status,
+    );
+  }
+
+  try {
+    const data: KeycloakTokenResponse = await resp.json();
+    const present = new Date();
+
+    // Update the config file with the new access token and refresh token
+    updateConfigFile({
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token,
+      expiresAt: new Date(new Date().setSeconds(present.getSeconds() + data.expires_in)),
+      refreshExpiresAt: new Date(new Date().setSeconds(present.getSeconds() + data.refresh_expires_in)),
+      organizationSlug: userConfig.organizationSlug,
+    });
+
+    // Update the api key with the new access token
+    config.apiKey = data.access_token;
+  } catch {
+    program.error(pc.red('Failed to refresh access token. Please login again with `wgc auth login`'));
+  }
+}
