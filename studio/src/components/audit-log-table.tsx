7481158f7d9d3f8732111ed74c848484c632eb58
JivusAyrus <suvijsurya76@gmail.com>
2024-04-05 03:11:34 +0530

    fix: improve

--- studio/src/components/audit-log-table.tsx
+++ studio/src/components/audit-log-table.tsx
@@ -136,7 +136,7 @@ export const AuditLogTable = ({ logs }: { logs?: AuditLog[] }) => {
               return (
                 <TableRow
                   key={id}
-                  className="group py-1 hover:bg-secondary/30"
+                  className="group py-1"
                 >
                   <TableCell className="align-top font-medium">
                     <span className="flex items-center space-x-2">
