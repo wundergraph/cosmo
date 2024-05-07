c45bc5280dba271ca9f29a785a16ae4d08254778
starptech <deusdustin@gmail.com>
2024-05-07 16:29:48 +0200

    chore: improve

--- router-tests/kafka_events_test.go
+++ router-tests/kafka_events_test.go
@@ -1,12 +1,15 @@
 package integration_test
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"github.com/hasura/go-graphql-client"
 	"github.com/stretchr/testify/require"
 	"github.com/twmb/franz-go/pkg/kgo"
 	"github.com/wundergraph/cosmo/router-tests/testenv"
 	"github.com/wundergraph/cosmo/router/pkg/config"
+	"net/http"
 	"sync"
 	"testing"
 	"time"
@@ -20,21 +23,7 @@ func TestKafkaEvents(t *testing.T) {
 
 		testenv.Run(t, &testenv.Config{}, func(t *testing.T, xEnv *testenv.Environment) {
 
-			// Delete topic for idempotency
-
-			deleteCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
-
-			_, err := xEnv.KafkaAdminClient.DeleteTopics(deleteCtx, topicName)
-			require.NoError(t, err)
-
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
-
-			// Create topic
-
-			_, err = xEnv.KafkaAdminClient.CreateTopic(ctx, -1, -1, nil, topicName)
-			require.NoError(t, err)
+			ensureTopicExists(t, xEnv, topicName)
 
 			var subscriptionOne struct {
 				employeeUpdatedMyKafka struct {
@@ -79,24 +68,79 @@ func TestKafkaEvents(t *testing.T) {
 
 			xEnv.WaitForSubscriptionCount(1, time.Second*10)
 
-			wg2 := &sync.WaitGroup{}
-			wg2.Add(1)
+			produceKafkaMessage(t, xEnv, topicName, `{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`)
 
-			var pErr error
+			xEnv.WaitForMessagesSent(1, time.Second*10)
+			xEnv.WaitForSubscriptionCount(0, time.Second*10)
+			xEnv.WaitForConnectionCount(0, time.Second*10)
+		})
+	})
 
-			xEnv.KafkaClient.Produce(ctx, &kgo.Record{
-				Topic: topicName,
-				Value: []byte(`{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`),
-			}, func(record *kgo.Record, err error) {
-				defer wg2.Done()
-				if err != nil {
-					pErr = err
-				}
+	t.Run("every subscription gets the message", func(t *testing.T) {
+
+		topicName := "employeeUpdated"
+
+		testenv.Run(t, &testenv.Config{}, func(t *testing.T, xEnv *testenv.Environment) {
+
+			ensureTopicExists(t, xEnv, topicName)
+
+			var subscriptionOne struct {
+				employeeUpdatedMyKafka struct {
+					ID      float64 `graphql:"id"`
+					Details struct {
+						Forename string `graphql:"forename"`
+						Surname  string `graphql:"surname"`
+					} `graphql:"details"`
+				} `graphql:"employeeUpdatedMyKafka(employeeID: 3)"`
+			}
+
+			surl := xEnv.GraphQLSubscriptionURL()
+			client := graphql.NewSubscriptionClient(surl)
+			t.Cleanup(func() {
+				_ = client.Close()
 			})
 
-			wg2.Wait()
+			wg := &sync.WaitGroup{}
+			wg.Add(2)
 
-			require.NoError(t, pErr)
+			subscriptionOneID, err := client.Subscribe(&subscriptionOne, nil, func(dataValue []byte, errValue error) error {
+				defer wg.Done()
+				require.NoError(t, errValue)
+				require.JSONEq(t, `{"employeeUpdatedMyKafka":{"id":1,"details":{"forename":"Jens","surname":"Neuse"}}}`, string(dataValue))
+				return nil
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, subscriptionOneID)
+
+			subscriptionTwoID, err := client.Subscribe(&subscriptionOne, nil, func(dataValue []byte, errValue error) error {
+				defer wg.Done()
+				require.NoError(t, errValue)
+				require.JSONEq(t, `{"employeeUpdatedMyKafka":{"id":1,"details":{"forename":"Jens","surname":"Neuse"}}}`, string(dataValue))
+				return nil
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, subscriptionTwoID)
+
+			go func() {
+				clientErr := client.Run()
+				require.NoError(t, clientErr)
+			}()
+
+			go func() {
+				wg.Wait()
+				unsubscribeErr := client.Unsubscribe(subscriptionOneID)
+				require.NoError(t, unsubscribeErr)
+
+				unsubscribeErr = client.Unsubscribe(subscriptionTwoID)
+				require.NoError(t, unsubscribeErr)
+
+				clientCloseErr := client.Close()
+				require.NoError(t, clientCloseErr)
+			}()
+
+			xEnv.WaitForSubscriptionCount(1, time.Second*10)
+
+			produceKafkaMessage(t, xEnv, topicName, `{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`)
 
 			xEnv.WaitForMessagesSent(1, time.Second*10)
 			xEnv.WaitForSubscriptionCount(0, time.Second*10)
@@ -115,21 +159,7 @@ func TestKafkaEvents(t *testing.T) {
 			},
 		}, func(t *testing.T, xEnv *testenv.Environment) {
 
-			// Delete topic for idempotency
-
-			deleteCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
-
-			_, err := xEnv.KafkaAdminClient.DeleteTopics(deleteCtx, topicName)
-			require.NoError(t, err)
-
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
-
-			// Create topic
-
-			_, err = xEnv.KafkaAdminClient.CreateTopic(ctx, -1, -1, nil, topicName)
-			require.NoError(t, err)
+			ensureTopicExists(t, xEnv, topicName)
 
 			var subscriptionOne struct {
 				employeeUpdatedMyKafka struct {
@@ -174,24 +204,7 @@ func TestKafkaEvents(t *testing.T) {
 
 			xEnv.WaitForSubscriptionCount(1, time.Second*10)
 
-			wg2 := &sync.WaitGroup{}
-			wg2.Add(1)
-
-			var pErr error
-
-			xEnv.KafkaClient.Produce(ctx, &kgo.Record{
-				Topic: topicName,
-				Value: []byte(`{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`),
-			}, func(record *kgo.Record, err error) {
-				defer wg2.Done()
-				if err != nil {
-					pErr = err
-				}
-			})
-
-			wg2.Wait()
-
-			require.NoError(t, pErr)
+			produceKafkaMessage(t, xEnv, topicName, `{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`)
 
 			xEnv.WaitForMessagesSent(1, time.Second*10)
 			xEnv.WaitForSubscriptionCount(0, time.Second*10)
@@ -199,36 +212,138 @@ func TestKafkaEvents(t *testing.T) {
 		})
 	})
 
-	t.Run("publish async", func(t *testing.T) {
-		t.Parallel()
+	t.Run("subscribe sync sse", func(t *testing.T) {
+
+		topicName := "employeeUpdated"
 
 		testenv.Run(t, &testenv.Config{}, func(t *testing.T, xEnv *testenv.Environment) {
 
-			// Create topic
+			ensureTopicExists(t, xEnv, topicName)
 
-			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-			defer cancel()
+			subscribePayload := []byte(`{"query":"subscription { employeeUpdatedMyKafka(employeeID: 1) { id details { forename surname } }}"}`)
 
-			topicName := "employeeUpdated"
-
-			var wg sync.WaitGroup
+			wg := &sync.WaitGroup{}
 			wg.Add(1)
 
-			var pErr error
-
-			xEnv.KafkaClient.Produce(ctx, &kgo.Record{
-				Topic: topicName,
-				Value: []byte(`{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`),
-			}, func(record *kgo.Record, err error) {
-				defer wg.Done()
-				if err != nil {
-					pErr = err
+			go func() {
+				client := http.Client{
+					Timeout: time.Second * 10,
 				}
-			})
+				req, err := http.NewRequest(http.MethodPost, xEnv.GraphQLServeSentEventsURL(), bytes.NewReader(subscribePayload))
+				require.NoError(t, err)
+
+				req.Header.Set("Content-Type", "application/json")
+				req.Header.Set("Accept", "text/event-stream")
+				req.Header.Set("Connection", "keep-alive")
+				req.Header.Set("Cache-Control", "no-cache")
+
+				resp, err := client.Do(req)
+				require.NoError(t, err)
+				require.Equal(t, http.StatusOK, resp.StatusCode)
+				defer resp.Body.Close()
+				reader := bufio.NewReader(resp.Body)
+
+				eventNext, _, err := reader.ReadLine()
+				require.NoError(t, err)
+				require.Equal(t, "event: next", string(eventNext))
+				data, _, err := reader.ReadLine()
+				require.NoError(t, err)
+				require.Equal(t, "data: {\"data\":{\"employeeUpdatedMyKafka\":{\"id\":1,\"details\":{\"forename\":\"Jens\",\"surname\":\"Neuse\"}}}}", string(data))
+				line, _, err := reader.ReadLine()
+				require.NoError(t, err)
+				require.Equal(t, "", string(line))
+
+				wg.Done()
+
+			}()
+
+			xEnv.WaitForSubscriptionCount(1, time.Second*5)
+
+			produceKafkaMessage(t, xEnv, topicName, `{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`)
 
 			wg.Wait()
 
-			require.NoError(t, pErr)
+			xEnv.WaitForSubscriptionCount(0, time.Second*10)
+			xEnv.WaitForConnectionCount(0, time.Second*10)
 		})
 	})
+
+	t.Run("subscribe sync sse with block", func(t *testing.T) {
+		t.Parallel()
+
+		subscribePayload := []byte(`{"query":"subscription { employeeUpdatedMyKafka(employeeID: 1) { id details { forename surname } }}"}`)
+
+		testenv.Run(t, &testenv.Config{
+			ModifySecurityConfiguration: func(securityConfiguration *config.SecurityConfiguration) {
+				securityConfiguration.BlockSubscriptions = true
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			client := http.Client{
+				Timeout: time.Second * 10,
+			}
+			req, err := http.NewRequest(http.MethodPost, xEnv.GraphQLServeSentEventsURL(), bytes.NewReader(subscribePayload))
+			require.NoError(t, err)
+
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Accept", "text/event-stream")
+			req.Header.Set("Connection", "keep-alive")
+			req.Header.Set("Cache-Control", "no-cache")
+
+			resp, err := client.Do(req)
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, resp.StatusCode)
+			defer resp.Body.Close()
+			reader := bufio.NewReader(resp.Body)
+
+			eventNext, _, err := reader.ReadLine()
+			require.NoError(t, err)
+			require.Equal(t, "event: next", string(eventNext))
+			data, _, err := reader.ReadLine()
+			require.NoError(t, err)
+			require.Equal(t, "data: {\"errors\":[{\"message\":\"operation type 'subscription' is blocked\"}],\"data\":null}", string(data))
+
+			xEnv.WaitForSubscriptionCount(0, time.Second*10)
+			xEnv.WaitForConnectionCount(0, time.Second*10)
+		})
+	})
+}
+
+func ensureTopicExists(t *testing.T, xEnv *testenv.Environment, topicName string) {
+	// Delete topic for idempotency
+
+	deleteCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_, err := xEnv.KafkaAdminClient.DeleteTopics(deleteCtx, topicName)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_, err = xEnv.KafkaAdminClient.CreateTopic(ctx, 1, 1, nil, topicName)
+	require.NoError(t, err)
+}
+
+func produceKafkaMessage(t *testing.T, xEnv *testenv.Environment, topicName string, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	var pErr error
+
+	xEnv.KafkaClient.Produce(ctx, &kgo.Record{
+		Topic: topicName,
+		Value: []byte(message),
+	}, func(record *kgo.Record, err error) {
+		defer wg.Done()
+		if err != nil {
+			pErr = err
+		}
+	})
+
+	wg.Wait()
+
+	require.NoError(t, pErr)
 }
