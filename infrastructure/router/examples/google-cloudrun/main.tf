d0e9da853c790c5d9f00e69c08dfbfe89c1e2449
John Kiragu <mutuakiragu@gmail.com>
2024-05-09 20:57:04 +0300

    update docs to match usage of the cloud run module

--- infrastructure/router/examples/google-cloudrun/main.tf
+++ infrastructure/router/examples/google-cloudrun/main.tf
@@ -1,8 +1,10 @@
+# Replace with necessary variables
 module "cosmo" {
   source           = "../../modules/google-cloudrun"
-  name             = "cosmo-on-cloudrun"
+  name             = ""
   config_file_path = "${path.module}/config.yaml"
-  region           = "europe-west1"
-  secret_name      = "subaruboys"
+  region           = ""
+  secret_name      = ""
   project          = var.google-cloud-project
 }
+
