2ffc8dc48ce14b90bc1e920207672275b6ec0bc5
thisisnithin <nithinkumar5353@gmail.com>
2024-06-04 17:42:03 +0530

    fix: sdl viewer slow performance due to line links

--- studio/src/components/schema/sdl-viewer-monaco.tsx
+++ studio/src/components/schema/sdl-viewer-monaco.tsx
@@ -229,44 +229,43 @@ export const SDLViewerMonaco = ({
         if (model && enableLinking) {
           const lineCount = model.getLineCount();
           for (let lineNumber = 1; lineNumber <= lineCount; lineNumber++) {
-            editor.changeViewZones((changeAccessor) => {
-              const domNode = document.createElement("div");
-
-              const marginDomNode = document.createElement("div");
-              marginDomNode.className = "z-30";
-
-              const wrapper = document.createElement("div");
-              wrapper.className = "pl-2";
-              marginDomNode.appendChild(wrapper);
-
-              const anchor = document.createElement("a");
-              anchor.href = `#L${lineNumber}`;
-              anchor.id = "wrap-" + lineNumber;
-              anchor.className = "h-10 cursor-pointer w-full";
-              anchor.innerHTML = `
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-4">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
-                </svg>
-              `;
-              anchor.style.visibility = "hidden";
-
-              wrapper.appendChild(anchor);
-
-              changeAccessor.addZone({
-                afterLineNumber: lineNumber - 1,
-                heightInLines: 0,
-                domNode,
-                marginDomNode,
-              });
-            });
-
             editor.onMouseMove((e) => {
               if (
                 e.target.position &&
                 e.target.position.lineNumber === lineNumber
               ) {
                 const div = document.getElementById(`wrap-${lineNumber}`);
-                if (div) {
+                if (!div) {
+                  editor.changeViewZones((changeAccessor) => {
+                    const domNode = document.createElement("div");
+
+                    const marginDomNode = document.createElement("div");
+                    marginDomNode.className = "z-30";
+
+                    const wrapper = document.createElement("div");
+                    wrapper.className = "pl-2";
+                    marginDomNode.appendChild(wrapper);
+
+                    const anchor = document.createElement("a");
+                    anchor.href = `#L${lineNumber}`;
+                    anchor.id = "wrap-" + lineNumber;
+                    anchor.className = "h-10 cursor-pointer w-full";
+                    anchor.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                </svg>
+              `;
+
+                    wrapper.appendChild(anchor);
+
+                    changeAccessor.addZone({
+                      afterLineNumber: lineNumber - 1,
+                      heightInLines: 0,
+                      domNode,
+                      marginDomNode,
+                    });
+                  });
+                } else {
                   div.style.visibility = "visible";
                 }
               } else {
