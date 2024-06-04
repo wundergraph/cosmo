fb8c9ee4c8af0f3bfdca9b100848bcd6123d71ef
thisisnithin <nithinkumar5353@gmail.com>
2024-06-04 18:29:14 +0530

    cleanup

--- studio/src/components/schema/sdl-viewer-monaco.tsx
+++ studio/src/components/schema/sdl-viewer-monaco.tsx
@@ -251,7 +251,6 @@ export const SDLViewerMonaco = ({
                 anchor.href = `#L${lineNumber}`;
                 anchor.id = "anchor-" + lineNumber;
                 anchor.className = "h-10 cursor-pointer w-full";
-                anchor.setAttribute("sdl-line", "visible");
                 anchor.innerHTML = `
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-4">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
