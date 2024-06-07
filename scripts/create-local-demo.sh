4a3f41a41ada51ae368b2720f04f8858ce773966
starptech <deusdustin@gmail.com>
2024-06-07 11:31:26 +0200

    chore: create demo with local cli

--- scripts/create-local-demo.sh
+++ scripts/create-local-demo.sh
@@ -7,16 +7,17 @@ set -e
 
 pnpm wgc federated-graph create mygraph --namespace default --label-matcher team=A,team=B --routing-url http://localhost:3002/graphql
 
-pnpm wgc contract create mygraph-external --source mygraph -r http://localhost:3003/graphql --exclude internal
-
 pnpm wgc subgraph create employees --namespace default --label team=A --routing-url http://localhost:4001/graphql
 pnpm wgc subgraph create family --namespace default --label team=A --routing-url http://localhost:4002/graphql
 pnpm wgc subgraph create hobbies --namespace default --label team=B --routing-url http://localhost:4003/graphql
 pnpm wgc subgraph create products --namespace default --label team=B --routing-url http://localhost:4004/graphql
 pnpm wgc subgraph create employeeupdated --namespace default --label team=B --event-driven-graph
 
+pnpm wgc contract create mygraph-external --source mygraph -r http://localhost:3003/graphql --exclude internal
+
 ./scripts/update-demo.sh
 
 pnpm wgc router token create mytoken --graph-name mygraph --namespace default
 
-pnpm wgc router token create myContractToken --graph-name mygraph-external --namespace default
+# Optionally
+# pnpm wgc router token create myContractToken --graph-name mygraph-external --namespace default
