2e33f560e9e1fb7e9f7822eb2ba14bb8cfb3eba7
Michael Edelman <mike.edelman@gmail.com>
2024-06-08 21:30:55 -0400

    enhancement: add 'flags' alias to 'feature-flags' cli command

--- cli/src/commands/feature-flag/index.ts
+++ cli/src/commands/feature-flag/index.ts
@@ -7,7 +7,7 @@ import DisableFeatureFlagCommand from './commands/disable.js';
 import UpdateFeatureFlagCommand from './commands/update.js';
 
 export default (opts: BaseCommandOptions) => {
-  const command = new Command('feature-flag');
+  const command = new Command('feature-flag').alias('flags');
   command.description('Provides commands for creating and managing a feature flags.');
 
   command.addCommand(CreateFeatureFlagCommand(opts));
