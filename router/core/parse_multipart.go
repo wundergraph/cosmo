471744e3559550c4fa30251023e6a1254e4b5d54
thisisnithin <nithinkumar5353@gmail.com>
2024-06-26 16:13:00 +0530

    refactor

--- router/core/parse_multipart.go
+++ router/core/parse_multipart.go
@@ -17,6 +17,8 @@ type MultipartParser struct {
 	operationProcessor *OperationProcessor
 	maxUploadFiles     int
 	maxUploadFileSize  int
+	body               []byte
+	files              []httpclient.File
 	fileHandlers       []*os.File
 	form               *multipart.Form
 }
@@ -39,32 +41,32 @@ func (p *MultipartParser) RemoveAll() {
 	}
 }
 
-func (p *MultipartParser) processInMemoryFile(filePart []*multipart.FileHeader, file multipart.File, body []byte, files []httpclient.File) ([]byte, []httpclient.File, error) {
+func (p *MultipartParser) processInMemoryFile(filePart []*multipart.FileHeader, file multipart.File) error {
 	tempFile, err := os.CreateTemp("", "cosmo-upload-")
 	if err != nil {
-		return body, files, err
+		return err
 	}
 
 	defer tempFile.Close()
 	p.fileHandlers = append(p.fileHandlers, tempFile)
 	_, err = io.Copy(tempFile, file)
 	if err != nil {
-		return body, files, err
+		return err
 	}
-	files = append(files, httpclient.NewFile(tempFile.Name(), filePart[0].Filename))
+	p.files = append(p.files, httpclient.NewFile(tempFile.Name(), filePart[0].Filename))
 
-	return body, files, err
+	return err
 }
 
-func (p *MultipartParser) processFilePart(filePart []*multipart.FileHeader, body []byte, files []httpclient.File) ([]byte, []httpclient.File, error) {
+func (p *MultipartParser) processFilePart(filePart []*multipart.FileHeader) error {
 	file, err := filePart[0].Open()
 	if err != nil {
-		return body, files, err
+		return err
 	}
 	defer file.Close()
 
 	if filePart[0].Size > int64(p.maxUploadFileSize) {
-		return body, files, &inputError{
+		return &inputError{
 			message:    "file too large to upload",
 			statusCode: http.StatusOK,
 		}
@@ -73,56 +75,54 @@ func (p *MultipartParser) processFilePart(filePart []*multipart.FileHeader, body
 	// Check if the file was written to the disk
 	if diskFile, ok := file.(*os.File); ok {
 		p.fileHandlers = append(p.fileHandlers, diskFile)
-		files = append(files, httpclient.NewFile(diskFile.Name(), filePart[0].Filename))
+		p.files = append(p.files, httpclient.NewFile(diskFile.Name(), filePart[0].Filename))
 	} else {
 		// The file is in memory. We write it manually to the disk.
-		body, files, err = p.processInMemoryFile(filePart, file, body, files)
+		err = p.processInMemoryFile(filePart, file)
 	}
 
-	return body, files, err
+	return err
 }
 
 func (p *MultipartParser) Parse(r *http.Request, buf *bytes.Buffer) ([]byte, []httpclient.File, error) {
-	var body []byte
-	var files []httpclient.File
 	contentType := r.Header.Get("Content-Type")
 	d, params, err := mime.ParseMediaType(contentType)
 	if err != nil || d != "multipart/form-data" {
-		return body, files, err
+		return p.body, p.files, err
 	}
 
 	boundary, ok := params["boundary"]
 	if !ok {
-		return body, files, errors.New("could not find request boundary")
+		return p.body, p.files, errors.New("could not find request boundary")
 	}
 
 	reader := multipart.NewReader(r.Body, boundary)
 	p.form, err = reader.ReadForm(0)
 	if err != nil {
-		return body, files, &inputError{
+		return p.body, p.files, &inputError{
 			message:    err.Error(),
 			statusCode: http.StatusOK,
 		}
 	}
 
 	if len(p.form.File) > p.maxUploadFiles {
-		return body, files, &inputError{
+		return p.body, p.files, &inputError{
 			message:    fmt.Sprintf("too many files: %d, max allowed: %d", len(p.form.File), p.maxUploadFiles),
 			statusCode: http.StatusOK,
 		}
 	}
 
-	body, err = p.operationProcessor.ReadBody(buf, strings.NewReader(strings.Join(p.form.Value["operations"], "")))
+	p.body, err = p.operationProcessor.ReadBody(buf, strings.NewReader(strings.Join(p.form.Value["operations"], "")))
 	if err != nil {
-		return body, files, err
+		return p.body, p.files, err
 	}
 
 	for _, filePart := range p.form.File {
-		body, files, err = p.processFilePart(filePart, body, files)
+		err = p.processFilePart(filePart)
 		if err != nil {
-			return body, files, err
+			return p.body, p.files, err
 		}
 	}
 
-	return body, files, err
+	return p.body, p.files, err
 }
