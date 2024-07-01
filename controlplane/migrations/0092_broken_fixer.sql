17f8cbc73d1417fb08bbb26da5629315fd9328d5
JivusAyrus <suvijsurya76@gmail.com>
2024-07-01 13:49:30 +0530

    chore: remove migration

--- controlplane/migrations/0092_broken_fixer.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "federated_graphs_to_feature_flag_schema_versions" ADD COLUMN "feature_flag_id" uuid;--> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "federated_graphs_to_feature_flag_schema_versions" ADD CONSTRAINT "federated_graphs_to_feature_flag_schema_versions_feature_flag_id_feature_flags_id_fk" FOREIGN KEY ("feature_flag_id") REFERENCES "feature_flags"("id") ON DELETE set null ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
