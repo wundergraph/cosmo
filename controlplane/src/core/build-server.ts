0e74d6c9c7699a335bb56d74bfc0cf3b2fdbc70e
anzeissner <zeissnerandreas@googlemail.com>
2024-07-23 10:49:05 +0200

    feat: instrument controlplane with metrics (#943)
    Co-authored-by: Andreas Zeissner <andi@wundergraph.com>
    Co-authored-by: starptech <deusdustin@gmail.com>

--- controlplane/src/core/build-server.ts
+++ controlplane/src/core/build-server.ts
@@ -10,6 +10,7 @@ import { App } from 'octokit';
 import { Worker } from 'bullmq';
 import routes from './routes.js';
 import fastifyHealth from './plugins/health.js';
+import fastifyMetrics, { MetricsPluginOptions } from './plugins/metrics.js';
 import fastifyDatabase from './plugins/database.js';
 import fastifyClickHouse from './plugins/clickhouse.js';
 import fastifyRedis from './plugins/redis.js';
@@ -50,6 +51,7 @@ export interface BuildConfig {
       key?: string; // e.g. string or '/path/to/my/client-key.pem'
     };
   };
+  prometheus?: MetricsOptions;
   openaiAPIKey?: string;
   allowedOrigins?: string[];
   debugSQL?: boolean;
@@ -107,6 +109,13 @@ export interface BuildConfig {
   };
 }
 
+export interface MetricsOptions {
+  enabled?: boolean;
+  path?: string;
+  host?: string;
+  port?: number;
+}
+
 const developmentLoggerOpts: LoggerOptions = {
   transport: {
     target: 'pino-pretty',
@@ -144,6 +153,16 @@ export default async function build(opts: BuildConfig) {
    */
 
   await fastify.register(fastifyHealth);
+
+  if (opts.prometheus?.enabled) {
+    await fastify.register(fastifyMetrics, {
+      path: opts.prometheus.path,
+    });
+    await fastify.metricsServer.listen({
+      host: opts.prometheus.host,
+      port: opts.prometheus.port,
+    });
+  }
 
   await fastify.register(fastifyDatabase, {
     databaseConnectionUrl: opts.database.url,
