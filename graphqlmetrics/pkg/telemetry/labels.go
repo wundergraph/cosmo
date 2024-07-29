2267faf3de96f6ca2e5b14e7b967192215412886
Andreas Zeissner <andi@wundergraph.com>
2024-07-29 16:11:32 +0200

    chore: remove unused labels

--- graphqlmetrics/pkg/telemetry/labels.go
+++ graphqlmetrics/pkg/telemetry/labels.go
@@ -1,8 +1,5 @@
 package telemetry
 
 const (
-	// TODO: check default labels
-	// wg.organization_id
-	OrganizationID = "wg_customer_id"
-	Endpoint       = "wg_endpoint"
+	Endpoint = "wg_graphqlmetrics_endpoint"
 )
