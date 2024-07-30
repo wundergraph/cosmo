0bcaf1416e3e8e10202c16663087c6eb5b67d30c
Nithin Kumar B <nithinkumar5353@gmail.com>
2024-07-30 17:48:26 +0530

    feat: package playground as library (#975)

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,21 +1,48 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import dts from 'vite-plugin-dts';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    minify: 'terser',
-    // Required to make require() work e.g for graphql-ws
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const singleFileOutput = process.env.VITE_SINGLE_FILE_OUTPUT === 'true';
+
+  // https://vitejs.dev/config/
+  return defineConfig({
+    build: {
+      minify: 'terser',
+      // Required to make require() work e.g for graphql-ws
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+      lib: !singleFileOutput
+        ? {
+            entry: path.resolve(__dirname, './src/index.ts'),
+            name: '@wundergraph/playground',
+            fileName: (format) => `index.${format}.js`,
+          }
+        : false,
+      sourcemap: !singleFileOutput,
+      emptyOutDir: true,
+      rollupOptions: !singleFileOutput
+        ? {
+            external: ['react', 'react-dom'],
+            output: {
+              globals: {
+                react: 'React',
+                'react-dom': 'ReactDOM',
+              },
+            },
+          }
+        : undefined,
     },
-  },
-  plugins: [react(), viteSingleFile()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react(), !singleFileOutput ? dts() : viteSingleFile()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-});
+  });
+};
