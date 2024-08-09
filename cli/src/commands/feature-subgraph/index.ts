4ee7fe63ebc103376905118ab9b4b8142c2b7df2
JivusAyrus <suvijsurya76@gmail.com>
2024-08-09 14:36:26 +0530

    fix: improve description

--- cli/src/commands/feature-subgraph/index.ts
+++ cli/src/commands/feature-subgraph/index.ts
@@ -6,7 +6,9 @@ import PublishFeatureSubgraphCommand from './commands/publish.js';
 
 export default (opts: BaseCommandOptions) => {
   const command = new Command('feature-subgraph').alias('fs');
-  command.description('Provides commands for creating and managing feature subgraphs.');
+  command.description(
+    'A feature subgraph serves as an "override" of an existing subgraph. When composing for a feature flag that includes the feature subgraph, the feature subgraph replaces the base subgraph. This  provides commands for creating and managing feature subgraphs.',
+  );
 
   command.addCommand(CreateFeatureSubgraphCommand(opts));
   command.addCommand(PublishFeatureSubgraphCommand(opts));
