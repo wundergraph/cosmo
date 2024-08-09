2170e63de2512e9819dc66e84d548bc866d32a69
Suvij Surya <suvijsurya76@gmail.com>
2024-08-09 14:47:41 +0530

    feat: add command to create and publish feature subgraph in one command (#960)

--- cli/src/commands/feature-subgraph/index.ts
+++ cli/src/commands/feature-subgraph/index.ts
@@ -2,12 +2,16 @@ import { Command } from 'commander';
 import { BaseCommandOptions } from '../../core/types/types.js';
 import { checkAuth } from '../auth/utils.js';
 import CreateFeatureSubgraphCommand from './commands/create.js';
+import PublishFeatureSubgraphCommand from './commands/publish.js';
 
 export default (opts: BaseCommandOptions) => {
   const command = new Command('feature-subgraph').alias('fs');
-  command.description('Provides commands for creating and managing feature subgraphs.');
+  command.description(
+    'A feature subgraph serves as an "override" of an existing subgraph. When composing for a feature flag that includes the feature subgraph, the feature subgraph replaces the base subgraph. This  provides commands for creating and managing feature subgraphs.',
+  );
 
   command.addCommand(CreateFeatureSubgraphCommand(opts));
+  command.addCommand(PublishFeatureSubgraphCommand(opts));
 
   command.hook('preAction', async () => {
     await checkAuth();
