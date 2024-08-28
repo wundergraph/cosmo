89ea29fb23f5cc4527d7366ff4fb8d9c11092ce3
anzeissner <andi@wundergraph.com>
2024-08-28 13:06:21 +0200

    feat: enable proxy handling for native fetch and connectrpc calls (#1124)

--- cli/src/core/client/client.ts
+++ cli/src/core/client/client.ts
@@ -2,10 +2,12 @@ import { compressionBrotli, compressionGzip, createConnectTransport } from '@con
 import { createPromiseClient, PromiseClient } from '@connectrpc/connect';
 import { PlatformService } from '@wundergraph/cosmo-connect/dist/platform/v1/platform_connect';
 import { NodeService } from '@wundergraph/cosmo-connect/dist/node/v1/node_connect';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
 export interface ClientOptions {
   baseUrl: string;
   apiKey?: string;
+  proxyUrl?: string;
 }
 
 export interface Client {
@@ -20,7 +22,9 @@ export const CreateClient = (opts: ClientOptions): Client => {
 
     // You have to tell the Node.js http API which HTTP version to use.
     httpVersion: '1.1',
-
+    nodeOptions: {
+      ...(opts.proxyUrl ? { agent: new HttpsProxyAgent(opts.proxyUrl) } : {}),
+    },
     // Avoid compression for small requests
     compressMinBytes: 1024,
 
