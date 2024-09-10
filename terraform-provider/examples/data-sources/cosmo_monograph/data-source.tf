18964a76fa2cfda204bc8523a9b59185d921ffca
Andreas Zeissner <andi@wundergraph.com>
2024-09-10 14:34:14 +0200

    chore: add monograph datasource example

---
+++ terraform-provider/examples/data-sources/cosmo_monograph/data-source.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    cosmo = {
+      source  = "terraform.local/wundergraph/cosmo"
+      version = "0.0.1"
+    }
+  }
+}
+
+data "cosmo_monograph" "example" {
+  name = var.name
+}
---
+++ terraform-provider/examples/data-sources/cosmo_monograph/variables.tf
@@ -0,0 +1,4 @@
+variable "name" {
+  description = "The name of the monograph to retrieve"
+  type        = string
+}
