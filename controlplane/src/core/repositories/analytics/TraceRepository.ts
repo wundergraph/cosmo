c46048ec9ee37a23cac9bab4a661f9094445a33b
thisisnithin <nithinkumar5353@gmail.com>
2024-09-21 04:33:19 +0530

    refactor

--- controlplane/src/core/repositories/analytics/TraceRepository.ts
+++ controlplane/src/core/repositories/analytics/TraceRepository.ts
@@ -65,9 +65,10 @@ export class TraceRepository {
       
       SELECT ${columns}
       FROM spans s, ${this.client.database}.otel_traces as t
-      WHERE t.ParentSpanId = s.spanId AND t.TraceId = s.traceId
-        AND (t.Timestamp >= start)
-        AND (t.Timestamp <= end) 
+      WHERE t.ParentSpanId = s.spanId 
+        AND t.TraceId = trace_id
+        AND t.Timestamp >= start
+        AND t.Timestamp <= end
         AND t.SpanAttributes['wg.organization.id'] = '${organizationID}'
         AND t.SpanAttributes['wg.federated_graph.id'] = '${federatedGraphId}'
     ),
