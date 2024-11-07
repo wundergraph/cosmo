61c0d9b9ac383ad744d47a461fc7ac9b07c9c981
df-wg <dave@wundergraph.com>
2024-11-07 21:57:56 +0200

    feat: add apq back in (#1346)

--- router/core/graph_server.go
+++ router/core/graph_server.go
@@ -267,13 +267,14 @@ func (s *graphServer) buildMultiGraphHandler(ctx context.Context, baseMux *chi.M
 }
 
 type graphMux struct {
-	mux                  *chi.Mux
-	planCache            ExecutionPlanCache[uint64, *planWithMetaData]
-	normalizationCache   *ristretto.Cache[uint64, NormalizationCacheEntry]
-	validationCache      *ristretto.Cache[uint64, bool]
-	queryDepthCache      *ristretto.Cache[uint64, int]
-	operationHashCache   *ristretto.Cache[uint64, string]
-	accessLogsFileLogger *logging.BufferedLogger
+	mux                     *chi.Mux
+	planCache               ExecutionPlanCache[uint64, *planWithMetaData]
+	persistedOperationCache *ristretto.Cache[uint64, NormalizationCacheEntry]
+	normalizationCache      *ristretto.Cache[uint64, NormalizationCacheEntry]
+	validationCache         *ristretto.Cache[uint64, bool]
+	queryDepthCache         *ristretto.Cache[uint64, int]
+	operationHashCache      *ristretto.Cache[uint64, string]
+	accessLogsFileLogger    *logging.BufferedLogger
 }
 
 func (s *graphMux) Shutdown(_ context.Context) error {
@@ -281,6 +282,10 @@ func (s *graphMux) Shutdown(_ context.Context) error {
 	var err error
 
 	s.planCache.Close()
+
+	if s.persistedOperationCache != nil {
+		s.persistedOperationCache.Close()
+	}
 
 	if s.normalizationCache != nil {
 		s.normalizationCache.Close()
@@ -394,6 +399,17 @@ func (s *graphServer) buildGraphMux(ctx context.Context,
 		}
 	} else {
 		gm.planCache = NewNoopExecutionPlanCache()
+	}
+
+	if s.engineExecutionConfiguration.EnablePersistedOperationsCache || s.automaticPersistedQueriesConfig.Enabled {
+		cacheSize := int64(1024 * 10)
+		persistedOperationCacheConfig := &ristretto.Config[uint64, NormalizationCacheEntry]{
+			MaxCost:     cacheSize,
+			NumCounters: cacheSize * 10,
+			BufferItems: 64,
+		}
+
+		gm.persistedOperationCache, _ = ristretto.NewCache[uint64, NormalizationCacheEntry](persistedOperationCacheConfig)
 	}
 
 	if s.engineExecutionConfiguration.EnableNormalizationCache && s.engineExecutionConfiguration.NormalizationCacheSize > 0 {
@@ -686,17 +702,19 @@ func (s *graphServer) buildGraphMux(ctx context.Context,
 	}
 
 	operationProcessor := NewOperationProcessor(OperationProcessorOptions{
-		Executor:                       executor,
-		MaxOperationSizeInBytes:        int64(s.routerTrafficConfig.MaxRequestBodyBytes),
-		PersistedOperationClient:       s.persistedOperationClient,
-		EnablePersistedOperationsCache: s.engineExecutionConfiguration.EnablePersistedOperationsCache,
-		NormalizationCache:             gm.normalizationCache,
-		ValidationCache:                gm.validationCache,
-		QueryDepthCache:                gm.queryDepthCache,
-		OperationHashCache:             gm.operationHashCache,
-		ParseKitPoolSize:               s.engineExecutionConfiguration.ParseKitPoolSize,
-		IntrospectionEnabled:           s.Config.introspection,
-		ApolloCompatibilityFlags:       s.apolloCompatibilityFlags,
+		Executor:                            executor,
+		MaxOperationSizeInBytes:             int64(s.routerTrafficConfig.MaxRequestBodyBytes),
+		PersistedOperationClient:            s.persistedOperationClient,
+		AutomaticPersistedOperationCacheTtl: s.automaticPersistedQueriesConfig.Cache.TTL,
+		EnablePersistedOperationsCache:      s.engineExecutionConfiguration.EnablePersistedOperationsCache,
+		PersistedOpsNormalizationCache:      gm.persistedOperationCache,
+		NormalizationCache:                  gm.normalizationCache,
+		ValidationCache:                     gm.validationCache,
+		QueryDepthCache:                     gm.queryDepthCache,
+		OperationHashCache:                  gm.operationHashCache,
+		ParseKitPoolSize:                    s.engineExecutionConfiguration.ParseKitPoolSize,
+		IntrospectionEnabled:                s.Config.introspection,
+		ApolloCompatibilityFlags:            s.apolloCompatibilityFlags,
 	})
 	operationPlanner := NewOperationPlanner(executor, gm.planCache)
 
