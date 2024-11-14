b4c9faa7731ecf04b7a15ffd14d1bfe4df15325a
Dave Freilich <dave@wundergraph.com>
2024-11-14 16:30:36 +0200

    fix: remove nil pointer issue

--- router/core/header_rule_engine.go
+++ router/core/header_rule_engine.go
@@ -397,7 +397,7 @@ func (h *HeaderPropagation) applyRequestRule(ctx RequestContext, request *http.R
 			request.Header.Set(rule.Named, ctx.Request().Header.Get(rule.Named))
 		} else if rule.Default != "" {
 			request.Header.Set(rule.Named, rule.Default)
-		} else if rule.ValueFrom.ContextField != "" {
+		} else if rule.ValueFrom != nil && rule.ValueFrom.ContextField != "" {
 			val := GetCustomDynamicAttributeValue(rule.ValueFrom, ctx)
 			value = fmt.Sprintf("%v", val)
 			if value != "" {
