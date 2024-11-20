dcd16800ff101ffa0691bc84b7368e67e588d039
Dave Freilich <dave@wundergraph.com>
2024-11-20 19:07:24 +0200

    move module

--- router-tests/modules/set_scopes_test.go
+++ router-tests/modules/set_scopes_test.go
@@ -3,8 +3,8 @@ package module_test
 import (
 	"github.com/stretchr/testify/require"
 	"github.com/wundergraph/cosmo/router-tests/jwks"
+	setScopesModule "github.com/wundergraph/cosmo/router-tests/modules/custom-set-scopes/module"
 	"github.com/wundergraph/cosmo/router-tests/testenv"
-	setScopesModule "github.com/wundergraph/cosmo/router/cmd/custom-set-scopes/module"
 	"github.com/wundergraph/cosmo/router/cmd/custom/module"
 	"github.com/wundergraph/cosmo/router/core"
 	"github.com/wundergraph/cosmo/router/pkg/authentication"
