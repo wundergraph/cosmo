9f37428d9c6eab80cb9a6ecb25901395ff1389f6
df-wg <dave@wundergraph.com>
2024-12-09 14:24:13 +0200

    feat: initial access log improvements (#1424)

--- router/core/engine_loader_hooks.go
+++ router/core/engine_loader_hooks.go
@@ -132,6 +132,9 @@ func (f *engineLoaderHooks) OnFinished(ctx context.Context, ds resolve.DataSourc
 		if responseInfo.Err != nil {
 			fields = append(fields, zap.Any("error", responseInfo.Err))
 		}
+		if responseInfo.Request != nil && responseInfo.Request.URL != nil {
+			fields = append(fields, zap.String("url", responseInfo.Request.URL.String()))
+		}
 		f.accessLogger.WriteRequestLog(responseInfo, fields)
 	}
 
