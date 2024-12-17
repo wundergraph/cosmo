9a39fa752f43672dd89f36c96aba400d02ccc669
Alessandro Pagnin <alepane@gmail.com>
2024-12-17 15:55:33 +0100

    chore: fix test data race

--- router-tests/events/nats_events_test.go
+++ router-tests/events/nats_events_test.go
@@ -127,26 +127,32 @@ func TestNatsEvents(t *testing.T) {
 			wg := &sync.WaitGroup{}
 			wg.Add(4)
 
+			var countMu sync.Mutex
 			count := 0
 
 			subscriptionOneID, err := client.Subscribe(&subscriptionOne, nil, func(dataValue []byte, errValue error) error {
-
 				defer wg.Done()
 
-				if count == 0 {
+				countMu.Lock()
+				oldCount := count
+				countMu.Unlock()
+
+				if oldCount == 0 {
 					var gqlErr graphql.Errors
 					require.ErrorAs(t, errValue, &gqlErr)
 					require.Equal(t, "Invalid message received", gqlErr[0].Message)
-				} else if count == 1 || count == 3 {
+				} else if oldCount == 1 || oldCount == 3 {
 					require.NoError(t, errValue)
 					require.JSONEq(t, `{"employeeUpdated":{"id":3,"details":{"forename":"Stefan","surname":"Avram"}}}`, string(dataValue))
-				} else if count == 2 {
+				} else if oldCount == 2 {
 					var gqlErr graphql.Errors
 					require.ErrorAs(t, errValue, &gqlErr)
 					require.Equal(t, "Cannot return null for non-nullable field 'Subscription.employeeUpdated.id'.", gqlErr[0].Message)
 				}
 
+				countMu.Lock()
 				count++
+				countMu.Unlock()
 
 				return nil
 			})
