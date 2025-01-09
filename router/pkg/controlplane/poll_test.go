8d5c7bc3fabdd744b9c94ee0cd690e237caca00c
endigma <endigma@mailcat.ca>
2025-01-09 19:57:48 +0000

    test: add comments explaining some poller tests

--- router/pkg/controlplane/poll_test.go
+++ router/pkg/controlplane/poll_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func Test_Poller(t *testing.T) {
+	// This test passing seems obvious, but it asserts that behavior remains the same after refactoring
 	t.Run("creating with invalid parameters should panic", func(t *testing.T) {
 		assert.Panics(t, func() {
 			NewPoll(-1*time.Second, 0*time.Second)
@@ -22,6 +23,8 @@ func Test_Poller(t *testing.T) {
 		})
 	})
 
+	// This is a guarunteed pass because Poll.Stop() always returns nil,
+	// but it's good to have a test for it should there be an error in the future
 	t.Run("stopping should work correctly", func(t *testing.T) {
 		p := NewPoll(1*time.Second, 0*time.Second)
 
