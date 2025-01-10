0f0271f243edc9e5ec6facdd2b2dfba75cf05546
JivusAyrus <suvijsurya76@gmail.com>
2025-01-10 21:42:30 +0530

    chore: remove migrations

--- controlplane/migrations/0110_smooth_hannibal_king.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "cache_warmer_operations" ADD COLUMN "client_name" text;--> statement-breakpoint
-ALTER TABLE "cache_warmer_operations" ADD COLUMN "client_version" text;
--- controlplane/migrations/0111_loud_bastion.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "cache_warmer_operations" ALTER COLUMN "is_manually_added" SET NOT NULL;
