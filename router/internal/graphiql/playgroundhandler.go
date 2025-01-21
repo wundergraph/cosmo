c52399a0fc8845f7926a2db9b4828d6d498e2665
Dave Freilich <dave@wundergraph.com>
2025-01-21 11:15:37 +0200

    fix: apply semaphore to playground limit

--- router/internal/graphiql/playgroundhandler.go
+++ router/internal/graphiql/playgroundhandler.go
@@ -1,27 +1,40 @@
 package graphiql
 
 import (
+	"bytes"
+	"golang.org/x/sync/semaphore"
 	"net/http"
 	"strconv"
 	"strings"
 )
 
 type PlaygroundOptions struct {
-	Html       string
-	GraphqlURL string
+	Html             string
+	GraphqlURL       string
+	ConcurrencyLimit int64
 }
 
 type Playground struct {
 	next          http.Handler
 	opts          *PlaygroundOptions
-	templateBytes *[]byte
+	templateBytes []byte
+	sem           *semaphore.Weighted
 }
+
+var (
+	defaultLimitUsage = int64(10)
+)
 
 func NewPlayground(opts *PlaygroundOptions) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
+		limit := opts.ConcurrencyLimit
+		if limit == 0 {
+			limit = defaultLimitUsage
+		}
 		p := &Playground{
 			next: next,
 			opts: opts,
+			sem:  semaphore.NewWeighted(opts.ConcurrencyLimit),
 		}
 		p.initPlayground()
 		return p
@@ -31,7 +44,7 @@ func NewPlayground(opts *PlaygroundOptions) func(http.Handler) http.Handler {
 func (p *Playground) initPlayground() {
 	tpl := strings.Replace(p.opts.Html, "{{graphqlURL}}", p.opts.GraphqlURL, -1)
 	play := []byte(tpl)
-	p.templateBytes = &play
+	p.templateBytes = play
 }
 
 func (p *Playground) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -44,9 +57,14 @@ func (p *Playground) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if err := p.sem.Acquire(r.Context(), 1); err != nil {
+		http.Error(w, "Too many requests", http.StatusTooManyRequests)
+		return
+	}
+	defer p.sem.Release(1) // Ensure the semaphore slot is released
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.Header().Set("Content-Length", strconv.Itoa(len(*p.templateBytes)))
+	w.Header().Set("Content-Length", strconv.Itoa(len(p.templateBytes)))
 
 	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write(*p.templateBytes)
+	_, _ = bytes.NewBuffer(p.templateBytes).WriteTo(w)
 }
