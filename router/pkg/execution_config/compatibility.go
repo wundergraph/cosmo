3d5b678fd5ea0442f4eb5cca50c60c9b15059b79
Aenimus <47415099+Aenimus@users.noreply.github.com>
2025-01-26 22:59:27 +0100

    feat: also add handshake for static execution configs (#1535)

--- router/pkg/execution_config/compatibility.go
+++ router/pkg/execution_config/compatibility.go
@@ -8,20 +8,20 @@ import (
 )
 
 const (
-	// ExecutionConfigVersionThreshold should ONLY be updated if there is a breaking change in the router execution config.
-	ExecutionConfigVersionThreshold         = 1
-	compatibilityVersionParseErrorMessage   = "Failed to parse compatibility version."
-	executionConfigVersionParseErrorMessage = "Failed to parse router execution config version of compatibility version."
+	// RouterCompatibilityVersionThreshold should ONLY be updated if there is a breaking change in the router execution config.
+	RouterCompatibilityVersionThreshold         = 1
+	compatibilityVersionParseErrorMessage       = "Failed to parse compatibility version."
+	routerCompatibilityVersionParseErrorMessage = "Failed to parse router execution config version of compatibility version."
 )
 
 func IsRouterCompatibleWithExecutionConfig(logger *zap.Logger, compatibilityVersion string) bool {
 	if compatibilityVersion == "" {
 		return true
 	}
-	/* A compatibility version is composed thus: <router execution configuration version>:<composition package version>
-	 * A router version supports a maximum router execution configuration version (ExecutionConfigVersionThreshold).
-	 * In the event the execution config version exceeds ExecutionConfigVersionThreshold, an error will request for
-	 * the router version be upgraded.
+	/* A compatibility version is composed thus: <router compatibility version>:<composition package version>
+	 * A router version supports a maximum router compatibility version (RouterCompatibilityVersionThreshold).
+	 * In the event the execution config version exceeds RouterCompatibilityVersionThreshold, an error will request the
+	 * router version be upgraded.
 	 * If the router version requires a newer router execution configuration version, a warning will explain that some
 	 * new features may be unavailable or functionality/behaviour may have changed.
 	 */
@@ -30,25 +30,25 @@ func IsRouterCompatibleWithExecutionConfig(logger *zap.Logger, compatibilityVers
 		logger.Error(compatibilityVersionParseErrorMessage, zap.String("compatibility_version", compatibilityVersion))
 		return false
 	}
-	routerExecutionVersion, err := strconv.ParseInt(segments[0], 10, 32)
+	routerCompatibilityVersion, err := strconv.ParseInt(segments[0], 10, 32)
 	if err != nil {
-		logger.Error(executionConfigVersionParseErrorMessage, zap.String("compatibility_version", compatibilityVersion))
+		logger.Error(routerCompatibilityVersionParseErrorMessage, zap.String("compatibility_version", compatibilityVersion))
 		return false
 	}
 	switch {
-	case routerExecutionVersion == ExecutionConfigVersionThreshold:
+	case routerCompatibilityVersion == RouterCompatibilityVersionThreshold:
 		return true
-	case routerExecutionVersion > ExecutionConfigVersionThreshold:
+	case routerCompatibilityVersion > RouterCompatibilityVersionThreshold:
 		logger.Error(
-			executionConfigVersionThresholdExceededError(routerExecutionVersion),
-			zap.Int64("execution_config_version", routerExecutionVersion),
+			executionConfigVersionThresholdExceededError(routerCompatibilityVersion),
+			zap.Int64("router_compatibility_version", routerCompatibilityVersion),
 			zap.String("composition_package_version", segments[1]),
 		)
 		return false
 	default:
 		logger.Warn(
-			executionConfigVersionInsufficientWarning(routerExecutionVersion),
-			zap.Int64("execution_config_version", routerExecutionVersion),
+			executionConfigVersionInsufficientWarning(routerCompatibilityVersion),
+			zap.Int64("router_compatibility_version", routerCompatibilityVersion),
 			zap.String("composition_package_version", segments[1]),
 		)
 		return true
@@ -58,7 +58,7 @@ func IsRouterCompatibleWithExecutionConfig(logger *zap.Logger, compatibilityVers
 func executionConfigVersionThresholdExceededError(executionConfigVersion int64) string {
 	return fmt.Sprintf(
 		"This router version supports a router execution config version up to %d. The router execution config version supplied is %d. Please upgrade your router version.",
-		ExecutionConfigVersionThreshold,
+		RouterCompatibilityVersionThreshold,
 		executionConfigVersion,
 	)
 }
@@ -66,7 +66,7 @@ func executionConfigVersionThresholdExceededError(executionConfigVersion int64) 
 func executionConfigVersionInsufficientWarning(executionConfigVersion int64) string {
 	return fmt.Sprintf(
 		"This router version requires a minimum router execution config version of %d to support all functionality. The router execution config version supplied is %d. Please create a new execution configuration.",
-		ExecutionConfigVersionThreshold,
+		RouterCompatibilityVersionThreshold,
 		executionConfigVersion,
 	)
 }
