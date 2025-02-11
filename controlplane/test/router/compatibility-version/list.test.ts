ccf52a4bbb725e8bfa943d1f9b0d2844cef2e5bc
Aenimus <daviditstutt@gmail.com>
2025-02-11 20:56:28 +0000

    chore: add mock

--- controlplane/test/router/compatibility-version/list.test.ts
+++ controlplane/test/router/compatibility-version/list.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, beforeEach, describe, expect, test } from 'vitest';
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, test, vi } from 'vitest';
 import { EnumStatusCode } from '@wundergraph/cosmo-connect/dist/common/common_pb';
 import { ROUTER_COMPATIBILITY_VERSIONS } from '@wundergraph/composition';
 import { SetupTest } from '../../test-util.js';
@@ -9,12 +9,24 @@ describe('router compatibility-version list tests', () => {
   let chClient: ClickHouseClient;
   let dbname = '';
 
+  vi.mock('../src/core/clickhouse/index.js', () => {
+    const ClickHouseClient = vi.fn();
+    ClickHouseClient.prototype.queryPromise = vi.fn();
+
+    return { ClickHouseClient };
+  });
+
+
   beforeAll(async () => {
     dbname = await beforeAllSetup();
   });
 
   beforeEach(() => {
     chClient = new ClickHouseClient();
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
   });
 
   afterAll(async () => {
