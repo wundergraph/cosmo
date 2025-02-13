5b756beb8a282d2378f022b606b32184051f95f7
Aenimus <daviditstutt@gmail.com>
2025-02-13 21:29:41 +0000

    chore: revert router changes

--- router/pkg/execution_config/compatibility.go
+++ router/pkg/execution_config/compatibility.go
@@ -3,7 +3,6 @@ package execution_config
 import (
 	"fmt"
 	"go.uber.org/zap"
-	"regexp"
 	"strconv"
 	"strings"
 )
@@ -14,8 +13,6 @@ const (
 	compatibilityVersionParseErrorMessage       = "Failed to parse compatibility version."
 	routerCompatibilityVersionParseErrorMessage = "Failed to parse router execution config version of compatibility version."
 )
-
-var routerCompatibilityVersionPattern = regexp.MustCompile(`^v(\d+$)`)
 
 func IsRouterCompatibleWithExecutionConfig(logger *zap.Logger, compatibilityVersion string) bool {
 	if compatibilityVersion == "" {
@@ -33,12 +30,7 @@ func IsRouterCompatibleWithExecutionConfig(logger *zap.Logger, compatibilityVers
 		logger.Error(compatibilityVersionParseErrorMessage, zap.String("compatibility_version", compatibilityVersion))
 		return false
 	}
-	matches := routerCompatibilityVersionPattern.FindStringSubmatch(segments[0])
-	if len(matches) != 2 {
-		logger.Error(routerCompatibilityVersionParseErrorMessage, zap.String("compatibility_version", compatibilityVersion))
-		return false
-	}
-	routerCompatibilityVersion, err := strconv.ParseInt(matches[1], 10, 32)
+	routerCompatibilityVersion, err := strconv.ParseInt(segments[0], 10, 32)
 	if err != nil {
 		logger.Error(routerCompatibilityVersionParseErrorMessage, zap.String("compatibility_version", compatibilityVersion))
 		return false
