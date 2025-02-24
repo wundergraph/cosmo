c9e79480eff4dcdafe3c814bc7fe42d021982151
Alessandro Pagnin <alepane@gmail.com>
2025-02-24 18:00:33 +0100

    chore: remove select

--- router/cmd/plan-generator.go
+++ router/cmd/plan-generator.go
@@ -65,31 +65,29 @@ func planGenerator() {
 				log.Fatalf("failed to create plan generator: %v", err)
 			}
 			for {
-				select {
-				case queryFile, ok := <-queriesQueue:
-					if !ok {
-						return
-					}
-					if filepath.Ext(queryFile.Name()) != ".graphql" {
-						continue
-					}
+				queryFile, ok := <-queriesQueue
+				if !ok {
+					return
+				}
+				if filepath.Ext(queryFile.Name()) != ".graphql" {
+					continue
+				}
 
-					if len(filter) > 0 && !slices.Contains(filter, queryFile.Name()) {
-						continue
-					}
+				if len(filter) > 0 && !slices.Contains(filter, queryFile.Name()) {
+					continue
+				}
 
-					queryFilePath := filepath.Join(queriesPath, queryFile.Name())
+				queryFilePath := filepath.Join(queriesPath, queryFile.Name())
 
-					outContent, err := pg.PlanOperation(queryFilePath)
-					if err != nil {
-						log.Printf("failed operation #%d: %s err: %v\n", i, queryFile.Name(), err.Error())
-					}
+				outContent, err := pg.PlanOperation(queryFilePath)
+				if err != nil {
+					log.Printf("failed operation #%d: %s err: %v\n", i, queryFile.Name(), err.Error())
+				}
 
-					outFileName := filepath.Join(outPath, queryFile.Name())
-					err = os.WriteFile(outFileName, []byte(outContent), 0644)
-					if err != nil {
-						log.Printf("failed to write file: %v", err)
-					}
+				outFileName := filepath.Join(outPath, queryFile.Name())
+				err = os.WriteFile(outFileName, []byte(outContent), 0644)
+				if err != nil {
+					log.Printf("failed to write file: %v", err)
 				}
 			}
 		}(i)
