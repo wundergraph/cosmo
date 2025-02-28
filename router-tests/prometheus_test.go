749d7edd0eebefa3247fabbfa23f83978815b54c
endigma <endigma@mailcat.ca>
2025-02-28 16:34:22 +0000

    improve readability of the exclude scope info integration test

--- router-tests/prometheus_test.go
+++ router-tests/prometheus_test.go
@@ -2986,83 +2986,25 @@ func TestPrometheus(t *testing.T) {
 
 			require.Len(t, requestsInFlightMetricsFiltered, 2)
 
-			require.Equal(t, []*io_prometheus_client.LabelPair{
-				{
-					Name:  PointerOf("wg_client_name"),
-					Value: PointerOf("unknown"),
-				},
-				{
-					Name:  PointerOf("wg_client_version"),
-					Value: PointerOf("missing"),
-				},
-				{
-					Name:  PointerOf("wg_federated_graph_id"),
-					Value: PointerOf("graph"),
-				},
-				{
-					Name:  PointerOf("wg_operation_protocol"),
-					Value: PointerOf("http"),
-				},
-				{
-					Name:  PointerOf("wg_router_cluster_name"),
-					Value: PointerOf(""),
-				},
-				{
-					Name:  PointerOf("wg_router_config_version"),
-					Value: PointerOf(xEnv.RouterConfigVersionMain()),
-				},
-				{
-					Name:  PointerOf("wg_router_version"),
-					Value: PointerOf("dev"),
-				},
-			}, requestsInFlightMetricsFiltered[0].Label)
+			require.NotContains(t, requestsInFlightMetricsFiltered[0].Label, &io_prometheus_client.LabelPair{
+				Name:  PointerOf("otel_scope_name"),
+				Value: PointerOf("cosmo.router.prometheus"),
+			})
 
-			require.Equal(t, []*io_prometheus_client.LabelPair{
-				{
-					Name:  PointerOf("wg_client_name"),
-					Value: PointerOf("unknown"),
-				},
-				{
-					Name:  PointerOf("wg_client_version"),
-					Value: PointerOf("missing"),
-				},
-				{
-					Name:  PointerOf("wg_federated_graph_id"),
-					Value: PointerOf("graph"),
-				},
-				{
-					Name:  PointerOf("wg_operation_name"),
-					Value: PointerOf("myQuery"),
-				},
-				{
-					Name:  PointerOf("wg_operation_protocol"),
-					Value: PointerOf("http"),
-				},
-				{
-					Name:  PointerOf("wg_operation_type"),
-					Value: PointerOf("query"),
-				},
-				{
-					Name:  PointerOf("wg_router_cluster_name"),
-					Value: PointerOf(""),
-				},
-				{
-					Name:  PointerOf("wg_router_config_version"),
-					Value: PointerOf(xEnv.RouterConfigVersionMain()),
-				},
-				{
-					Name:  PointerOf("wg_router_version"),
-					Value: PointerOf("dev"),
-				},
-				{
-					Name:  PointerOf("wg_subgraph_id"),
-					Value: PointerOf("0"),
-				},
-				{
-					Name:  PointerOf("wg_subgraph_name"),
-					Value: PointerOf("employees"),
-				},
-			}, requestsInFlightMetricsFiltered[1].Label)
+			require.NotContains(t, requestsInFlightMetricsFiltered[0].Label, &io_prometheus_client.LabelPair{
+				Name:  PointerOf("otel_scope_version"),
+				Value: PointerOf("0.0.1"),
+			})
+
+			require.NotContains(t, requestsInFlightMetricsFiltered[1].Label, &io_prometheus_client.LabelPair{
+				Name:  PointerOf("otel_scope_name"),
+				Value: PointerOf("cosmo.router.prometheus"),
+			})
+
+			require.NotContains(t, requestsInFlightMetricsFiltered[1].Label, &io_prometheus_client.LabelPair{
+				Name:  PointerOf("otel_scope_version"),
+				Value: PointerOf("0.0.1"),
+			})
 		})
 	})
 
