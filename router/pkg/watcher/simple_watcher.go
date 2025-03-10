38c701c6ed67dc39658b30b4bef290e1b9c772d0
endigma <endigma@mailcat.ca>
2025-03-10 14:50:16 +0000

    improve simplewatch logging

--- router/pkg/watcher/simple_watcher.go
+++ router/pkg/watcher/simple_watcher.go
@@ -12,23 +12,25 @@ func SimpleWatch(ctx context.Context, logger *zap.Logger, interval time.Duration
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 
+	ll := logger.With(zap.String("path", path))
+
 	var prevModTime time.Time
 
 	stat, err := os.Stat(path)
 	if err != nil {
-		logger.Debug("target file cannot be statted", zap.String("path", path), zap.Error(err))
+		ll.Debug("Target file cannot be statted", zap.Error(err))
 	} else {
 		prevModTime = stat.ModTime()
 	}
 
-	logger.Debug("watching", zap.String("filename", path), zap.Time("initialModTime", prevModTime))
+	ll.Debug("Watching file for changes", zap.Time("initialModTime", prevModTime))
 
 	for {
 		select {
 		case <-ticker.C:
 			stat, err := os.Stat(path)
 			if err != nil {
-				logger.Debug("target file cannot be statted", zap.String("path", path), zap.Error(err))
+				ll.Debug("Target file cannot be statted", zap.Error(err))
 
 				// Reset the mod time so we catch any new file at the target path
 				prevModTime = time.Time{}
@@ -36,7 +38,7 @@ func SimpleWatch(ctx context.Context, logger *zap.Logger, interval time.Duration
 				continue
 			}
 
-			logger.Debug("checking", zap.String("filename", path), zap.Time("prev", prevModTime), zap.Time("mod", stat.ModTime()))
+			ll.Debug("Checking file for changes", zap.Time("prev", prevModTime), zap.Time("mod", stat.ModTime()))
 
 			if stat.ModTime().After(prevModTime) {
 				prevModTime = stat.ModTime()
