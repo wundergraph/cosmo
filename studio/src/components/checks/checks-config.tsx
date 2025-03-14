9b78ec9db9633d3f5441f2402cc64e706ae40897
Wilson Rivera <wilson@wundergraph.com>
2025-03-13 20:03:43 -0400

    feat: convert the input to a dropdown with specific days range

--- studio/src/components/checks/checks-config.tsx
+++ studio/src/components/checks/checks-config.tsx
@@ -1,5 +1,3 @@
-import { useUser } from "@/hooks/use-user";
-import { SubmitHandler, useZodForm } from "@/hooks/use-form";
 import { useMutation } from "@connectrpc/connect-query";
 import { EnumStatusCode } from "@wundergraph/cosmo-connect/dist/common/common_pb";
 import {
@@ -8,20 +6,65 @@ import {
 import {
   GetNamespaceChecksConfigurationResponse,
 } from "@wundergraph/cosmo-connect/dist/platform/v1/platform_pb";
-import { Input } from "@/components/ui/input";
-import { Button } from "../ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { useToast } from "../ui/use-toast";
-import { z } from "zod";
-import {useMemo} from "react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Button } from "../ui/button";
+import { Card, CardContent } from "../ui/card";
+import { useState, useEffect } from "react";
+
+const TimeframeDropdown = ({
+  onChange,
+  value,
+  limit,
+  disabled,
+}: {
+  onChange: (value: number) => void;
+  value: string;
+  limit: number;
+  disabled: boolean;
+}) => {
+  const selectOptions = [
+    { label: "1 day", value: 1 },
+    { label: "3 days", value: 3 },
+    { label: "7 days", value: 7 },
+    { label: "10 days", value: 10 },
+    { label: "14 days", value: 14 },
+    { label: "30 days", value: 30 },
+    { label: "45 days", value: 45 },
+    { label: "60 days", value: 60 },
+    { label: "90 days", value: 90 },
+  ];
+
+  return (
+    <Select
+      value={value}
+      onValueChange={(value) => {
+        onChange(Number(value));
+      }}
+      disabled={disabled}
+    >
+      <SelectTrigger className="h-8 w-36">
+        <SelectValue placeholder={value} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {selectOptions.filter((option) => option.value <= limit).map((option) => (
+            <SelectItem key={`${option.value}`} value={`${option.value}`}>
+              {option.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
 
 export const ChecksConfig = ({
   namespace,
@@ -30,98 +73,86 @@ export const ChecksConfig = ({
   namespace: string;
   data: GetNamespaceChecksConfigurationResponse;
 }) => {
-  const user = useUser();
-
-  const schema = z.object({
-    timeframeInDays: z.coerce.number().min(1).max(data.timeframeLimitInDays),
-  });
-
-  type ChecksConfigurationInput = z.infer<typeof schema>;
+  const [timeframeInDays, setTimeframeInDays] = useState(data.timeframeInDays);
+  useEffect(() => setTimeframeInDays(data.timeframeInDays), [data.timeframeInDays]);
 
   const { mutate, isPending } = useMutation(updateNamespaceChecksConfig);
 
   const { toast } = useToast();
 
-  const form = useZodForm<ChecksConfigurationInput>({
-    schema,
-    mode: "onChange",
-    values: useMemo(() => ({ timeframeInDays: data.timeframeInDays }), [data]),
-  });
-
-  const onSubmit: SubmitHandler<ChecksConfigurationInput> = (data) => {
-    mutate(
-      {
-        namespace,
-        timeframeInDays: data.timeframeInDays,
-      },
-      {
-        onSuccess: (d) => {
-          if (d.response?.code === EnumStatusCode.OK) {
-            toast({
-              description: "Checks configuration updated successfully.",
-              duration: 3000,
-            });
-          } else if (d.response?.details) {
-            toast({ description: d.response.details, duration: 3000 });
-          }
-        },
-        onError: () => {
-          toast({
-            description:
-              "Could not update the checks configuration. Please try again.",
-            duration: 3000
-          });
-        },
-      }
-    )
-  };
-
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="flex flex-col gap-y-4"
-      >
-        <FormField
-          control={form.control}
-          name="timeframeInDays"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Days to consider</FormLabel>
-              <FormControl>
-                <Input
-                  type="number"
-                  min={1}
-                  max={data.timeframeLimitInDays}
-                  step={1}
-                  disabled={isPending}
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>
-                <p>This is the number of days to consider when performing a subgraph check.</p>
-                <p>Maximum value is <strong>{data.timeframeLimitInDays}</strong></p>
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+    <div className="space-y-6 rounded-lg border p-6">
+      <div className="flex w-full items-center justify-between">
+        <div className="flex flex-col gap-y-1">
+          <h3 className="font-semibold tracking-tight">Schema Checks Configuration</h3>
+          <p className="text-sm text-muted-foreground">
+            Configure the options used for checks of subgraphs of the namespace.
+          </p>
+        </div>
 
         <Button
-          className="ml-auto"
           isLoading={isPending}
-          type="submit"
-          disabled={
-            !form.formState.isValid ||
-            (
-              !user?.currentOrganization.roles.includes("admin") &&
-              !user?.currentOrganization.roles.includes("developer")
-            )
-          }
+          onClick={() => {
+            mutate(
+              { namespace, timeframeInDays, },
+              {
+                onSuccess: (d) => {
+                  if (d.response?.code === EnumStatusCode.OK) {
+                    toast({
+                      description: "Schema checks configuration updated successfully.",
+                      duration: 3000,
+                    });
+                  } else if (d.response?.details) {
+                    toast({ description: d.response.details, duration: 3000 });
+                  }
+                },
+                onError: () => {
+                  toast({
+                    description:
+                      "Could not update the schema checks configuration. Please try again.",
+                    duration: 3000
+                  });
+                },
+              }
+            );
+          }}
         >
           Save
         </Button>
-      </form>
-    </Form>
+      </div>
+
+      <Card>
+        <CardContent className="pt-6">
+          <div
+            className="flex w-full flex-col justify-between gap-y-4 md:flex-row md:items-center"
+          >
+            <div className="flex items-start gap-x-4">
+              <div className="flex flex-col gap-y-1">
+                <span
+                  className="break-all text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Check days to consider
+                </span>
+                <span className="text-sm text-muted-foreground">
+                  This is the number of days to consider when performing a subgraph check.
+                </span>
+              </div>
+            </div>
+
+            <div className="ml-8 md:ml-0">
+              <TimeframeDropdown
+                value={`${timeframeInDays}`}
+                limit={data.timeframeLimitInDays}
+                onChange={(value) => {
+                  setTimeframeInDays(value);
+                  console.log(value);
+                }}
+                disabled={isPending}
+              />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
