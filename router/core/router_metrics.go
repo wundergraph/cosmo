6344b85f33e39512bb39aa809763ae2f46f06bc5
endigma <endigma@mailcat.ca>
2025-03-18 11:03:59 +0100

    feat: schema usage metrics in prometheus

--- router/core/router_metrics.go
+++ router/core/router_metrics.go
@@ -1,9 +1,10 @@
 package core
 
 import (
+	"strconv"
+
 	"go.opentelemetry.io/otel/attribute"
 	otelmetric "go.opentelemetry.io/otel/metric"
-	"strconv"
 
 	graphqlmetricsv1 "github.com/wundergraph/cosmo/router/gen/proto/wg/cosmo/graphqlmetrics/v1"
 	"github.com/wundergraph/cosmo/router/internal/graphqlmetrics"
