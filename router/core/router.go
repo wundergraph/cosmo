638964c508a5903a77c8b19180e157ffd28b48c5
endigma <endigma@mailcat.ca>
2025-03-18 12:08:56 +0100

    Add Prometheus GraphQL schema usage metrics option

--- router/core/router.go
+++ router/core/router.go
@@ -106,8 +106,9 @@ type (
 	}
 
 	GraphQLMetricsConfig struct {
-		Enabled           bool
-		CollectorEndpoint string
+		Enabled                 bool
+		PrometheusExportEnabled bool
+		CollectorEndpoint       string
 	}
 
 	BatchingConfig struct {
@@ -1764,8 +1765,9 @@ func DefaultSubgraphTransportOptions() *SubgraphTransportOptions {
 
 func DefaultGraphQLMetricsConfig() *GraphQLMetricsConfig {
 	return &GraphQLMetricsConfig{
-		Enabled:           false,
-		CollectorEndpoint: "",
+		Enabled:                 false,
+		PrometheusExportEnabled: false,
+		CollectorEndpoint:       "",
 	}
 }
 
