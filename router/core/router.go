752b48bfeeedc9c49d03aaca127b3de03de02d78
endigma <endigma@mailcat.ca>
2025-03-19 09:40:54 +0100

    clean up naming

--- router/core/router.go
+++ router/core/router.go
@@ -106,9 +106,8 @@ type (
 	}
 
 	GraphQLMetricsConfig struct {
-		Enabled                 bool
-		PrometheusExportEnabled bool
-		CollectorEndpoint       string
+		Enabled           bool
+		CollectorEndpoint string
 	}
 
 	BatchingConfig struct {
@@ -1765,9 +1764,8 @@ func DefaultSubgraphTransportOptions() *SubgraphTransportOptions {
 
 func DefaultGraphQLMetricsConfig() *GraphQLMetricsConfig {
 	return &GraphQLMetricsConfig{
-		Enabled:                 false,
-		PrometheusExportEnabled: false,
-		CollectorEndpoint:       "",
+		Enabled:           false,
+		CollectorEndpoint: "",
 	}
 }
 
