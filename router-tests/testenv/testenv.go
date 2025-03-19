ba0b3328675c65dde8426192c01dc1adc91a4af2
endigma <endigma@mailcat.ca>
2025-03-19 09:40:54 +0100

    clean up naming

--- router-tests/testenv/testenv.go
+++ router-tests/testenv/testenv.go
@@ -255,7 +255,7 @@ type MetricOptions struct {
 	EnablePrometheusRouterCache  bool
 	OTLPEngineStatsOptions       EngineStatOptions
 	PrometheusEngineStatsOptions EngineStatOptions
-	EnablePrometheusSchemaUsage  bool
+	PromIncludeSchemaUsage       bool
 }
 
 type Config struct {
@@ -918,7 +918,7 @@ func configureRouter(listenerAddr string, testConfig *Config, routerConfig *node
 			ExcludeMetrics:      testConfig.MetricOptions.MetricExclusions.ExcludedPrometheusMetrics,
 			ExcludeMetricLabels: testConfig.MetricOptions.MetricExclusions.ExcludedPrometheusMetricLabels,
 			ExcludeScopeInfo:    testConfig.MetricOptions.MetricExclusions.ExcludeScopeInfo,
-			IncludeSchemaUsage:  testConfig.MetricOptions.EnablePrometheusSchemaUsage,
+			IncludeSchemaUsage:  testConfig.MetricOptions.PromIncludeSchemaUsage,
 		}
 	}
 
