6f500d9fc1f98c5f84660f604558371d3b995d36
endigma <endigma@mailcat.ca>
2025-03-26 13:47:32 +0000

    add test for reused fields

--- router-tests/prometheus_improved_test.go
+++ router-tests/prometheus_improved_test.go
@@ -87,6 +87,74 @@ func TestPrometheusSchemaUsage(t *testing.T) {
 		})
 	})
 
+	t.Run("multiple usages of field are counted", func(t *testing.T) {
+		t.Parallel()
+
+		metricReader := metric.NewManualReader()
+		promRegistry := prometheus.NewRegistry()
+
+		testenv.Run(t, &testenv.Config{
+			MetricReader:       metricReader,
+			PrometheusRegistry: promRegistry,
+			MetricOptions: testenv.MetricOptions{
+				PrometheusSchemaFieldUsage: testenv.PrometheusSchemaFieldUsage{
+					Enabled: true,
+				},
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			res := xEnv.MakeGraphQLRequestOK(testenv.GraphQLRequest{
+				Query: `
+				query myQuery {
+					employee1: employee(id: 1) { id currentMood }
+					employee2: employee(id: 2) { id }
+				}`,
+			})
+			require.JSONEq(t, `{
+			    "data": {
+			        "employee1": {
+			            "id": 1,
+			            "currentMood": "HAPPY"
+			        },
+			        "employee2": {
+			            "id": 2
+			        }
+			    }
+			}`, res.Body)
+
+			mf, err := promRegistry.Gather()
+			require.NoError(t, err)
+
+			schemaUsage := findMetricFamilyByName(mf, SchemaFieldUsageMetricName)
+			assert.NotNil(t, schemaUsage)
+
+			schemaUsageMetrics := schemaUsage.GetMetric()
+
+			require.Len(t, schemaUsageMetrics, 3)
+
+			for _, metric := range schemaUsageMetrics {
+				assertLabelValue(t, metric.Label, otel.WgOperationName, "myQuery")
+				assertLabelValue(t, metric.Label, otel.WgOperationType, "query")
+
+				assertLabelNotPresent(t, metric.Label, otel.WgOperationSha256)
+			}
+
+			assertLabelValue(t, schemaUsageMetrics[0].Label, otel.WgGraphQLFieldName, "currentMood")
+			assertLabelValue(t, schemaUsageMetrics[0].Label, otel.WgGraphQLFieldType, "Employee")
+
+			assert.InEpsilon(t, 1.0, *schemaUsageMetrics[0].Counter.Value, 0.0001)
+
+			assertLabelValue(t, schemaUsageMetrics[1].Label, otel.WgGraphQLFieldName, "employee")
+			assertLabelValue(t, schemaUsageMetrics[1].Label, otel.WgGraphQLFieldType, "Query")
+
+			assert.InEpsilon(t, 2.0, *schemaUsageMetrics[1].Counter.Value, 0.0001)
+
+			assertLabelValue(t, schemaUsageMetrics[2].Label, otel.WgGraphQLFieldName, "id")
+			assertLabelValue(t, schemaUsageMetrics[2].Label, otel.WgGraphQLFieldType, "Employee")
+
+			assert.InEpsilon(t, 2.0, *schemaUsageMetrics[2].Counter.Value, 0.0001)
+		})
+	})
+
 	t.Run("include operation sha when enabled", func(t *testing.T) {
 		t.Parallel()
 
