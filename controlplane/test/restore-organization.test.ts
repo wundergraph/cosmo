0d9962ce793c311c7b550489d8875972e1a600cb
Wilson Rivera <wilson@wundergraph.com>
2025-04-01 13:25:15 -0400

    feat: improve the organization deletion dialog

--- controlplane/test/restore-organization.test.ts
+++ controlplane/test/restore-organization.test.ts
@@ -1,8 +1,6 @@
 import { EnumStatusCode } from '@wundergraph/cosmo-connect/dist/common/common_pb';
 import { joinLabel } from '@wundergraph/cosmo-shared';
 import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, test, vi } from 'vitest';
-import { QueueEvents } from "bullmq";
-import { OidcRepository } from '../src/core/repositories/OidcRepository.js';
 import { OrganizationRepository } from '../src/core/repositories/OrganizationRepository.js';
 import { afterAllSetup, beforeAllSetup, genID, genUniqueLabel, TestUser } from '../src/core/test-util.js';
 import { ClickHouseClient } from '../src/core/clickhouse/index.js';
--- studio/src/components/settings/delete-organization.tsx
+++ studio/src/components/settings/delete-organization.tsx
@@ -25,7 +25,7 @@ export const DeleteOrganization = () => {
   );
   const canDeleteOrganization = !hasActiveSubscription && user?.currentOrganization.roles.includes("admin");
 
-  const regex = new RegExp(`^I want to delete my organization ${user?.currentOrganization.name}$`);
+  const regex = new RegExp(`^I want to delete the organization ${user?.currentOrganization.name}$`);
   const schema = z.object({
     organizationName: z.string().regex(regex, {
       message: "Please enter the organization name as requested.",
@@ -117,24 +117,28 @@ export const DeleteOrganization = () => {
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
-              <DialogTitle>
-                Delete {user?.currentOrganization?.name}
+              <DialogTitle className="leading-6">
+                <span className="text-destructive">Permanently</span> delete the organization{" "}
+                &quot;{user?.currentOrganization?.name}&quot;
               </DialogTitle>
             </DialogHeader>
             <form onSubmit={handleSubmit(handleDeleteOrg)} className="mt-2 space-y-3">
               <div>
-                Removing the organization <strong>{user?.currentOrganization?.name}</strong> is a permanent action
-                which cannot be undone! Are you sure you want to continue?
+                Deleting the organization &quot;<strong>{user?.currentOrganization?.name}</strong>&quot; is a{" "}
+                <span className="text-destructive">permanent</span> action that cannot be undone.
               </div>
 
               <div>
-                This will remove the organization, graphs, subgraphs, feature flags, members, api keys and
-                any other
+                Deleting the organization will also delete any graphs, subgraphs, feature flags, members, API keys{" "}
+                tied to the organization as well as any other related data.
               </div>
 
               <div className="flex flex-col gap-y-3">
                 <span>
-                  To confirm, enter &quot;I want to delete my organization {user?.currentOrganization.name}&quot;
+                  To confirm, enter &quot;
+                  <span className="rounded-md border px-1 font-bold focus:outline-none bg-secondary text-secondary-foreground">
+                    I want to delete the organization {user?.currentOrganization.name}
+                  </span>&quot;
                   in the box below.
                 </span>
                 <Input
