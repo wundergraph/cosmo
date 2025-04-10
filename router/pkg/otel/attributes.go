87552bb385514b800ad61da4913c0037b8fe7be8
endigma <endigma@mailcat.ca>
2025-04-10 19:17:06 +0100

    Move comment to make it not a docs comment

--- router/pkg/otel/attributes.go
+++ router/pkg/otel/attributes.go
@@ -46,10 +46,12 @@ const (
 	WgIsBatchingOperation              = attribute.Key("wg.operation.batching.is_batched")
 	WgBatchingOperationsCount          = attribute.Key("wg.operation.batching.operations_count")
 	WgBatchingOperationIndex           = attribute.Key("wg.operation.batching.operation_index")
+
 	// HTTPRequestUploadFileCount is the number of files uploaded in a request (Not specified in the OpenTelemetry specification)
 	HTTPRequestUploadFileCount = attribute.Key("http.request.upload.file_count")
 
 	// Prometheus Schema Field Usage Attrs
+
 	WgOperationSha256   = attribute.Key("wg.operation.sha256")
 	WgGraphQLFieldName  = attribute.Key("wg.graphql.field.name")
 	WgGraphQLParentType = attribute.Key("wg.graphql.parent_type")
