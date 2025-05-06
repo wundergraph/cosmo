ac234c33fa65bb0e130d7c59481c03cf15c3692f
spetrunin <neyasut@gmail.com>
2025-05-06 20:56:50 +0300

    chore: use assert for introspection tests

--- router-tests/introspection_test.go
+++ router-tests/introspection_test.go
@@ -1,9 +1,12 @@
 package integration
 
 import (
+	"bytes"
+	"encoding/json"
 	"testing"
 
 	"github.com/sebdah/goldie/v2"
+	"github.com/stretchr/testify/require"
 
 	"github.com/wundergraph/cosmo/router-tests/testenv"
 )
@@ -125,7 +128,9 @@ func TestIntrospection(t *testing.T) {
 			res := xEnv.MakeGraphQLRequestOK(testenv.GraphQLRequest{
 				Query: introspectionQuery,
 			})
-			g.AssertJson(t, "base-graph-schema", res.Body)
+			body := bytes.NewBuffer(nil)
+			require.NoError(t, json.Indent(body, []byte(res.Body), "", "  "))
+			g.Assert(t, "base-graph-schema", body.Bytes())
 		})
 	})
 
@@ -139,7 +144,9 @@ func TestIntrospection(t *testing.T) {
 					"X-Feature-Flag": {"myff"},
 				},
 			})
-			g.AssertJson(t, "feature-graph-schema", res.Body)
+			body := bytes.NewBuffer(nil)
+			require.NoError(t, json.Indent(body, []byte(res.Body), "", "  "))
+			g.Assert(t, "feature-graph-schema", body.Bytes())
 		})
 	})
 
