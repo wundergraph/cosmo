9d0d78e90d629cf8d0b42120f521f1edeeabf7ff
endigma <endigma@mailcat.ca>
2025-05-20 16:49:59 +0100

    Improve demo subgraph docker build speed

--- demo/deploy.sh
+++ demo/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-fly deploy -a product-api --dockerfile ./Dockerfile.products
-fly deploy -a employees-api --dockerfile ./Dockerfile.employees
-fly deploy -a family-api --dockerfile ./Dockerfile.family
-fly deploy -a hobbies-api --dockerfile ./Dockerfile.hobbies
-fly deploy -a availability-api --dockerfile ./Dockerfile.availability
-fly deploy -a mood-api --dockerfile ./Dockerfile.mood
-fly deploy -a product-api-fs --dockerfile ./Dockerfile.products_fg
+fly deploy -a product-api --dockerfile ./Dockerfile --build-target products
+fly deploy -a employees-api --dockerfile ./Dockerfile --build-target employees
+fly deploy -a family-api --dockerfile ./Dockerfile --build-target family
+fly deploy -a hobbies-api --dockerfile ./Dockerfile --build-target hobbies
+fly deploy -a availability-api --dockerfile ./Dockerfile --build-target availability
+fly deploy -a mood-api --dockerfile ./Dockerfile --build-target mood
+fly deploy -a product-api-fs --dockerfile ./Dockerfile --build-target products_fg
