c7bd0857edacf442b48fce66ced9c6d22b817c08
lawli <lawli@ebay.com>
2025-05-20 18:41:42 +0800

    tf token

--- router/cmd/custom/main.go
+++ router/cmd/custom/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	routercmd "github.com/wundergraph/cosmo/router/cmd"
+
 	// Import your modules here
 	_ "github.com/wundergraph/cosmo/router/cmd/custom/module"
 )
 
 func main() {
+	fmt.Println("Starting custom router...")
 	routercmd.Main()
 }
