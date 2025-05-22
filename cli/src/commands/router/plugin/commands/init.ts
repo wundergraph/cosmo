ee2b87c3ca61e51e46de1653a41b77654affa536
Dustin Deus <deusdustin@gmail.com>
2025-05-22 18:44:12 +0200

    feat: plugin init should bootstrap fully functional project (#1878)

--- cli/src/commands/router/plugin/commands/init.ts
+++ cli/src/commands/router/plugin/commands/init.ts
@@ -1,26 +1,39 @@
+/* eslint-disable import/no-named-as-default-member */
+
 import { access, mkdir, rename, rm, writeFile } from 'node:fs/promises';
 import { tmpdir } from 'node:os';
 import { randomUUID } from 'node:crypto';
 import { Command, program } from 'commander';
-import { relative, resolve } from 'pathe';
+import { resolve } from 'pathe';
 import pc from 'picocolors';
 import pupa from 'pupa';
 import Spinner from 'ora';
 import { compileGraphQLToMapping, compileGraphQLToProto } from '@wundergraph/protographic';
 import { camelCase, upperFirst } from 'lodash-es';
 import { BaseCommandOptions } from '../../../../core/types/types.js';
-import { goMod, mainGo, mainGoTest, readme, schema } from '../templates/go-plugin.js';
+import PluginTemplates from '../templates/plugin.js';
+import ProjectTemplates from '../templates/project.js';
 import { renderResultTree } from '../helper.js';
 
 export default (opts: BaseCommandOptions) => {
   const command = new Command('init');
   command.description('Scaffold a new gRPC router plugin');
   command.argument('name', 'Name of the plugin');
-  command.option('-d, --directory <directory>', 'Directory to create the plugin in', '.');
+  command.option('-p, --project <project>', 'Project name', 'cosmo');
+  command.option('-d, --directory <directory>', 'Directory to create the project in', '.');
+  command.option('--only-plugin', 'Only create the plugin without a router project', false);
   command.option('-l, --language <language>', 'Programming language to use for the plugin', 'go');
   command.action(async (name, options) => {
     const startTime = performance.now();
-    const pluginDir = resolve(process.cwd(), options.directory, name);
+    const cwd = process.cwd();
+
+    if (options.onlyPlugin) {
+      options.project = '';
+    }
+
+    const projectDir = resolve(cwd, options.directory, options.project);
+    const pluginDir = resolve(cwd, projectDir, 'plugins', name);
+    const originalPluginName = name;
 
     name = upperFirst(camelCase(name));
     const serviceName = name + 'Service';
@@ -55,33 +68,88 @@ export default (opts: BaseCommandOptions) => {
         options.language = 'go';
       }
 
-      await writeFile(resolve(tempDir, 'README.md'), pupa(readme, { name }));
-      await writeFile(resolve(srcDir, 'schema.graphql'), pupa(schema, { name }));
+      const goModulePath = 'github.com/wundergraph/cosmo/plugin';
 
       spinner.text = 'Generating mapping and proto files...';
 
-      const mapping = compileGraphQLToMapping(schema, serviceName);
-      await writeFile(resolve(generatedDir, 'mapping.json'), JSON.stringify(mapping, null, 2));
+      if (!options.onlyPlugin && options.project) {
+        await writeFile(resolve(tempDir, 'README.md'), pupa(ProjectTemplates.readme, { name, originalPluginName }));
+        await writeFile(resolve(srcDir, 'schema.graphql'), pupa(PluginTemplates.schema, { name }));
 
-      const goModulePath = 'github.com/wundergraph/cosmo/plugin';
+        const mapping = compileGraphQLToMapping(PluginTemplates.schema, serviceName);
+        await writeFile(resolve(generatedDir, 'mapping.json'), JSON.stringify(mapping, null, 2));
 
-      const proto = compileGraphQLToProto(schema, {
-        serviceName,
-        packageName: 'service',
-        goPackage: goModulePath,
-      });
+        const proto = compileGraphQLToProto(PluginTemplates.schema, {
+          serviceName,
+          packageName: 'service',
+          goPackage: goModulePath,
+        });
 
-      await writeFile(resolve(generatedDir, 'service.proto'), proto.proto);
-      await writeFile(resolve(generatedDir, 'service.proto.lock.json'), JSON.stringify(proto.lockData, null, 2));
+        await writeFile(resolve(generatedDir, 'service.proto'), proto.proto);
+        await writeFile(resolve(generatedDir, 'service.proto.lock.json'), JSON.stringify(proto.lockData, null, 2));
+        await writeFile(resolve(srcDir, 'main.go'), pupa(PluginTemplates.mainGo, { serviceName }));
+        await writeFile(resolve(srcDir, 'main_test.go'), pupa(PluginTemplates.mainGoTest, { serviceName }));
+        await writeFile(resolve(tempDir, 'go.mod'), pupa(PluginTemplates.goMod, { modulePath: goModulePath }));
+        await writeFile(resolve(tempDir, 'Makefile'), pupa(PluginTemplates.makefile, { originalPluginName }));
+        await writeFile(resolve(tempDir, '.gitignore'), PluginTemplates.gitignore);
+        await writeFile(resolve(tempDir, '.cursorignore'), PluginTemplates.cursorIgnore);
 
-      await writeFile(resolve(srcDir, 'main.go'), pupa(mainGo, { serviceName }));
-      await writeFile(resolve(srcDir, 'main_test.go'), pupa(mainGoTest, { serviceName }));
+        // Create cursor rules in .cursor/rules
+        await mkdir(resolve(tempDir, '.cursor', 'rules'), { recursive: true });
+        await writeFile(
+          resolve(tempDir, '.cursor', 'rules', 'plugin-development.mdc'),
+          pupa(PluginTemplates.cursorRules, { name, originalPluginName }),
+        );
 
-      // go mod init
-      await writeFile(resolve(tempDir, 'go.mod'), pupa(goMod, { modulePath: goModulePath }));
+        // Create a project directory structure
+        await mkdir(projectDir, { recursive: true });
+        await mkdir(resolve(projectDir, 'plugins'), { recursive: true });
 
-      await mkdir(resolve(process.cwd(), options.directory), { recursive: true });
-      await rename(tempDir, pluginDir);
+        // Write router config to the project root
+        await writeFile(resolve(projectDir, 'config.yaml'), ProjectTemplates.routerConfig);
+        await writeFile(resolve(projectDir, 'graph.yaml'), pupa(ProjectTemplates.graphConfig, { originalPluginName }));
+        await writeFile(resolve(projectDir, 'Makefile'), pupa(ProjectTemplates.makefile, { originalPluginName }));
+        await writeFile(resolve(projectDir, '.gitignore'), ProjectTemplates.gitignore);
+        await writeFile(
+          resolve(projectDir, 'README.md'),
+          pupa(ProjectTemplates.projectReadme, { name, originalPluginName }),
+        );
+
+        // Move plugin from temp directory to project plugins directory
+        await rename(tempDir, pluginDir);
+      } else {
+        await writeFile(resolve(tempDir, 'README.md'), pupa(PluginTemplates.readme, { name, originalPluginName }));
+        await writeFile(resolve(tempDir, 'Makefile'), pupa(PluginTemplates.makefile, { originalPluginName }));
+        await writeFile(resolve(srcDir, 'schema.graphql'), pupa(PluginTemplates.schema, { name }));
+
+        const mapping = compileGraphQLToMapping(PluginTemplates.schema, serviceName);
+        await writeFile(resolve(generatedDir, 'mapping.json'), JSON.stringify(mapping, null, 2));
+
+        // Create cursor rules in .cursor/rules
+        await mkdir(resolve(tempDir, '.cursor', 'rules'), { recursive: true });
+        await writeFile(
+          resolve(tempDir, '.cursor', 'rules', 'plugin-development.mdc'),
+          pupa(PluginTemplates.cursorRules, { name, originalPluginName }),
+        );
+
+        const proto = compileGraphQLToProto(PluginTemplates.schema, {
+          serviceName,
+          packageName: 'service',
+          goPackage: goModulePath,
+        });
+
+        await writeFile(resolve(generatedDir, 'service.proto'), proto.proto);
+        await writeFile(resolve(generatedDir, 'service.proto.lock.json'), JSON.stringify(proto.lockData, null, 2));
+        await writeFile(resolve(srcDir, 'main.go'), pupa(PluginTemplates.mainGo, { serviceName }));
+        await writeFile(resolve(srcDir, 'main_test.go'), pupa(PluginTemplates.mainGoTest, { serviceName }));
+        await writeFile(resolve(tempDir, 'go.mod'), pupa(PluginTemplates.goMod, { modulePath: goModulePath }));
+        await writeFile(resolve(tempDir, '.gitignore'), PluginTemplates.gitignore);
+        await writeFile(resolve(tempDir, '.cursorignore'), PluginTemplates.cursorIgnore);
+
+        await mkdir(resolve(projectDir, 'plugins'), { recursive: true });
+
+        await rename(tempDir, pluginDir);
+      }
 
       const endTime = performance.now();
       const elapsedTimeMs = endTime - startTime;
--- router/__schemas/plugin.sh
+++ router/__schemas/plugin.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Commented commands for reference
-#pnpm --reporter=silent wgc router compose -i ../router/_schemas/issue-167/graph.yaml > ../router/_schemas/issue-167/config.json
-#pnpm wgc router compose -i ../router/__schemas/graph.yaml -o ../router/__schemas/config.json
-
 rm -rf ../plugins
 
 pnpx tsx --env-file ../../cli/.env ../../cli/src/index.ts router plugin init hello-world --only-plugin -d ../
