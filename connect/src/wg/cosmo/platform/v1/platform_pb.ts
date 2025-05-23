c506af7124bbee1ce643c05ffe332e242c308b03
JivusAyrus <suvijsurya76@gmail.com>
2025-05-24 01:50:27 +0530

    feat: fetch operations based on clients

--- connect/src/wg/cosmo/platform/v1/platform_pb.ts
+++ connect/src/wg/cosmo/platform/v1/platform_pb.ts
@@ -20941,6 +20941,11 @@ export class GetOperationsRequest extends Message<GetOperationsRequest> {
    */
   namespace = "";
 
+  /**
+   * @generated from field: optional string clientName = 3;
+   */
+  clientName?: string;
+
   constructor(data?: PartialMessage<GetOperationsRequest>) {
     super();
     proto3.util.initPartial(data, this);
@@ -20951,6 +20956,7 @@ export class GetOperationsRequest extends Message<GetOperationsRequest> {
   static readonly fields: FieldList = proto3.util.newFieldList(() => [
     { no: 1, name: "federatedGraphName", kind: "scalar", T: 9 /* ScalarType.STRING */ },
     { no: 2, name: "namespace", kind: "scalar", T: 9 /* ScalarType.STRING */ },
+    { no: 3, name: "clientName", kind: "scalar", T: 9 /* ScalarType.STRING */, opt: true },
   ]);
 
   static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): GetOperationsRequest {
