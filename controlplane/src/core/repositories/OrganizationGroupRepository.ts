460195019838d87499498c888a29083e4970572c
Wilson Rivera <wilson@wundergraph.com>
2025-05-25 05:10:39 -0400

    feat: don't include the api keys in the group members count

--- controlplane/src/core/repositories/OrganizationGroupRepository.ts
+++ controlplane/src/core/repositories/OrganizationGroupRepository.ts
@@ -112,11 +112,12 @@ export class OrganizationGroupRepository {
             FROM "organization_group_members" as "ogm"
             WHERE "ogm"."group_id" = "organizationGroups"."id"
           ) AS INTEGER)
-          +
+        `.as('members_count'),
+        apiKeysCount: sql<number>`
           CAST((
             SELECT COUNT("api_keys"."id") FROM "api_keys" WHERE "api_keys"."group_id" = "organizationGroups"."id"
           ) AS INTEGER)
-        `.as('members_count'),
+        `.as('api_keys_count'),
       }),
     });
 
