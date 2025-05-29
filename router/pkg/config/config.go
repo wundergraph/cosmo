3d1aef05047272dd7b9180642d0e600bc9d0b70f
Milinda Dias <83293842+SkArchon@users.noreply.github.com>
2025-05-29 14:23:47 +0530

    feat: add new negate flag to regular expressions (#1911)

--- router/pkg/config/config.go
+++ router/pkg/config/config.go
@@ -251,7 +251,8 @@ type RequestHeaderRule struct {
 	Operation HeaderRuleOperation `yaml:"op"`
 	// Propagate options
 	// Matching is the regex to match the header name against
-	Matching string `yaml:"matching"`
+	Matching    string `yaml:"matching"`
+	NegateMatch bool   `yaml:"negate_match,omitempty"`
 	// Named is the exact header name to match
 	Named string `yaml:"named"`
 	// Rename renames the header's key to the provided value
@@ -295,7 +296,8 @@ type ResponseHeaderRule struct {
 	// Operation describes the header operation to perform e.g. "propagate"
 	Operation HeaderRuleOperation `yaml:"op"`
 	// Matching is the regex to match the header name against
-	Matching string `yaml:"matching"`
+	Matching    string `yaml:"matching"`
+	NegateMatch bool   `yaml:"negate_match,omitempty"`
 	// Named is the exact header name to match
 	Named string `yaml:"named"`
 	// Rename renames the header's key to the provided value
