a27445a9029aaee9322a114b7659f94f593c6db3
JivusAyrus <suvijsurya76@gmail.com>
2025-06-12 22:59:54 +0530

    chore: remove deprecated SQL migration files for gql_metrics_operations and add new migrations for data migration and table renaming.

--- graphqlmetrics/migrations/20250204112304_add_org_id_to_gql_metrics_operations.sql
+++
@@ -1,6 +0,0 @@
--- migrate:up
-ALTER TABLE gql_metrics_operations ADD COLUMN IF NOT EXISTS OrganizationID LowCardinality(String) CODEC(ZSTD(3));
-
--- migrate:down
-
-ALTER TABLE gql_metrics_operations DROP COLUMN OrganizationID;
