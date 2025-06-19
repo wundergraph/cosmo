7f8eb9b7b8e937309d7a6e4b81b1a08364f2e837
endigma <endigma@mailcat.ca>
2025-06-19 10:27:43 +0100

    Add complex query to QueryPlanStats module test

--- router-tests/modules/query_stats_test.go
+++ router-tests/modules/query_stats_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 func TestCustomModuleQueryStats(t *testing.T) {
-	t.Run("it gets the correct stats for a query", func(t *testing.T) {
+	t.Run("it gets the correct stats for a simple query", func(t *testing.T) {
 		t.Parallel()
 
 		resultsChan := make(chan core.QueryPlanStats, 1)
@@ -48,6 +48,105 @@ func TestCustomModuleQueryStats(t *testing.T) {
 			testenv.AwaitChannelWithT(t, 10*time.Second, resultsChan, func(t *testing.T, qps core.QueryPlanStats) {
 				assert.Equal(t, 4, qps.TotalSubgraphFetches)
 				assert.Equal(t, map[string]int{"employees": 1, "hobbies": 1, "mood": 2}, qps.SubgraphFetches)
+			})
+		})
+	})
+
+	t.Run("it gets the correct stats for a very complex query", func(t *testing.T) {
+		t.Parallel()
+
+		resultsChan := make(chan core.QueryPlanStats, 1)
+
+		cfg := config.Config{
+			Graph: config.Graph{},
+			Modules: map[string]any{
+				"myModule": module.MyModule{
+					Value: 1,
+				},
+				"queryStatsModule": queryStatsModule.QueryStatsModule{
+					ResultsChan: resultsChan,
+				},
+			},
+		}
+
+		testenv.Run(t, &testenv.Config{
+			RouterOptions: []core.Option{
+				core.WithModulesConfig(cfg.Modules),
+				core.WithCustomModules(&module.MyModule{}, &queryStatsModule.QueryStatsModule{}),
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			res, err := xEnv.MakeGraphQLRequest(testenv.GraphQLRequest{
+				Query: `
+				query Employees {
+				    employees {
+				        id
+				        tag
+				        notes
+				        updatedAt
+				        currentMood
+				        derivedMood
+				        isAvailable
+				        products
+				        details {
+				            forename
+				            surname
+				            middlename
+				            hasChildren
+				            maritalStatus
+				            nationality
+				            location {
+				                key {
+				                    name
+				                }
+				            }
+				            pastLocations {
+				                type
+				                name
+				                country {
+				                    key {
+				                        name
+				                    }
+				                }
+				            }
+				            pets {
+				                class
+				                gender
+				                name
+				            }
+				        }
+				        role {
+				            departments
+				            title
+				            employees {
+				                id
+				                tag
+				                notes
+				                updatedAt
+				                currentMood
+				                derivedMood
+				                isAvailable
+				                products
+				            }
+				        }
+				    }
+				}`,
+				OperationName: json.RawMessage(`"Employees"`),
+			})
+			require.NoError(t, err)
+			assert.Equal(t, 200, res.Response.StatusCode)
+
+			testenv.AwaitChannelWithT(t, 10*time.Second, resultsChan, func(t *testing.T, qps core.QueryPlanStats) {
+				assert.Equal(t, 10, qps.TotalSubgraphFetches)
+
+				expectedSubgraphFetches := map[string]int{
+					"availability": 2,
+					"employees":    3,
+					"family":       1,
+					"mood":         2,
+					"products":     2,
+				}
+
+				assert.Equal(t, expectedSubgraphFetches, qps.SubgraphFetches)
 			})
 		})
 	})
