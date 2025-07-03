fa68e481e9b05920aaa47839c1b6ec1c8d2a815c
Aenimus <daviditstutt@gmail.com>
2025-07-03 16:54:12 +0100

    chore: retain field-inheritable directives in parent object

--- composition/src/utils/string-constants.ts
+++ composition/src/utils/string-constants.ts
@@ -165,7 +165,7 @@ export const EXECUTABLE_DIRECTIVE_LOCATIONS = new Set<string>([
 export const ROOT_TYPE_NAMES = new Set<string>([MUTATION, QUERY, SUBSCRIPTION]);
 export const AUTHORIZATION_DIRECTIVES = new Set<string>([AUTHENTICATED, REQUIRES_SCOPES]);
 export const PERSISTED_CLIENT_DIRECTIVES = new Set<string>([DEPRECATED]);
-export const INHERITABLE_DIRECTIVE_NAMES = [EXTERNAL, SHAREABLE];
+export const INHERITABLE_DIRECTIVE_NAMES = new Set<string>([EXTERNAL, SHAREABLE]);
 export const IGNORED_FIELDS = new Set<string>([ENTITIES_FIELD, SERVICE_FIELD]);
 
 export const INPUT_NODE_KINDS = new Set<Kind>([
