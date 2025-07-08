9c66fcc91fb4619fc5ca0fed1f3c75bf614022d3
Wilson Rivera <wilson@wundergraph.com>
2025-07-07 22:10:00 -0400

    chore: linting

--- controlplane/src/core/bufservices/subgraph/getSubgraphMembers.ts
+++ controlplane/src/core/bufservices/subgraph/getSubgraphMembers.ts
@@ -8,7 +8,7 @@ import {
 import { SubgraphRepository } from '../../repositories/SubgraphRepository.js';
 import type { RouterOptions } from '../../routes.js';
 import { enrichLogger, getLogger, handleError } from '../../util.js';
-import { UnauthorizedError } from "../../errors/errors.js";
+import { UnauthorizedError } from '../../errors/errors.js';
 
 export function getSubgraphMembers(
   opts: RouterOptions,
