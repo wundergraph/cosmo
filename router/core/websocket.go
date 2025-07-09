9216621360a0481c0837b5b73547b00de410f2b0
DerZade <derzade@gmail.com>
2025-07-09 06:36:56 +0200

    fix(router): add nil check while copying expression context for WS subgraph requests

--- router/core/websocket.go
+++ router/core/websocket.go
@@ -985,7 +985,10 @@ func (h *WebSocketConnectionHandler) executeSubscription(registration *Subscript
 		w:                   nil,
 		r:                   registration.clientRequest,
 	})
-	reqContext.expressionContext = getRequestContext(h.request.Context()).expressionContext
+
+	if origCtx := getRequestContext(h.request.Context()); origCtx != nil {
+		reqContext.expressionContext = origCtx.expressionContext
+	}
 	resolveCtx = resolveCtx.WithContext(withRequestContext(h.ctx, reqContext))
 	if h.graphqlHandler.authorizer != nil {
 		resolveCtx = WithAuthorizationExtension(resolveCtx)
