55b33c14251d0a68233f87438628864ada32cc1d
Jens Neuse <jens.neuse@gmx.de>
2025-07-10 11:28:30 +0200

    chore: improve error handling

--- cli/src/commands/grpc-service/commands/list-templates.ts
+++ cli/src/commands/grpc-service/commands/list-templates.ts
@@ -22,24 +22,31 @@ export async function fetchAvailableTemplates(): Promise<string[]> {
 }
 
 export default (_opts: BaseCommandOptions) => {
-  const command = new Command('list-templates');
-  command.description('List all available gRPC service templates');
-  command.action(async () => {
-    const spinner = Spinner('Fetching available templates...').start();
-    const templates = await fetchAvailableTemplates();
-    spinner.stop();
-    if (templates.length > 0) {
-      console.log(pc.yellow('Available templates:'));
-      for (const t of templates) {
-        console.log(`  - ${t}`);
+  const command = new Command();
+  command
+    .name('list-templates')
+    .description('List all available gRPC service templates')
+    .action(async () => {
+      const spinner = Spinner('Fetching available templates...').start();
+      try {
+        const templates = await fetchAvailableTemplates();
+        spinner.stop();
+        if (templates.length > 0) {
+          console.log(pc.yellow('Available templates:'));
+          for (const t of templates) {
+            console.log(`  - ${t}`);
+          }
+          console.log('');
+          console.log(pc.yellow('To use a template, run:'));
+          console.log(`  wgc grpc-service init --template ${templates[0]} --directory ./output`);
+          console.log('');
+        } else {
+          console.log(pc.red('No templates found in wundergraph/cosmo-templates under grpc-service.'));
+        }
+      } catch (error) {
+        spinner.fail('Failed to fetch templates');
+        console.error(error);
       }
-      console.log('');
-      console.log(pc.yellow('To use a template, run:'));
-      console.log(`  wgc grpc-service init --template ${templates[0]} --directory ./output`);
-      console.log('');
-    } else {
-      console.log(pc.red('No templates found in wundergraph/cosmo-templates under grpc-service.'));
-    }
-  });
+    });
   return command;
 };
