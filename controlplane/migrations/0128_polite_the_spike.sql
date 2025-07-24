040697c258a59dbe780b190c4aa415d07b21a7f2
JivusAyrus <suvijsurya76@gmail.com>
2025-07-24 18:51:48 +0530

    chore: merge migrations

--- controlplane/migrations/0128_polite_the_spike.sql
+++ controlplane/migrations/0128_polite_the_spike.sql
@@ -3,8 +3,7 @@ CREATE TABLE IF NOT EXISTS "plugin_image_versions" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"schema_version_id" uuid NOT NULL,
 	"version" text NOT NULL,
-	"architecture" text NOT NULL,
-	"os" text NOT NULL,
+	"platform" text[] NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
--- controlplane/migrations/0129_greedy_ultron.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "plugin_image_versions" ADD COLUMN "platform" text[] NOT NULL;--> statement-breakpoint
-ALTER TABLE "plugin_image_versions" DROP COLUMN IF EXISTS "architecture";--> statement-breakpoint
-ALTER TABLE "plugin_image_versions" DROP COLUMN IF EXISTS "os";
