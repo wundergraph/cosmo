e105b559adf4b2d5acedd85307f8842aca9698d7
Wilson Rivera <wilson@wundergraph.com>
2025-08-05 16:28:16 -0400

    chore: downgrade Keycloak to `26.2.5`

--- .github/scripts/setup-keycloak.sh
+++ .github/scripts/setup-keycloak.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export KC_VERSION=26.3.2
+export KC_VERSION=26.2.5
 curl -LO  https://github.com/keycloak/keycloak/releases/download/"${KC_VERSION}"/keycloak-"${KC_VERSION}".zip
 
 unzip -q keycloak-${KC_VERSION}.zip
