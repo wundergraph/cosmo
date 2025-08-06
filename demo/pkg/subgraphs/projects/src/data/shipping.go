6a76e58e28c00c9fcbd1e0a6c98aa3a350688338
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2025-08-06 16:17:36 +0200

    chore: fix missing tracking number

--- demo/pkg/subgraphs/projects/src/data/shipping.go
+++ demo/pkg/subgraphs/projects/src/data/shipping.go
@@ -6,28 +6,34 @@ import (
 
 var ServiceShipping = []*projects.Shipping{
 	{
-		Id:     "1",
-		Weight: 2.5,
+		Id:             "1",
+		TrackingNumber: "UPS-1234567890",
+		Weight:         2.5,
 	},
 	{
-		Id:     "2",
-		Weight: 1.8,
+		Id:             "2",
+		TrackingNumber: "FEDEX-1234567891",
+		Weight:         1.8,
 	},
 	{
-		Id:     "3",
-		Weight: 4.2,
+		Id:             "3",
+		TrackingNumber: "DHL-1234567892",
+		Weight:         4.2,
 	},
 	{
-		Id:     "4",
-		Weight: 0.9,
+		Id:             "4",
+		TrackingNumber: "USPS-1234567893",
+		Weight:         0.9,
 	},
 	{
-		Id:     "5",
-		Weight: 3.7,
+		Id:             "5",
+		TrackingNumber: "ONTRAC-1234567894",
+		Weight:         3.7,
 	},
 	{
-		Id:     "6",
-		Weight: 2.1,
+		Id:             "6",
+		TrackingNumber: "CANPOST-1234567895",
+		Weight:         2.1,
 	},
 }
 
