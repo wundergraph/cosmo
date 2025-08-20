36d68e8f806206ebf9134b17e51c41c6e1ce81cf
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2025-08-20 13:43:44 +0200

    chore: update tests

--- protographic/tests/sdl-to-proto/10-options.test.ts
+++ protographic/tests/sdl-to-proto/10-options.test.ts
@@ -75,14 +75,27 @@ describe('SDL to Proto Options', () => {
     `);
   });
 
-  it('should generate proto with required option', () => {
+  it('should not generate required options for operation fields', () => {
     const sdl = `
       type Query {
-        stringField: String
-        intField: Int
+        stringField: String!
+        intField: Int!
         floatField: Float
         booleanField: Boolean
         idField: ID
+      }
+      
+      type Mutation {
+        createUser(input: UserInput!): User!
+      }
+
+      input UserInput {
+        name: String!
+      }
+      
+      type User {
+        id: ID!
+        name: String!
       }
     `;
 
@@ -96,10 +109,16 @@ describe('SDL to Proto Options', () => {
       "syntax = "proto3";
       package service.v1;
 
+      import "google/protobuf/descriptor.proto";
       import "google/protobuf/wrappers.proto";
+
+      extend google.protobuf.FieldOptions {
+        optional bool is_required = 50000;
+      }
 
       // Service definition for DefaultService
       service DefaultService {
+        rpc MutationCreateUser(MutationCreateUserRequest) returns (MutationCreateUserResponse) {}
         rpc QueryBooleanField(QueryBooleanFieldRequest) returns (QueryBooleanFieldResponse) {}
         rpc QueryFloatField(QueryFloatFieldRequest) returns (QueryFloatFieldResponse) {}
         rpc QueryIdField(QueryIdFieldRequest) returns (QueryIdFieldResponse) {}
@@ -112,14 +131,14 @@ describe('SDL to Proto Options', () => {
       }
       // Response message for stringField operation.
       message QueryStringFieldResponse {
-        google.protobuf.StringValue string_field = 1;
+        string string_field = 1;
       }
       // Request message for intField operation.
       message QueryIntFieldRequest {
       }
       // Response message for intField operation.
       message QueryIntFieldResponse {
-        google.protobuf.Int32Value int_field = 1;
+        int32 int_field = 1;
       }
       // Request message for floatField operation.
       message QueryFloatFieldRequest {
@@ -141,6 +160,23 @@ describe('SDL to Proto Options', () => {
       // Response message for idField operation.
       message QueryIdFieldResponse {
         google.protobuf.StringValue id_field = 1;
+      }
+      // Request message for createUser operation.
+      message MutationCreateUserRequest {
+        UserInput input = 1;
+      }
+      // Response message for createUser operation.
+      message MutationCreateUserResponse {
+        User create_user = 1;
+      }
+
+      message UserInput {
+        string name = 1 [(is_required) = true];
+      }
+
+      message User {
+        string id = 1 [(is_required) = true];
+        string name = 2 [(is_required) = true];
       }"
     `);
   });
