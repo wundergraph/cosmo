fa4a94cf9d04b3c179c30a0bb857caf6f5a118c3
Milinda Dias <83293842+SkArchon@users.noreply.github.com>
2025-08-26 00:24:50 +0530

    fix: add tracing by default and fix bugs (#2166)

--- cli/src/commands/router/commands/plugin/commands/init.ts
+++ cli/src/commands/router/commands/plugin/commands/init.ts
@@ -26,7 +26,9 @@ export default (opts: BaseCommandOptions) => {
     const cwd = process.cwd();
 
     const projectDir = resolve(cwd, options.directory, options.project);
-    const pluginDir = resolve(cwd, projectDir, name);
+
+    const pluginDir = options.project ? resolve(cwd, projectDir, 'plugins', name) : resolve(cwd, projectDir, name);
+
     const originalPluginName = name;
 
     name = upperFirst(camelCase(name));
@@ -97,6 +99,7 @@ export default (opts: BaseCommandOptions) => {
 
         // Create a project directory structure
         await mkdir(projectDir, { recursive: true });
+        await mkdir(resolve(projectDir, 'plugins'), { recursive: true });
 
         // Write router config to the project root
         await writeFile(resolve(projectDir, 'config.yaml'), ProjectTemplates.routerConfig);
