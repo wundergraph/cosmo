2dd7dfbcac8cf8cb2fb3b8d169706d835c379621
JivusAyrus <suvijsurya76@gmail.com>
2025-08-29 16:10:16 +0530

    chore: revert

--- controlplane/src/core/repositories/SubgraphRepository.ts
+++ controlplane/src/core/repositories/SubgraphRepository.ts
@@ -1182,7 +1182,6 @@ export class SubgraphRepository {
 
         const linkedCheck = await schemaCheckRepo.getLinkedSchemaCheck({
           schemaCheckID: c.id,
-          organizationId: this.organizationId,
         });
 
         return {
