dbe4ae72188e570693d69fa9b2d1c4db2efb6ee4
Yuji Okazawa <yujiorama+github@gmail.com>
2025-08-31 05:57:12 +0000

    feat: add approve and close commands for managing proposals

--- cli/src/commands/proposal/index.ts
+++ cli/src/commands/proposal/index.ts
@@ -3,12 +3,16 @@ import { BaseCommandOptions } from '../../core/types/types.js';
 import { checkAuth } from '../auth/utils.js';
 import CreateProposalCommand from './commands/create.js';
 import UpdateProposalCommand from './commands/update.js';
+import CloseProposalCommand from './commands/close.js';
+import ApproveProposalCommand from './commands/approve.js';
 
 export default (opts: BaseCommandOptions) => {
   const command = new Command('proposal');
   command.description('Provides commands for creating and maintaining proposals for a federated graph');
   command.addCommand(CreateProposalCommand(opts));
   command.addCommand(UpdateProposalCommand(opts));
+  command.addCommand(CloseProposalCommand(opts));
+  command.addCommand(ApproveProposalCommand(opts));
 
   command.hook('preAction', async (thisCmd) => {
     await checkAuth();
