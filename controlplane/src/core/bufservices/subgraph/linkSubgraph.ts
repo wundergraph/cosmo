090c794e9b76869d5b228a1a3390131f69be812e
JivusAyrus <suvijsurya76@gmail.com>
2025-09-04 16:12:10 +0530

    fix: lint

--- controlplane/src/core/bufservices/subgraph/linkSubgraph.ts
+++ controlplane/src/core/bufservices/subgraph/linkSubgraph.ts
@@ -24,7 +24,7 @@ export function linkSubgraph(
 
     const subgraphRepo = new SubgraphRepository(logger, opts.db, authContext.organizationId);
     const namespaceRepo = new NamespaceRepository(opts.db, authContext.organizationId);
-    
+
     const sourceNamespace = await namespaceRepo.byName(req.sourceSubgraphNamespace);
     if (!sourceNamespace) {
       return {
