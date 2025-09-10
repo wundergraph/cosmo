3d428fb6102f6213dd52d08ee5f1de136a7320e7
Yury Smolski <140245+ysmolski@users.noreply.github.com>
2025-09-10 18:36:45 +0300

    just a test

--- router/cmd/main.go
+++ router/cmd/main.go
@@ -23,6 +23,8 @@ import (
 	"go.uber.org/zap"
 )
 
+// foobar comment
+
 var (
 	overrideEnvFlag = flag.String("override-env", os.Getenv("OVERRIDE_ENV"), "Path to .env file to override environment variables")
 	routerVersion   = flag.Bool("version", false, "Prints the version and dependency information")
