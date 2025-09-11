22026628520d6cc4fff501b266881d087d1caec0
miklosbarabas <miklos@wundergraph.com>
2025-09-11 13:59:38 +0200

    feat: setup sentry for the controlplane

--- controlplane/src/core/env.schema.ts
+++ controlplane/src/core/env.schema.ts
@@ -178,6 +178,31 @@ export const envVariables = z
      * Admission Webhook
      */
     AUTH_ADMISSION_JWT_SECRET: z.string(),
+
+    /**
+     * Sentry
+     */
+    SENTRY_ENABLED: z
+      .string()
+      .optional()
+      .transform((val) => val === 'true')
+      .default('false'),
+    SENTRY_DSN: z.string().optional(),
+    SENTRY_SEND_DEFAULT_PII: z
+      .string()
+      .optional()
+      .transform((val) => val === 'true'),
+    SENTRY_TRACES_SAMPLE_RATE: z
+      .string()
+      .transform((val) => Number.parseInt(val))
+      .optional()
+      .default('0.0'),
+    SENTRY_PROFILE_SESSION_SAMPLE_RATE: z
+      .string()
+      .transform((val) => Number.parseInt(val))
+      .optional()
+      .default('0.0'),
+    SENTRY_EVENT_LOOP_BLOCK_THRESHOLD_MS: z.coerce.number().optional(),
   })
   .refine((input) => {
     if (input.STRIPE_WEBHOOK_SECRET && !input.STRIPE_SECRET_KEY) {
--- controlplane/src/core/sentry.config.ts
+++ controlplane/src/core/sentry.config.ts
@@ -4,17 +4,28 @@ import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import { eventLoopBlockIntegration } from '@sentry/node-native';
 
-if (process.env.SENTRY_ENABLED) {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-      eventLoopBlockIntegration({ threshold: Number(process.env.SENTRY_EVENT_LOOP_BLOCK_THRESHOLD_MS) }),
-      nodeProfilingIntegration(),
-    ],
-    profileSessionSampleRate: Number(process.env.SENTRY_PROFILE_SESSION_SAMPLE_RATE),
-    sendDefaultPii: Boolean(process.env.SENTRY_SEND_DEFAULT_PII),
-    tracesSampleRate: Number(process.env.SENTRY_TRACES_SAMPLE_RATE),
-  });
+export interface SentryConfig {
+  sentry: {
+    enabled: boolean;
+    dsn: string;
+    eventLoopBlockIntegrationThresholdMs?: number;
+    profileSessionSampleRate?: number;
+    sendDefaultPii?: boolean;
+    tracesSampleRate?: number;
+  };
 }
 
-export * as Sentry from '@sentry/node';
+export function init(opts: SentryConfig) {
+  if (opts.sentry.enabled) {
+    Sentry.init({
+      dsn: opts.sentry.dsn,
+      integrations: [
+        eventLoopBlockIntegration({ threshold: opts.sentry.eventLoopBlockIntegrationThresholdMs }),
+        nodeProfilingIntegration(),
+      ],
+      profileSessionSampleRate: opts.sentry.profileSessionSampleRate,
+      sendDefaultPii: opts.sentry.sendDefaultPii,
+      tracesSampleRate: opts.sentry.tracesSampleRate,
+    });
+  }
+}
