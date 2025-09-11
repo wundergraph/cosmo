5a99c5ceb3af611d8e69a5d010abc06a73c5690c
miklosbarabas <miklos@wundergraph.com>
2025-09-11 16:54:11 +0200

    chore: testing query planner ci

--- router/cmd/main.go
+++ router/cmd/main.go
@@ -23,8 +23,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// TEST COMMENT, REMOVE ME!
-
 var (
 	overrideEnvFlag = flag.String("override-env", os.Getenv("OVERRIDE_ENV"), "Path to .env file to override environment variables")
 	routerVersion   = flag.Bool("version", false, "Prints the version and dependency information")
