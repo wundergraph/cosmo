db1964c1fdaea1b24566aafe27aaa7feb5bf9328
Milinda Dias <skay.archon@gmail.com>
2025-09-18 03:10:19 +0530

    fix: increase duration

--- router-tests/circuit_breaker_test.go
+++ router-tests/circuit_breaker_test.go
@@ -724,7 +724,7 @@ func TestCircuitBreaker(t *testing.T) {
 			require.Equal(t, failedTries, employeesCalls.Load())
 
 			// Wait for current bucket to be cleaned up
-			time.Sleep(breaker.RollingDuration + time.Millisecond*2000)
+			time.Sleep(breaker.RollingDuration*3 + time.Millisecond*1000)
 
 			// ====
 			// Verify a success case with messages validated from here onwards
