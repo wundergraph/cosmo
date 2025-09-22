cfb3597ac3b4a14d6138c99dab20fc5c81c5c19d
miklosbarabas <miklos@wundergraph.com>
2025-09-22 14:18:14 +0200

    chore: router ci improvement - using gotestsum to retry flaky tests

--- router-tests/events/nats_events_test.go
+++ router-tests/events/nats_events_test.go
@@ -62,7 +62,7 @@ type natsSubscriptionArgs struct {
 	errValue  error
 }
 
-func TestFlakyNatsEvents(t *testing.T) {
+func TestNatsEvents(t *testing.T) {
 	t.Parallel()
 
 	t.Run("subscribe async", func(t *testing.T) {
