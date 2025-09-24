a02a85deb398d9533d4a050c96fbb89e23c32957
miklosbarabas <miklos@wundergraph.com>
2025-09-24 13:35:09 +0200

    chore: testing query planner ci

--- router/cmd/main.go
+++ router/cmd/main.go
@@ -23,7 +23,7 @@ import (
 	"go.uber.org/zap"
 )
 
-// TRIGGER WORKFLOW, REMOVE ME LATER! #GRRBRRRRR23467565675! "jobs fail with error code 143"
+// TODO: REMOVE ME! - TRIGGER WORKFLOW, REMOVE ME LATER! #GRRBRRRRR23467565675! "jobs fail with error code 143"
 
 var (
 	overrideEnvFlag = flag.String("override-env", os.Getenv("OVERRIDE_ENV"), "Path to .env file to override environment variables")
