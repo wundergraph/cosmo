d61dcde7fea216285fc78208313c7d90dbfed94c
Alessandro Pagnin <alepane@gmail.com>
2025-09-24 16:54:20 +0200

    chore: add a test to verify that StreamReceiveEventHook can access headers

--- router-tests/modules/stream_receive_test.go
+++ router-tests/modules/stream_receive_test.go
@@ -358,6 +358,105 @@ func TestReceiveHook(t *testing.T) {
 		})
 	})
 
+	t.Run("Test Receive hook can access custom header", func(t *testing.T) {
+		t.Parallel()
+
+		customHeader := http.CanonicalHeaderKey("X-Custom-Header")
+
+		cfg := config.Config{
+			Graph: config.Graph{},
+
+			Modules: map[string]interface{}{
+				"streamReceiveModule": stream_receive.StreamReceiveModule{
+					Callback: func(ctx core.StreamReceiveEventHookContext, events []datasource.StreamEvent) ([]datasource.StreamEvent, error) {
+						if val, ok := ctx.Request().Header[customHeader]; !ok || val[0] != "Test" {
+							return events, nil
+						}
+						for _, event := range events {
+							evt, ok := event.(*kafka.Event)
+							if !ok {
+								continue
+							}
+							evt.Data = []byte(`{"__typename":"Employee","id": 3,"update":{"name":"foo"}}`)
+						}
+
+						return events, nil
+					},
+				},
+			},
+		}
+
+		testenv.Run(t, &testenv.Config{
+			RouterConfigJSONTemplate: testenv.ConfigWithEdfsKafkaJSONTemplate,
+			EnableKafka:              true,
+			RouterOptions: []core.Option{
+				core.WithModulesConfig(cfg.Modules),
+				core.WithCustomModules(&stream_receive.StreamReceiveModule{}),
+			},
+			LogObservation: testenv.LogObservationConfig{
+				Enabled:  true,
+				LogLevel: zapcore.InfoLevel,
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			topics := []string{"employeeUpdated"}
+			events.KafkaEnsureTopicExists(t, xEnv, time.Second, topics...)
+
+			var subscriptionOne struct {
+				employeeUpdatedMyKafka struct {
+					ID      float64 `graphql:"id"`
+					Details struct {
+						Forename string `graphql:"forename"`
+						Surname  string `graphql:"surname"`
+					} `graphql:"details"`
+				} `graphql:"employeeUpdatedMyKafka(employeeID: 3)"`
+			}
+			headers := http.Header{
+				customHeader: []string{"Test"},
+			}
+
+			surl := xEnv.GraphQLWebSocketSubscriptionURL()
+			client := graphql.NewSubscriptionClient(surl)
+			client.WithWebSocketOptions(graphql.WebsocketOptions{
+				HTTPHeader: headers,
+			})
+
+			subscriptionArgsCh := make(chan kafkaSubscriptionArgs)
+			subscriptionOneID, err := client.Subscribe(&subscriptionOne, nil, func(dataValue []byte, errValue error) error {
+				subscriptionArgsCh <- kafkaSubscriptionArgs{
+					dataValue: dataValue,
+					errValue:  errValue,
+				}
+				return nil
+			})
+			require.NoError(t, err)
+			require.NotEmpty(t, subscriptionOneID)
+
+			clientRunCh := make(chan error)
+			go func() {
+				clientRunCh <- client.Run()
+			}()
+
+			xEnv.WaitForSubscriptionCount(1, Timeout)
+
+			events.ProduceKafkaMessage(t, xEnv, Timeout, topics[0], `{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`)
+
+			testenv.AwaitChannelWithT(t, Timeout, subscriptionArgsCh, func(t *testing.T, args kafkaSubscriptionArgs) {
+				require.NoError(t, args.errValue)
+				assert.JSONEq(t, `{"employeeUpdatedMyKafka":{"id":3,"details":{"forename":"Stefan","surname":"Avram"}}}`, string(args.dataValue))
+			})
+
+			unSub1Err := client.Unsubscribe(subscriptionOneID)
+			require.NoError(t, unSub1Err)
+			require.NoError(t, client.Close())
+			testenv.AwaitChannelWithT(t, Timeout, clientRunCh, func(t *testing.T, err error) {
+				require.NoError(t, err)
+			}, "unable to close client before timeout")
+
+			requestLog := xEnv.Observer().FilterMessage("Stream Hook has been run")
+			assert.Len(t, requestLog.All(), 1)
+		})
+	})
+
 	t.Run("Test Batch hook error should close Kafka clients and subscriptions", func(t *testing.T) {
 		t.Parallel()
 
