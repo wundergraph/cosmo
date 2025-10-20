6f0b9c472b813a5136bcd809da0aa78c8c1e7617
Dominik Korittki <23359034+dkorittki@users.noreply.github.com>
2025-10-20 14:09:54 +0200

    chore: add tests for error log deduplication

--- router-tests/modules/stream_receive_test.go
+++ router-tests/modules/stream_receive_test.go
@@ -2,7 +2,9 @@ package module_test
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -548,6 +550,219 @@ func TestReceiveHook(t *testing.T) {
 			}, "client should have completed when server closed connection")
 
 			xEnv.WaitForTriggerCount(0, Timeout)
+		})
+	})
+
+	t.Run("Test error deduplication with multiple subscriptions", func(t *testing.T) {
+		t.Parallel()
+
+		cfg := config.Config{
+			Graph: config.Graph{},
+			Events: config.EventsConfiguration{
+				SubscriptionHooks: config.SubscriptionHooksConfiguration{
+					MaxConcurrentEventReceiveHandlers: defaultMaxConcurrentEventReceiveHandlers,
+				},
+			},
+			Modules: map[string]interface{}{
+				"streamReceiveModule": stream_receive.StreamReceiveModule{
+					Callback: func(ctx core.StreamReceiveEventHandlerContext, events []datasource.StreamEvent) ([]datasource.StreamEvent, error) {
+						return nil, errors.New("deduplicated error")
+					},
+				},
+			},
+		}
+
+		testenv.Run(t, &testenv.Config{
+			RouterConfigJSONTemplate: testenv.ConfigWithEdfsKafkaJSONTemplate,
+			EnableKafka:              true,
+			RouterOptions: []core.Option{
+				core.WithModulesConfig(cfg.Modules),
+				core.WithCustomModules(&stream_receive.StreamReceiveModule{}),
+				core.WithSubscriptionHooks(cfg.Events.SubscriptionHooks),
+			},
+			LogObservation: testenv.LogObservationConfig{
+				Enabled:  true,
+				LogLevel: zapcore.ErrorLevel,
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			topics := []string{"employeeUpdated"}
+			events.KafkaEnsureTopicExists(t, xEnv, time.Second, topics...)
+
+			var subscriptionOne struct {
+				employeeUpdatedMyKafka struct {
+					ID      float64 `graphql:"id"`
+					Details struct {
+						Forename string `graphql:"forename"`
+						Surname  string `graphql:"surname"`
+					} `graphql:"details"`
+				} `graphql:"employeeUpdatedMyKafka(employeeID: 3)"`
+			}
+
+			surl := xEnv.GraphQLWebSocketSubscriptionURL()
+
+			// Create 3 subscriptions that will all receive the same error
+			clients := make([]*graphql.SubscriptionClient, 3)
+			clientRunChs := make([]chan error, 3)
+
+			for i := range 3 {
+				clients[i] = graphql.NewSubscriptionClient(surl)
+				clientRunChs[i] = make(chan error)
+
+				subscriptionID, err := clients[i].Subscribe(&subscriptionOne, nil, func(dataValue []byte, errValue error) error {
+					return nil
+				})
+				require.NoError(t, err)
+				require.NotEmpty(t, subscriptionID)
+
+				go func() {
+					clientRunChs[i] <- clients[i].Run()
+				}()
+			}
+
+			// Wait for all subscriptions to be established
+			xEnv.WaitForSubscriptionCount(3, Timeout)
+
+			// Produce a message that will trigger the error in all handlers
+			events.ProduceKafkaMessage(t, xEnv, Timeout, topics[0], `{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`)
+
+			// Wait for all subscriptions to be closed due to the error
+			xEnv.WaitForSubscriptionCount(0, Timeout)
+
+			// Verify all clients completed
+			for i := 0; i < 3; i++ {
+				testenv.AwaitChannelWithT(t, Timeout, clientRunChs[i], func(t *testing.T, err error) {
+					require.NoError(t, err)
+				}, "client should have completed when server closed connection")
+			}
+
+			xEnv.WaitForTriggerCount(0, Timeout)
+
+			// Verify error deduplication: should see only one error log entry
+			errorLogs := xEnv.Observer().FilterMessage("some handlers have thrown an error")
+			assert.Len(t, errorLogs.All(), 1, "should have exactly one deduplicated error log entry")
+
+			// Verify the error log contains the correct error message and count
+			if len(errorLogs.All()) > 0 {
+				logEntry := errorLogs.All()[0]
+				fields := logEntry.ContextMap()
+
+				assert.Equal(t, "deduplicated error", fields["error"], "error message should match")
+				assert.Equal(t, int64(3), fields["amount_handlers"], "should count all 3 handlers that threw the error")
+			}
+		})
+	})
+
+	t.Run("Test unique error messages are all logged", func(t *testing.T) {
+		t.Parallel()
+
+		var errorCounter atomic.Int32
+
+		cfg := config.Config{
+			Graph: config.Graph{},
+			Events: config.EventsConfiguration{
+				SubscriptionHooks: config.SubscriptionHooksConfiguration{
+					MaxConcurrentEventReceiveHandlers: defaultMaxConcurrentEventReceiveHandlers,
+				},
+			},
+			Modules: map[string]interface{}{
+				"streamReceiveModule": stream_receive.StreamReceiveModule{
+					Callback: func(ctx core.StreamReceiveEventHandlerContext, events []datasource.StreamEvent) ([]datasource.StreamEvent, error) {
+						count := errorCounter.Add(1)
+						return nil, fmt.Errorf("unique error %d", count)
+					},
+				},
+			},
+		}
+
+		testenv.Run(t, &testenv.Config{
+			RouterConfigJSONTemplate: testenv.ConfigWithEdfsKafkaJSONTemplate,
+			EnableKafka:              true,
+			RouterOptions: []core.Option{
+				core.WithModulesConfig(cfg.Modules),
+				core.WithCustomModules(&stream_receive.StreamReceiveModule{}),
+				core.WithSubscriptionHooks(cfg.Events.SubscriptionHooks),
+			},
+			LogObservation: testenv.LogObservationConfig{
+				Enabled:  true,
+				LogLevel: zapcore.ErrorLevel,
+			},
+		}, func(t *testing.T, xEnv *testenv.Environment) {
+			topics := []string{"employeeUpdated"}
+			events.KafkaEnsureTopicExists(t, xEnv, time.Second, topics...)
+
+			var subscriptionOne struct {
+				employeeUpdatedMyKafka struct {
+					ID      float64 `graphql:"id"`
+					Details struct {
+						Forename string `graphql:"forename"`
+						Surname  string `graphql:"surname"`
+					} `graphql:"details"`
+				} `graphql:"employeeUpdatedMyKafka(employeeID: 3)"`
+			}
+
+			surl := xEnv.GraphQLWebSocketSubscriptionURL()
+
+			// Create 3 subscriptions that will each receive a unique error
+			clients := make([]*graphql.SubscriptionClient, 3)
+			clientRunChs := make([]chan error, 3)
+
+			for i := range 3 {
+				clients[i] = graphql.NewSubscriptionClient(surl)
+				clientRunChs[i] = make(chan error)
+
+				subscriptionID, err := clients[i].Subscribe(&subscriptionOne, nil, func(dataValue []byte, errValue error) error {
+					return nil
+				})
+				require.NoError(t, err)
+				require.NotEmpty(t, subscriptionID)
+
+				go func() {
+					clientRunChs[i] <- clients[i].Run()
+				}()
+			}
+
+			// Wait for all subscriptions to be established
+			xEnv.WaitForSubscriptionCount(3, Timeout)
+
+			// Produce a message that will trigger a unique error in each handler
+			events.ProduceKafkaMessage(t, xEnv, Timeout, topics[0], `{"__typename":"Employee","id": 1,"update":{"name":"foo"}}`)
+
+			// Wait for all subscriptions to be closed due to the error
+			xEnv.WaitForSubscriptionCount(0, Timeout)
+
+			// Verify all clients completed
+			for i := range 3 {
+				testenv.AwaitChannelWithT(t, Timeout, clientRunChs[i], func(t *testing.T, err error) {
+					require.NoError(t, err)
+				}, "client should have completed when server closed connection")
+			}
+
+			xEnv.WaitForTriggerCount(0, Timeout)
+
+			// Verify no deduplication: should see three error log entries (one for each unique error)
+			errorLogs := xEnv.Observer().FilterMessage("some handlers have thrown an error")
+			assert.Len(t, errorLogs.All(), 3, "should have three separate error log entries for unique errors")
+
+			// Verify each error log contains a unique error message and count of 1
+			if len(errorLogs.All()) == 3 {
+				var errorMessages []string
+				for _, logEntry := range errorLogs.All() {
+					fields := logEntry.ContextMap()
+					errorMsg, ok := fields["error"].(string)
+					require.True(t, ok, "error field should be a string")
+
+					// Check that error message is unique (starts with "unique error")
+					assert.Contains(t, errorMsg, "unique error", "error message should contain 'unique error'")
+					assert.NotContains(t, errorMessages, errorMsg, "each error message should be unique")
+					errorMessages = append(errorMessages, errorMsg)
+
+					// Each unique error should have been thrown by exactly 1 handler
+					assert.Equal(t, int64(1), fields["amount_handlers"], "each unique error should have amount_handlers = 1")
+				}
+
+				// Verify we got exactly 3 unique error messages
+				assert.Len(t, errorMessages, 3, "should have exactly 3 unique error messages")
+			}
 		})
 	})
 }
