268257555a22ed5e675b0999eae366bd453e044d
Yury Smolski <140245+ysmolski@users.noreply.github.com>
2025-10-22 11:44:14 +0300

    implement directivesRoot for requireFetchReasons
    It is just a dummy handler.

--- demo/pkg/subgraphs/employees/employees.go
+++ demo/pkg/subgraphs/employees/employees.go
@@ -1,6 +1,8 @@
 package employees
 
 import (
+	"context"
+
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/wundergraph/cosmo/demo/pkg/subgraphs/employees/subgraph"
 	"github.com/wundergraph/cosmo/demo/pkg/subgraphs/employees/subgraph/generated"
@@ -8,8 +10,15 @@ import (
 )
 
 func NewSchema(natsPubSubByProviderID map[string]nats.Adapter) graphql.ExecutableSchema {
-	return generated.NewExecutableSchema(generated.Config{Resolvers: &subgraph.Resolver{
-		NatsPubSubByProviderID: natsPubSubByProviderID,
-		EmployeesData:          subgraph.Employees,
-	}})
+	return generated.NewExecutableSchema(generated.Config{
+		Resolvers: &subgraph.Resolver{
+			NatsPubSubByProviderID: natsPubSubByProviderID,
+			EmployeesData:          subgraph.Employees,
+		},
+		Directives: generated.DirectiveRoot{
+			Openfed__requireFetchReasons: func(ctx context.Context, obj any, next graphql.Resolver) (res any, err error) {
+				return next(context.WithValue(ctx, "Openfed__requireFetchReasons", obj))
+			},
+		},
+	})
 }
