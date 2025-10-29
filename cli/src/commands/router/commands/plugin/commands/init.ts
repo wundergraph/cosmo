dc15d41bfb6faa83067c3df6219251f0734e70c2
Milinda Dias <skay.archon@gmail.com>
2025-10-29 19:33:17 +0530

    fix: refactoring

--- cli/src/commands/router/commands/plugin/commands/init.ts
+++ cli/src/commands/router/commands/plugin/commands/init.ts
@@ -131,7 +131,7 @@ export default (opts: BaseCommandOptions) => {
         // Write router config to the project root
         await writeFile(resolve(projectDir, 'config.yaml'), ProjectTemplates.routerConfig);
         await writeFile(resolve(projectDir, 'graph.yaml'), pupa(ProjectTemplates.graphConfig, { originalPluginName }));
-        await writeFile(resolve(projectDir, 'Makefile'), pupa(ProjectTemplates.makefile, { originalPluginName }));
+        await writeFile(resolve(projectDir, 'Makefile'), pupa(PluginTemplates.makefile, { originalPluginName }));
         await writeFile(resolve(projectDir, '.gitignore'), ProjectTemplates.gitignore);
         await writeFile(
             resolve(projectDir, 'README.md'),
