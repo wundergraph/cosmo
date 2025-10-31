ab7c5d47ce084bb0b99aebd6781061634bba4359
Ludwig Bedacht <ludwig.bedacht@gmail.com>
2025-10-31 08:28:34 +0100

    chore: remove unused import

--- protographic/tests/sdl-to-proto/04-federation.test.ts
+++ protographic/tests/sdl-to-proto/04-federation.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { compileGraphQLToProto } from '../../src';
 import { expectValidProto } from '../util';
 
