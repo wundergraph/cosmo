e6d811fdd6097cc10820fea8d7738911b6b5a58d
miklosbarabas <miklos@wundergraph.com>
2025-11-16 19:26:28 +0100

    feat: add user context in controlplane sentry - also added appVersion to Sentry release

--- controlplane/vite.config.ts
+++ controlplane/vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     pool: 'forks',
     // Increase the timeout for integration tests
-    testTimeout: 20_000,
+    testTimeout: 35_000,
     teardownTimeout: 10_000,
     // Ensure always the CJS version is used otherwise we might conflict with multiple versions of graphql
     alias: [{ find: /^graphql$/, replacement: 'graphql/index.js' }],
