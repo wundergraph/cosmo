19e3c543826c0947f4c30403429ca4583b706902
Alessandro Pagnin <alepane@gmail.com>
2025-11-19 17:21:59 +0100

    chore: remove unused BaseURL

--- router/pkg/mcpserver/server.go
+++ router/pkg/mcpserver/server.go
@@ -54,9 +54,6 @@ type Options struct {
 	OperationsDir string
 	// ListenAddr is the address where the server should listen to
 	ListenAddr string
-	// BaseURL of the MCP server. This is the URL advertised to the LLM clients.
-	// By default, the base URL is relative to the URL that the router is running on.
-	BaseURL string
 	// Enabled determines whether the MCP server should be started
 	Enabled bool
 	// Logger is the logger to be used
@@ -76,7 +73,6 @@ type Options struct {
 // GraphQLSchemaServer represents an MCP server that works with GraphQL schemas and operations
 type GraphQLSchemaServer struct {
 	server                    *server.MCPServer
-	baseURL                   string
 	graphName                 string
 	operationsDir             string
 	listenAddr                string
@@ -218,7 +214,6 @@ func NewGraphQLSchemaServer(routerGraphQLEndpoint string, opts ...func(*Options)
 		enableArbitraryOperations: options.EnableArbitraryOperations,
 		exposeSchema:              options.ExposeSchema,
 		stateless:                 options.Stateless,
-		baseURL:                   options.BaseURL,
 	}
 
 	return gs, nil
@@ -240,13 +235,6 @@ func WithGraphName(graphName string) func(*Options) {
 func WithOperationsDir(operationsDir string) func(*Options) {
 	return func(o *Options) {
 		o.OperationsDir = operationsDir
-	}
-}
-
-// WithBaseURL sets the base URL
-func WithBaseURL(baseURL string) func(*Options) {
-	return func(o *Options) {
-		o.BaseURL = baseURL
 	}
 }
 
