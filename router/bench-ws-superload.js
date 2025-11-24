95d139547255bba886ae1bb6ceab74df503cc8d9
endigma <endigma@mailcat.ca>
2025-11-24 13:15:47 +0000

    wip

--- router/bench-ws-superload.js
+++ router/bench-ws-superload.js
@@ -1,5 +1,6 @@
 import ws from 'k6/ws';
 import { check } from 'k6';
+import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 
 /*
   This script was originally intended to simulate an extremely high load on websocket connections. It may also be useful as a base for new websocket scenarios.
@@ -7,22 +8,29 @@ import { check } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '0s', target: 10000 },
-    { duration: '5m', target: 10000 },
+    { duration: '0s', target: 10000 }, // Start immediately at 500 VUs
+    { duration: '5m', target: 10000 }, // Ramp up to 1000 over 5 minutes
   ],
 };
 
 export default function () {
+  const id = uuidv4();
+
+  const max = 120;
+  const intervalMilliseconds = 500;
+
   const url = 'ws://localhost:3002/graphql';
   const params = {
     headers: {
       'Sec-WebSocket-Protocol': 'graphql-transport-ws',
+      'X-User-Id': id,
     },
   };
 
   const res = ws.connect(url, params, function (socket) {
     socket.on('open', () => {
       // Send connection_init message
+      console.log(`${id}: Connection initialized`);
       socket.send(
         JSON.stringify({
           type: 'connection_init',
@@ -33,27 +41,36 @@ export default function () {
     socket.on('message', function (message) {
       const data = JSON.parse(message);
 
-      console.log(message);
+      let recieved = 0;
 
       switch (data.type) {
         case 'connection_ack':
           // Connection acknowledged, start subscription
           socket.send(
             JSON.stringify({
-              id: '1',
+              id: id,
               type: 'subscribe',
               payload: {
-                query: 'subscription { countHob(max: 50000, intervalMilliseconds: 1) }',
+                query: `subscription { countHob(max: ${max}, intervalMilliseconds: ${intervalMilliseconds}) }`,
               },
             }),
           );
-          console.log('Subscription started');
+          console.log(`${id}: Subscription started`);
           break;
         case 'next':
-          console.log('Subscription next:', data.payload);
+          recieved++;
+
+          if (recieved >= 5) {
+            socket.send(
+              JSON.stringify({
+                id: id,
+                type: 'complete',
+              }),
+            );
+          }
           break;
         case 'complete':
-          console.log('Subscription completed');
+          console.log(`${id}: Subscription completed`);
           break;
       }
     });
@@ -68,17 +85,6 @@ export default function () {
       }
     });
   });
-
-  // Cancel subscription after 20 seconds
-  setTimeout(() => {
-    socket.send(
-      JSON.stringify({
-        id: '1',
-        type: 'complete',
-      }),
-    );
-    socket.close();
-  }, 20000);
 
   check(res, {
     'WebSocket connection established': (r) => r && r.status === 101,
