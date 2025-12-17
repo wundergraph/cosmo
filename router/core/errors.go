17ede0f97a81eab76d6e2e6e11eafd21cbf2deeb
Milinda Dias <skay.archon@gmail.com>
2025-12-17 16:04:03 +0530

    fix: updates

--- router/core/errors.go
+++ router/core/errors.go
@@ -125,6 +125,7 @@ func trackFinalResponseError(ctx context.Context, err error) {
 	requestContext.SetError(err)
 	requestContext.graphQLErrorServices = getAggregatedSubgraphServiceNames(requestContext.error)
 	requestContext.graphQLErrorCodes = getAggregatedSubgraphErrorCodes(requestContext.error)
+
 	rtrace.AttachErrToSpan(span, err)
 }
 
