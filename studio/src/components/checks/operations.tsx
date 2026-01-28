d728a19c6f2f5c8706f885d72a313c6d20d964dc
Alessandro Pagnin <alepane@gmail.com>
2026-01-28 19:30:29 +0100

    fix: remove second hash

--- studio/src/components/checks/operations.tsx
+++ studio/src/components/checks/operations.tsx
@@ -455,22 +455,11 @@ export const CheckOperations = () => {
                   <AccordionContent>
                     <div className="mt-2 flex flex-col gap-y-6 px-2">
                       <div className="items-center justify-between space-y-6 md:flex-row xl:flex xl:space-y-0">
-                        <div className="space-y-1">
-                          <p className="text-muted-foreground">
-                            {firstSeenFormatted === lastSeenAtFormatted
-                              ? `Last seen at ${lastSeenAtFormatted}`
-                              : `First seen at ${firstSeenFormatted} and last seen at ${lastSeenAtFormatted}`}
-                          </p>
-                          <div className="flex items-center gap-2 text-sm text-muted-foreground">
-                            <span className="font-mono">{hash}</span>
-                            <CopyButton
-                              tooltip="Copy operation hash"
-                              value={hash}
-                              size="icon-sm"
-                              className="text-muted-foreground"
-                            />
-                          </div>
-                        </div>
+                        <p className="text-muted-foreground">
+                          {firstSeenFormatted === lastSeenAtFormatted
+                            ? `Last seen at ${lastSeenAtFormatted}`
+                            : `First seen at ${firstSeenFormatted} and last seen at ${lastSeenAtFormatted}`}
+                        </p>
                         <div className="flex items-center gap-x-2">
                           <OperationContentDialog
                             hash={hash}
