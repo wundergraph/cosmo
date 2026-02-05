cbcb94f037b27103328fbad5f5066bd24896f6d4
Alena Sviridenko <alenasviridenko@github.com>
2026-02-05 12:15:59 +0000

    Exposing operation timings

---
+++ router-tests/modules/custom-operation-timings/module.go
@@ -0,0 +1,42 @@
+package custom_operation_timings
+
+import (
+	"net/http"
+
+	"github.com/wundergraph/cosmo/router/core"
+)
+
+const myModuleID = "operationTimingsModule"
+
+// OperationTimingsModule is a simple module that reads and logs the operation timings
+type OperationTimingsModule struct {
+	ResultsChan chan core.OperationTimings
+}
+
+func (m *OperationTimingsModule) Middleware(ctx core.RequestContext, next http.Handler) {
+	timings := ctx.Operation().Timings()
+
+	m.ResultsChan <- timings
+
+	// Call the next handler in the chain or return early by calling w.Write()
+	next.ServeHTTP(ctx.ResponseWriter(), ctx.Request())
+}
+
+func (m *OperationTimingsModule) Module() core.ModuleInfo {
+	return core.ModuleInfo{
+		// This is the ID of your module, it must be unique
+		ID: myModuleID,
+		// The priority of your module, lower numbers are executed first
+		Priority: 1,
+		New: func() core.Module {
+			return &OperationTimingsModule{
+				ResultsChan: make(chan core.OperationTimings),
+			}
+		},
+	}
+}
+
+// Interface guard
+var (
+	_ core.RouterMiddlewareHandler = (*OperationTimingsModule)(nil)
+)
