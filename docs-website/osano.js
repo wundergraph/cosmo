e7405d05124ef2961164bdbf87d68a05024144d0
StarpTech <deusdustin@gmail.com>
2026-03-13 14:53:55 +0100

    chore: move official cosmo docs to monorepo

---
+++ docs-website/osano.js
@@ -0,0 +1,7 @@
+(function() {
+  const script = document.createElement('script');
+  script.src = 'https://cmp.osano.com/useYQdd6k1/6006b494-d3ca-4de7-860d-e8e78f744c5f/osano.js';
+  script.async = true;
+
+  document.head.appendChild(script);
+})();
