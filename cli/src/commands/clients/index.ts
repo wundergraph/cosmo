dfd762d13add8b2f3a4cc03c56f8aedcb99643f7
Ondřej Synáček <ondrej@wundergraph.com>
2026-04-27 13:57:18 +0200

    feat: delete clients CLI integration

--- cli/src/commands/clients/index.ts
+++ cli/src/commands/clients/index.ts
@@ -1,14 +1,16 @@
 import { Command } from 'commander';
 import { BaseCommandOptions } from '../../core/types/types.js';
 import { checkAuth } from '../auth/utils.js';
+import DeleteClientsCommand from './commands/delete.js';
 import ListClientsCommand from './commands/list.js';
 
 export default (opts: BaseCommandOptions) => {
   const command = new Command('clients');
   command.description('Provides commands for managing clients');
+  command.addCommand(DeleteClientsCommand(opts));
   command.addCommand(ListClientsCommand(opts));
 
-  command.hook('preAction', async (thisCmd) => {
+  command.hook('preAction', async () => {
     await checkAuth();
   });
 
