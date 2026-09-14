1bfb86321dcc9a4879406b9b952f6ed67fe72879
Ondrej Synacek <ondrej@wundergraph.com>
2026-09-14 11:06:32 +0200

    chore(deps): downgrade vitest to v4 (#3241)

--- cli/vite.config.ts
+++ cli/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     // globals: true,
+    clearMocks: true,
     restoreMocks: true,
     exclude: [...configDefaults.exclude, 'dist/**'],
   },
